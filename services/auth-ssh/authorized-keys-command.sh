4ce7956ca9bbf4b248fe7da71b930779fc56ac44
Sebastian Siemssen <fubhy@fubhy.com>
2017-06-26 11:27:15 +0200

    Making auth-ssh work again.

--- services/auth-ssh/authorized-keys-command.sh
+++ services/auth-ssh/authorized-keys-command.sh
@@ -5,5 +5,5 @@ server="http://$API_HOST:8080"
 keys=$(wget $server/keys --content-on-error -q -O -)
 
 while read -r key; do
-  echo "command=\"/bin/bash /home/api/retrieve-token.sh '"$key"'\" $key"
+  echo "command=\"/bin/bash ~$user/retrieve-token.sh '"$key"'\" $key"
 done <<< "$keys"
