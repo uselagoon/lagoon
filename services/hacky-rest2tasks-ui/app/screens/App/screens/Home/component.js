f77c83e7cbafed75b7a7161534a94fb5d17d8cb3
Schnitzel <michael@amazee.com>
2017-06-27 18:06:57 -0500

    update hacky ui: also allow removal

--- services/hacky-rest2tasks-ui/app/screens/App/screens/Home/component.js
+++ services/hacky-rest2tasks-ui/app/screens/App/screens/Home/component.js
@@ -4,12 +4,14 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Title from 'Title';
 import DeployForm from 'DeployForm';
+import RemoveForm from 'RemoveForm';
 
 const Home = (): React.Element<any> => (
   <div>
     <Helmet title="laggon Hacky UI" />
-    <Title>laggon Hacky UI</Title>
+    <Title>lagoon Hacky UI</Title>
     <DeployForm />
+    <RemoveForm />
   </div>
 );
 
