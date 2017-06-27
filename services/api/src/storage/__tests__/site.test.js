f3f252a671e4a278a60d9ea86243b25342a58a56
Karl Horky <karl.horky@gmail.com>
2017-06-27 19:23:20 +0200

    Make site storage functional

---
+++ services/api/src/storage/__tests__/site.test.js
@@ -0,0 +1,23 @@
+// @flow
+
+const { getServerInfoFromFilename } = require('../../storage/site');
+
+describe('getServerInfoFromFilename', () => {
+  test('should return server information', () => {
+    const serverInfo = getServerInfoFromFilename('compact/deploytest1.yaml');
+    expect(
+      serverInfo,
+    ).toEqual({ serverInfrastructure: 'compact', serverIdentifier: 'deploytest1' });
+  });
+
+  test('should not break on deeper directory hierarchies', () => {
+    const serverInfo = getServerInfoFromFilename('deep/deeper/compact/deploytest1.yaml');
+    expect(
+      serverInfo,
+    ).toEqual({ serverInfrastructure: 'compact', serverIdentifier: 'deploytest1' });
+  });
+  test('should return null with an invalid path', () => {
+    const serverInfo = getServerInfoFromFilename('invalid/path');
+    expect(serverInfo).toEqual(null);
+  });
+});
--- services/api/src/storage/site.js
+++ services/api/src/storage/site.js
@@ -1,55 +1,125 @@
 // @flow
 
-/* eslint-disable */
-
+const path = require('path');
+const R = require('ramda');
 const Yaml = require('js-yaml');
 // const { readFile } = require('../util/fs');
 const { listYamlFiles, readYamlFile } = require('.');
 
+import type { Site } from '../types';
+
+async function getSiteYamlFromFile(path: string): Promise<?Site> {
+  const yaml = await readYamlFile(path);
+  // TODO: Validate this with a schema?
+  return R.prop('drupalsites')(yaml);
+}
+
+function getServerInfoFromFilename(
+  fileName: string,
+): ?{ fileName: string, serverInfrastructure: string, serverIdentifier: string } {
+  const matches = fileName.match(/([^/]+)\/([^/.]+)\.[^/.]+$/);
+  return matches
+    ? { fileName, serverInfrastructure: matches[1], serverIdentifier: matches[2] }
+    : null;
+}
+
+function addSiteHost(site) {
+  const siteHost = R.propOr(
+    `${site.serverIdentifier}.${site.serverInfrastructure}`,
+    'amazeeio::servername',
+  )(site.yaml);
+  return { ...site, siteHost };
+}
+
+function addServerNames(site) {
+  let serverNames;
+  const CLUSTER_MEMBER_KEY = 'drupalhosting::profiles::nginx_backend::cluster_member';
+  if (site.serverInfrastructure === 'cluster' && site.yaml[CLUSTER_MEMBER_KEY]) {
+    serverNames = Object
+      .keys(site.yaml[CLUSTER_MEMBER_KEY])
+      .map(memberKey => `${memberKey}.${site.siteHost}`);
+  } else if (site.serverInfrastructure === 'single') {
+    serverNames = [`backend.${site.siteHost}`];
+  } else {
+    serverNames = site.siteHost instanceof Array ? site.siteHost : [site.siteHost];
+  }
+  return { ...site, serverNames };
+}
+
+function maybeAddJumphostKey(site) {
+  return { ...site, jumpHost: R.prop('amazeeio::jumphost') };
+}
+
 // export const readSiteFiles = (repoPath: string):
-export const getAllSites = async (repoPath: string) => {
+const getAllSites = async (repoPath: string) => {
   const sites = [];
 
   // const yamlFiles = await listYamlFiles(repoPath);
   // console.log(yamlFiles);
   // return;
+  const yamlFiles = await listYamlFiles(repoPath);
+  // yamlFiles.map()
+  // const { serverInfrastructure: string, serverIdentifier: string } = getServerInfoFromFilename(yamlFiles),
+  //
+  const allSites = R.compose(
+    // reduce down to a single array of all sites
+    async allYaml => R.reduce(metaObj => R.compose(
+      // set extra (meta) values
+      R.map(maybeAddJumphostKey),
+      R.map(addServerNames),
+      R.map(addSiteHost),
+      // R.map((site) => {siteName: Object.entries),
+      // R.apply(Promise.all),
+      // // get yaml from file
+      // R.map(async file => ({ ...file, yaml: await getSiteYamlFromFile(file.fileName) })),
+      // R.map(getServerInfoFromFilename),
+      R.map(site => ({
+        ...site,
+        id: `${site.serverIdentifier}.${site.serverInfrastructure}/${site.siteName}`,
+      })),
+    )(metaObj), {})(await allYaml),
+    R.apply(Promise.all),
+    // get yaml from file
+    R.map(async file => ({ ...file, yaml: await getSiteYamlFromFile(file.fileName) })),
+    R.map(getServerInfoFromFilename),
+  )(yamlFiles);
+
+  yamlFiles.map(() => {
+  });
+
   for (const fileName of await listYamlFiles(repoPath)) {
     // Extract infrastructure and identifier from file name.
-    const [ serverInfrastructure, serverIdentifier ] = fileName
+    const [serverInfrastructure, serverIdentifier] = fileName
       .substr(0, fileName.lastIndexOf('.'))
       .split('/');
 
     const yaml = await readYamlFile(`${repoPath}${fileName}`, 'utf8');
     console.log('filename!', fileName);
+    //
     if (yaml.hasOwnProperty('drupalsites')) {
-      const serverNameOverwriteKey = 'amazeeio::servername';
-
-      const siteHost = yaml.hasOwnProperty(serverNameOverwriteKey)
-        ? yaml[serverNameOverwriteKey]
-        : `${serverIdentifier}.${serverInfrastructure}`;
-
-      let serverNames;
-      let jumpHost;
+      // const serverNameOverwriteKey = 'amazeeio::servername';
+      //
+      // const siteHost = yaml.hasOwnProperty(serverNameOverwriteKey)
+      //   ? yaml[serverNameOverwriteKey]
+      //   : `${serverIdentifier}.${serverInfrastructure}`;
+      // let serverNames;
+      // let jumpHost;
       // @todo Is it correct to hard-code this?
-      const clusterMemberKey = 'drupalhosting::profiles::nginx_backend::cluster_member';
-
-      const jumphostKey = 'amazeeio::jumphost';
-
-      if (serverInfrastructure === 'cluster' && yaml.hasOwnProperty(clusterMemberKey)) {
-        serverNames = Object.keys(yaml[clusterMemberKey]).map(key => `${key}.${siteHost}`);
-      } else if (serverInfrastructure === 'single') {
-        serverNames = [ `backend.${siteHost}` ];
-      } else {
-        serverNames = siteHost instanceof Array ? siteHost : [ siteHost ];
-      }
-
-      if (yaml.hasOwnProperty(jumphostKey)) {
-        jumpHost = yaml[jumphostKey];
-      } else {
-        jumpHost = null;
-      }
-
-      Object.keys(yaml.drupalsites).forEach(siteName => {
+      // const clusterMemberKey = 'drupalhosting::profiles::nginx_backend::cluster_member';
+      // const jumphostKey = 'amazeeio::jumphost';
+      // if (serverInfrastructure === 'cluster' && yaml.hasOwnProperty(clusterMemberKey)) {
+      //   serverNames = Object.keys(yaml[clusterMemberKey]).map(key => `${key}.${siteHost}`);
+      // } else if (serverInfrastructure === 'single') {
+      //   serverNames = [`backend.${siteHost}`];
+      // } else {
+      //   serverNames = siteHost instanceof Array ? siteHost : [siteHost];
+      // }
+      // if (yaml.hasOwnProperty(jumphostKey)) {
+      //   jumpHost = yaml[jumphostKey];
+      // } else {
+      //   jumpHost = null;
+      // }
+      Object.keys(yaml.drupalsites).forEach((siteName) => {
         if (!yaml.drupalsites.hasOwnProperty(siteName)) {
           return;
         }
@@ -71,6 +141,8 @@ export const getAllSites = async (repoPath: string) => {
   }
   return sites;
 };
+
+module.exports = { getServerInfoFromFilename, getAllSites };
 // export
 // const path = require('path');
 // const R = require('ramda');
