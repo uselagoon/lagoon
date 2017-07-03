2e9bbc73879530868884829541e404846b90dbe8
Sebastian Siemssen <fubhy@fubhy.com>
2017-07-03 12:09:18 +0200

    Adding comments.

--- initHiera.sh
+++ initHiera.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Clean up, just to be sure.
+rm -rf hiera
+
+# Clone all submodules (should just be hiera for now).
 git submodule update --init
 
+# Loop over all remote branches and make them avaiable locally.
 for branch in $(git -C hiera branch --all | grep '^\s*remotes' | egrep --invert-match '(:?HEAD|master)$'); do
   git -C hiera branch --track "${branch##*/}" "$branch"
 done
