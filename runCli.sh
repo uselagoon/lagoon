0ba30c1d813a1ca0405ab22b2561bedaf26672c5
Karl Horky <karl.horky@gmail.com>
2017-07-03 16:59:27 +0200

    Fix login for Docker for Mac

--- runCli.sh
+++ runCli.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-function container () {
-  format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}"
-  command=`docker ps | grep ${1} | sed 's/\|/ /' | awk '{print $1}'`
-  docker inspect --format "$format" $command
-}
+# TODO: @fubhy wants to get this working for Docker for Mac
+# function container () {
+#   format="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}"
+#   command=`docker ps | grep ${1} | sed 's/\|/ /' | awk '{print $1}'`
+#   docker inspect --format "$format" $command
+# }
+#
+# api=$(container "api")
+# ssh=$(container "auth-ssh")
+#
+# export SSH_AUTH_HOST=$ssh
+# export SSH_AUTH_PORT="2020"
+# export SSH_AUTH_USER="api"
+# export API_URL="http://$api/graphql"
 
-api=$(container "api")
-ssh=$(container "auth-ssh")
-
-export SSH_AUTH_HOST=$ssh
+export SSH_AUTH_HOST=localhost
 export SSH_AUTH_PORT="2020"
 export SSH_AUTH_USER="api"
-export API_URL="http://$api/graphql"
+export API_URL="http://localhost:8080/graphql"
 
 cd cli && yarn run execute "$@"
