f4bde217ea8ebd7d03819f2bbd8d416be6b41b3f
Karl Horky <karl.horky@gmail.com>
2017-07-04 14:48:33 +0200

    Fix linting error

--- cli/src/gql.js
+++ cli/src/gql.js
@@ -4,10 +4,10 @@ export default function gql(literals: string[], ...substitutions: any[]) {
   let result = '';
 
   // run the loop only for the substitution count
-  for (let i = 0; i < substitutions.length; i++) {
-    result += literals[i];
-    result += substitutions[i];
-  }
+  substitutions.forEach((substitution, key) => {
+    result += literals[key];
+    result += substitution;
+  });
 
   // add the last literal
   result += literals[literals.length - 1];
