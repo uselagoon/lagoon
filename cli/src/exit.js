e76f805c1c16d17329f36ea1e2227b3611b0ab73
Karl Horky <karl.horky@gmail.com>
2017-07-11 12:59:47 +0200

    Prettier

--- cli/src/exit.js
+++ cli/src/exit.js
@@ -11,11 +11,7 @@ export function exitNoConfig(clog: Clog): number {
   return 1;
 }
 
-export function exitError(
-  clog: Clog,
-  message: string,
-  code?: number = 1,
-): number {
+export function exitError(clog: Clog, message: string, code?: number = 1): number {
   clog(message);
   return code;
 }
