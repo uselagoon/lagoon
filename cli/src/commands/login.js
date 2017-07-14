459e4b59efc066ec6744e802bc19fc837cc3368c
Karl Horky <karl.horky@gmail.com>
2017-07-14 17:26:19 +0200

    Add -i identity option to pass in private key path

--- cli/src/commands/login.js
+++ cli/src/commands/login.js
@@ -4,10 +4,12 @@ import os from 'os';
 import path from 'path';
 import { green } from 'chalk';
 import inquirer from 'inquirer';
+import R from 'ramda';
 import { utils } from 'ssh2-streams';
 import untildify from 'untildify';
 import { sshConnect, sshExec } from '../util/ssh';
-import { readFile, writeFile } from '../util/fs';
+import { fileExists, readFile, writeFile } from '../util/fs';
+import { printErrors } from '../printErrors';
 
 import typeof Yargs from 'yargs';
 import type { BaseArgs } from './index';
@@ -16,35 +18,114 @@ const name = 'login';
 const description = 'Authenticate with the amazee.io API via given SSH key';
 
 export async function setup(yargs: Yargs): Promise<Object> {
-  return yargs.usage(`$0 ${name} - ${description}`).argv;
+  return yargs.usage(`$0 ${name} - ${description}`).options({
+    identity: {
+      describe: 'Path to identity (private key)',
+      type: 'string',
+      alias: 'i',
+    },
+  }).argv;
 }
 
-type Args = BaseArgs;
+type GetPrivateKeyPathArgs = {
+  fileExistsAtDefaultPath: boolean,
+  defaultPrivateKeyPath: string,
+  identityOption?: string,
+  cerr: typeof console.error,
+};
 
-export async function run({ clog }: Args): Promise<number> {
-  const homeDir = os.homedir();
+const getPrivateKeyPath = async (
+  args: GetPrivateKeyPathArgs,
+): Promise<string> =>
+  R.cond([
+    // If a file exists at the default private key path, use that
+    [R.prop('fileExistsAtDefaultPath'), R.prop('defaultPrivateKeyPath')],
+    // If the identity option for the command has been specified, use the value of that
+    [
+      R.propSatisfies(
+        // Option is not null or undefined and a file exists at that path
+        async identityOption =>
+          R.complement(R.isNil)(identityOption) && fileExists(identityOption),
+        'identityOption',
+      ),
+      R.prop('identityOption'),
+    ],
+    // If none of the previous conditions have been satisfied, ask the user if they want to overwrite the file
+    [
+      R.T,
+      async ({ cerr }) => {
+        // TODO: Ramdaify / FPify?
+        let privateKeyPath = '';
+        let privateKeyExists = false;
+        while (!privateKeyPath || !privateKeyExists) {
+          // eslint-disable-next-line no-await-in-loop
+          const promptReturn = await inquirer.prompt([
+            {
+              type: 'input',
+              name: 'privateKeyPath',
+              message: 'Path to private key file',
+            },
+          ]);
+          privateKeyPath = promptReturn.privateKeyPath;
 
-  const { privateKeyFilePath } = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'privateKeyFilePath',
-      message: 'Path to private key file',
-      default: path.join(homeDir, '.ssh', 'id_rsa'),
-    },
-  ]);
-  const privateKey = await readFile(untildify(privateKeyFilePath));
-  let passphrase = '';
-  if (utils.parseKey(privateKey).encryption) {
-    const promptReturn = await inquirer.prompt([
-      {
-        type: 'password',
-        name: 'passphrase',
-        message: 'Private key password (never saved)',
-        default: '',
+          // eslint-disable-next-line no-await-in-loop
+          privateKeyExists = await fileExists(privateKeyPath);
+          if (!privateKeyExists) {
+            printErrors(cerr, 'File does not exist at given path!');
+          }
+        }
+        return privateKeyPath;
       },
-    ]);
-    passphrase = promptReturn.passphrase;
-  }
+    ],
+  ])(args);
+
+const getPrivateKeyPassphrase = async (
+  privateKeyHasEncryption: boolean,
+): Promise<string> =>
+  R.ifElse(
+    // If the private key doesn't have encryption...
+    R.not,
+    // ... return an empty string
+    R.always(''),
+    // If the private key has encryption, ask for the password
+    async () => {
+      const { passphrase } = await inquirer.prompt([
+        {
+          type: 'password',
+          name: 'passphrase',
+          message: 'Private key password (never saved)',
+          default: '',
+        },
+      ]);
+      return passphrase;
+    },
+  )(privateKeyHasEncryption);
+
+type Args = BaseArgs & {
+  identity: string,
+};
+
+export async function run({
+  clog,
+  cerr,
+  identity: identityOption,
+}: Args): Promise<number> {
+  const homeDir = os.homedir();
+  const defaultPrivateKeyPath = path.join(homeDir, '.ssh', 'id_rsa');
+  const fileExistsAtDefaultPath = await fileExists(defaultPrivateKeyPath);
+
+  const privateKeyPath = await getPrivateKeyPath({
+    fileExistsAtDefaultPath,
+    defaultPrivateKeyPath,
+    identityOption,
+    cerr,
+  });
+
+  const privateKey = await readFile(untildify(privateKeyPath));
+  const passphrase = await getPrivateKeyPassphrase(
+    utils.parseKey(privateKey).encryption,
+  );
+
   const connection = await sshConnect({
     host: process.env.SSH_AUTH_HOST || 'auth.amazee.io',
     port: Number(process.env.SSH_AUTH_PORT) || 2020,
