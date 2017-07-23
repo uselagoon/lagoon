c338b8ffeeaaffa976a7fb5357dab2d08c289ada
Schnitzel <michael@amazee.com>
2017-07-23 19:47:35 +0200

    pushing up to 4 times, as 2 times was still not enough

--- docker-executors/docker-oc-build-deploy/exec-push.sh
+++ docker-executors/docker-oc-build-deploy/exec-push.sh
@@ -4,5 +4,5 @@ set -o pipefail
 
 docker tag ${IMAGE}-$SERVICE ${OPENSHIFT_REGISTRY}/${OPENSHIFT_PROJECT}/${SERVICE}:latest
 
-for i in {1..2}; do docker push ${OPENSHIFT_REGISTRY}/${OPENSHIFT_PROJECT}/${SERVICE}:latest && break || sleep 5; done
+for i in {1..4}; do docker push ${OPENSHIFT_REGISTRY}/${OPENSHIFT_PROJECT}/${SERVICE}:latest && break || sleep 5; done
 
