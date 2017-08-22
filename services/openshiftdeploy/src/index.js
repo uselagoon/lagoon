5f8631cdc9118932ea4d9276398249601c6d5cd5
Schnitzel <michael@amazee.com>
2017-08-22 15:34:02 -0500

    choosing ocBuildDeployImageName based on the new structure

--- services/openshiftdeploy/src/index.js
+++ services/openshiftdeploy/src/index.js
@@ -15,7 +15,6 @@ const amazeeioapihost = process.env.AMAZEEIO_API_HOST || "http://api:3000"
 
 const ocBuildDeployImageLocation = process.env.OC_BUILD_DEPLOY_IMAGE_LOCATION || "dockerhub"
 const dockerRunParam = process.env.DOCKER_RUN_PARARM || ""
-const ocBuildDeployBranch = process.env.AMAZEEIO_GIT_BRANCH || "master"
 const ciOverrideImageRepo = process.env.CI_OVERRIDE_IMAGE_REPO || ""
 
 const amazeeioAPI = new Lokka({
@@ -102,7 +101,11 @@ const messageConsumer = async msg => {
   let ocBuildDeploystage
   let ocBuildDeployImageName
   if (ocBuildDeployImageLocation == "dockerhub") {
-    ocBuildDeployImageName = `amazeeio/oc-build-deploy:${ocBuildDeployBranch}`
+    if (process.env.AMAZEEIO_GIT_SAFE_BRANCH == "master") {
+      ocBuildDeployImageName = 'amazeeio/oc-build-deploy:latest';
+    } else {
+      ocBuildDeployImageName = `amazeeiodev/oc-build-deploy:latest-${process.env.AMAZEEIO_GIT_SAFE_BRANCH}`;
+    }
     ocBuildDeploystage =
     `
       stage ('oc-build-deploy docker pull') {
