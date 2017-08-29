4d75c93398f5504a6fbcfec41fdb98becc8596f7
Karl Horky <karl.horky@gmail.com>
2017-08-29 18:07:48 +0200

    Set identity option to the path of the private key

--- runCli.sh
+++ runCli.sh
@@ -5,4 +5,10 @@ export SSH_AUTH_PORT="2020"
 export SSH_AUTH_USER="api"
 export API_URL="http://localhost:3000/graphql"
 
-cd "$(dirname "${BASH_SOURCE[0]}")/cli" && yarn run execute "$@"
+# Set the identity option to the local private key for the `login` and `logout`
+# commands unless the option is already set.
+if [[ $@ =~ login|logout && ! $@ =~ ^.*(--identity\ |-i\ ).*$ ]]; then
+  ARGS="--identity $(dirname "${BASH_SOURCE[0]}")/local/id_rsa"
+fi
+
+cd "$(dirname "${BASH_SOURCE[0]}")/cli" && yarn run execute "$@" $ARGS
