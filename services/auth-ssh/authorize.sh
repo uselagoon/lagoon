2ce1ac3b346053c3b01b1988cb228a15eddd84bc
Patrick Stapfer <ryyppy@users.noreply.github.com>
2017-08-29 09:11:28 +0200

    Amazeeio 945 auth ssh fingerprint (#58)
    * Change api/keys to accept sshKey fingerprint
    * Add fingerprint feature to auth-ssh

--- services/auth-ssh/authorize.sh
+++ services/auth-ssh/authorize.sh
@@ -15,38 +15,16 @@ source /authorize.env
 bearer="Authorization: bearer $SERVICE_ADMIN_TOKEN"
 
 api=$AMAZEEIO_API_HOST
+fingerprint=$1
 
-keys=$(wget --header "$bearer" $api/keys --content-on-error -q -O -)
+data="{\"fingerprint\": \"$fingerprint\"}"
+keys=$(wget --header "Content-Type: application/json" --header "$bearer" $api/keys --post-data "$data" --content-on-error -q -O -)
 
 options="no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty"
 command="/bin/bash /home/get-jwt-token.sh"
 
-##### START ACTUAL IMPLEMENTATION
-
-# TODO: This is a super dirty hack. Directly printing the output into
-# stdout, however, doesn't work. I have no fucking clue and this is
-# super weird.
-#
-# Even with the solution below (the hack) we get random login failures
-# here and there. Someone with better bash and debugging skills than me
-# needs to take a look.
-#
-# The following code does not work:
-
-# while read -r key; do
-#   printf "$key\n"
-# done <<< "$keys"
-
-##### END ACTUAL IMPLEMENTATION
-##### START HACKY TEMPORARY SOLUTION
-
-# Empty the keys file or create it.
-:> /home/keys
-
-while read -r key; do
-  printf '%s\n' "$options,command=\"$command '$key'\" $key" >> /home/keys
-done <<< "$keys"
-
-cat /home/keys
-
-##### END HACKY TEMPORARY SOLUTION
+if [ -n "$keys" ]; then
+    while read -r key; do
+        printf '%s\n' "$options,command=\"$command '$key'\" $key"
+    done <<< "$keys"
+fi
