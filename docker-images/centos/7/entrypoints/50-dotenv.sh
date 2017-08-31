34da257271052fb772ba3b01fe629f06598de9f3
Schnitzel <michael@amazee.com>
2017-08-31 10:13:44 +0200

    creating an actual tempfile instead of using the same file

--- docker-images/centos/7/entrypoints/50-dotenv.sh
+++ docker-images/centos/7/entrypoints/50-dotenv.sh
@@ -20,7 +20,8 @@
 # first export all current environment variables into a file.
 # We do that in order to keep the hierarchy of environment variables. Already defined ones are probably overwritten
 # via an `docker run -e VAR=VAL` system and they should still be used even they are defined in dotenv files.
-export -p > /tmp/dotenv-exiting
+TMPFILE=$(mktemp --tmpdir dotenv.XXXXXXXX)
+export -p > $TMPFILE
 
 # set -a is short for `set -o allexport` which will export all variables in a file
 set -a
@@ -32,4 +33,7 @@ set -a
 set +a
 
 # now export all previously existing environments variables so they are stronger than maybe existing ones in the dotenv files
-. /tmp/dotenv-exiting
+. $TMPFILE
+
+# remove the tmpfile
+rm $TMPFILE
