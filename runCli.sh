5d3635ff804e3d5a44498fcbf07a6a8a668e604a
Karl Horky <karl.horky@gmail.com>
2017-11-28 17:01:25 +0100

    Don't forward options with "--" for Yarn 1.0+
    Error message:
    warning From Yarn 1.0 onwards, scripts don't require "--" for options to be forwarded. In a future version, any explicit "--" will be forwarded as-is to the scripts.

--- runCli.sh
+++ runCli.sh
@@ -5,11 +5,29 @@ export SSH_AUTH_PORT="2020"
 export SSH_AUTH_USER="api"
 export API_URL="http://localhost:3000/graphql"
 
+# Test if the first version number provided is higher than the second one
+# > version_gt 2 1
+# > echo $?
+# 0
+# > version_gt 1.2.2 1
+# > echo $?
+# 0
+# > version_gt 1 2
+# > echo $?
+# 1
+# > version_gt 1 1
+# > echo $?
+# 1
+function version_gt() {
+  test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
+}
+
 # Set the identity option to the local private key for the `login` and `logout`
 # commands unless the option is already set.
 if [[ $@ =~ login|logout && ! $@ =~ ^.*(--identity\ |-i\ ).*$ ]]; then
   ARGS="--identity $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/local-dev/cli_id_rsa"
-  if [[ $@ =~ ^[^-][^-] ]]; then
+
+  if version_gt "$(yarn -v)" 2 && [[ $@ =~ ^[^-][^-] ]]; then
     PREFIX="--"
   fi
 fi
