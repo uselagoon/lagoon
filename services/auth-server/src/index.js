dcb8cd38f8fdb9a92b5b14c2a6e5f74222bda43e
Karl Horky <karl.horky@gmail.com>
2017-11-30 12:52:50 +0100

    Add back auth-server formatting dev deps & format

--- services/auth-server/src/index.js
+++ services/auth-server/src/index.js
@@ -10,13 +10,11 @@ import type { $Request, $Response, NextFunction } from 'express';
 const app = express();
 
 // Add custom configured logger (morgan through winston).
-app.use(
-  morgan('combined', {
-    stream: {
-      write: message => logger.info(message),
-    },
-  })
-);
+app.use(morgan('combined', {
+  stream: {
+    write: message => logger.info(message),
+  },
+}));
 
 const port = process.env.PORT || 3000;
 const jwtSecret = process.env.JWTSECRET || '';
@@ -26,17 +24,15 @@ const audience = process.env.JWTAUDIENCE || 'api.amazee.io';
 app.post('/generate', ...generateRoute({ jwtSecret, issuer, audience }));
 
 export interface ErrorWithStatus extends Error {
-  status: number,
+  status: number;
 }
 
 // $FlowIgnore
-app.use(
-  (err: ErrorWithStatus, req: $Request, res: $Response, next: NextFunction) => {
-    logger.error(err.toString());
-    res.status(err.status || 500);
-    res.send(`Request failed: ${err.toString()}`);
-  }
-);
+app.use((err: ErrorWithStatus, req: $Request, res: $Response, next: NextFunction) => {
+  logger.error(err.toString());
+  res.status(err.status || 500);
+  res.send(`Request failed: ${err.toString()}`);
+});
 
 app.listen(port, () => {
   logger.debug(`Server listening on port ${port}.`);
