8d9735b2997d01408dce849e9aa255e317a3331c
Elliot Schlegelmilch <elliot@amazee.io>
2017-12-11 13:10:55 -0800

    remove unused jwt-token.

--- services/rsh-console/docker-entrypoint.sh
+++ services/rsh-console/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ep /home/get-jwt-token.sh
-
 # filling /authorize.env with all our current env variables, this file
 # will be sourced by /authorize.sh in order to have all environment variables.
 # We can't use `ep /authorize.sh` as we are not running as root, but openssh
