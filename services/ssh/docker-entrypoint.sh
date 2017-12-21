69416919b07b2e6c401ae53ee610c1c1947cb788
Schnitzel <michael@amazee.com>
2017-12-21 11:15:00 +0100

    load lagoon token

--- services/ssh/docker-entrypoint.sh
+++ services/ssh/docker-entrypoint.sh
@@ -18,9 +18,8 @@ export >> /authorize.env
 
 set -x
 
-if [ -f /var/run/secrets/kubernetes.io/serviceaccount/token ]; then
-  TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-  /usr/bin/oc login --insecure-skip-tls-verify --token="${TOKEN}" ${OPENSHIFT_CONSOLE_URL:-https://kubernetes.default.svc}
+if [ ! -z ${LAGOON_SERVICEACCOUNT_TOKEN+x} ]; then
+  /usr/bin/oc login --insecure-skip-tls-verify --token="${LAGOON_SERVICEACCOUNT_TOKEN}" ${OPENSHIFT_CONSOLE_URL:-https://kubernetes.default.svc}
 else
   # this will be used only for local development
   /usr/bin/oc login --insecure-skip-tls-verify -u developer -p developer ${OPENSHIFT_CONSOLE_URL:-https://kubernetes.default.svc}
