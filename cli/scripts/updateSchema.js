96084597784d9b3c5d492fe06e2544e78c3177f1
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-01-04 16:57:58 +0100

    Add bearer auth to updateSchema script
    Also updates the GraphQL schema to the current API schema.

--- cli/scripts/updateSchema.js
+++ cli/scripts/updateSchema.js
@@ -1,28 +1,59 @@
-// @flow
+const path = require("path");
+const fetch = require("node-fetch");
+const fs = require("fs");
 
-const fetch = require('node-fetch');
-const fs = require('fs');
+// Let's use the default lagoon environment variables for creating a JWT
+require("dotenv-extended").load({
+  defaults: path.join(__dirname, "..", "..", ".env.defaults")
+});
+
+const { createJWTWithoutSshKey } = require("@lagoon/commons/src/jwt");
+
 const {
   buildClientSchema,
   introspectionQuery,
-  printSchema,
-} = require('graphql/utilities');
-const path = require('path');
+  printSchema
+} = require("graphql/utilities");
 
-const schemaDir = path.join(__dirname, '..', 'data');
-const schemaPath = path.join(schemaDir, 'schema');
+const jwtSecret = process.env.JWTSECRET || "";
+const issuer = process.env.JWTISSUER || "auth.amazee.io";
+const audience = process.env.JWTAUDIENCE || "api.amazee.io";
 
-const SERVER = 'http://api-develop-testhiera.appuio.amazeeio.review/graphql';
+const payload = {
+  sub: "updateSchemaScript",
+  aud: audience,
+  iss: issuer,
+  role: "admin"
+};
+
+const jwt = createJWTWithoutSshKey({
+  payload,
+  jwtSecret
+});
+
+const schemaDir = path.join(__dirname, "..", "data");
+const schemaPath = path.join(schemaDir, "schema");
+
+const SERVER = "http://localhost:3000/graphql";
 
 // Save JSON of full schema introspection for Babel Relay Plugin to use
 fetch(`${SERVER}`, {
-  method: 'POST',
+  method: "POST",
   headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${jwt}`
   },
-  body: JSON.stringify({ query: introspectionQuery }),
+  body: JSON.stringify({ query: introspectionQuery })
 })
+  .then(res => {
+    if (res.status !== 200) {
+      console.error(res.statusText);
+      console.error(res.status);
+    } else {
+      return res;
+    }
+  })
   .then(res => res.json())
   .then(schemaJSON => {
     fs.writeFileSync(`${schemaPath}.json`, JSON.stringify(schemaJSON, null, 2));
@@ -30,4 +61,7 @@ fetch(`${SERVER}`, {
     // Save user readable type system shorthand of schema
     const graphQLSchema = buildClientSchema(schemaJSON.data);
     fs.writeFileSync(`${schemaPath}.graphql`, printSchema(graphQLSchema));
+  })
+  .catch(err => {
+    console.error(err);
   });
