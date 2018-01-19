2660d33bdeb64f4cd1257cfb7670c05c2af6802b
Karl Horky <karl.horky@gmail.com>
2018-01-19 18:20:01 +0100

    Remove unnecessary table config, show all values

--- cli/src/commands/project.js
+++ cli/src/commands/project.js
@@ -16,25 +16,6 @@ import {
 import typeof Yargs from 'yargs';
 import type { BaseArgs } from '.';
 
-const tableConfig = {
-  columns: {
-    // $FlowIssue: Flow doesn't understand numbers as keys https://github.com/facebook/flow/issues/380
-    0: {
-      alignment: 'left',
-      minWidth: 15,
-    },
-    // $FlowIssue: Flow doesn't understand numbers as keys https://github.com/facebook/flow/issues/380
-    1: {
-      alignment: 'left',
-      minWidth: 15,
-    },
-  },
-};
-
-// Common filter
-const onlyValues = ([, value]: [string, string]) =>
-  value != null && value !== '';
-
 export const command = 'project';
 export const description = 'Show project details';
 
@@ -106,7 +87,8 @@ projectDetailsArgs): Promise<number> {
     return printErrors(clog, `No project '${projectName}' found!`);
   }
 
-  const tableBody = [
+  clog(`Project details for '${projectName}':`);
+  clog(table([
     ['Project Name', R.prop('name', project)],
     ['Customer', R.path(['customer', 'name'], project)],
     ['Git URL', R.prop('git_url', project)],
@@ -116,12 +98,7 @@ projectDetailsArgs): Promise<number> {
     ['Pull Requests', String(R.prop('pullrequests', project))],
     ['Openshift', R.path(['openshift', 'name'], project)],
     ['Created', R.path(['created'], project)],
-  ];
-
-  const tableData = R.filter(onlyValues)(tableBody);
-
-  clog(`Project details for '${projectName}':`);
-  clog(table(tableData, tableConfig));
+  ]));
 
   return 0;
 }
