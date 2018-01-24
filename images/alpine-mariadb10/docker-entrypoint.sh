eb5f743267e805f2a294ce4dd32128731e6887e8
Elliot Schlegelmilch <elliot@amazee.io>
2018-01-24 08:12:05 -0800

    s/MYSQL/MARIADB/

--- images/alpine-mariadb10/docker-entrypoint.sh
+++ images/alpine-mariadb10/docker-entrypoint.sh
@@ -17,14 +17,14 @@ else
 
 	mysql_install_db --skip-name-resolve
 
-	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-		MYSQL_ROOT_PASSWORD=`pwgen 16 1`
-		echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
+	if [ "$MARIADB_ROOT_PASSWORD" = "" ]; then
+		MARIADB_ROOT_PASSWORD=`pwgen 16 1`
+		echo "[i] MySQL root Password: $MARIADB_ROOT_PASSWORD"
 	fi
 
-	MYSQL_DATABASE=${MYSQL_DATABASE:-""}
-	MYSQL_USER=${MYSQL_USER:-""}
-	MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
+	MARIADB_DATABASE=${MARIADB_DATABASE:-""}
+	MARIADB_USER=${MARIADB_USER:-""}
+	MARIADB_PASSWORD=${MARIADB_PASSWORD:-""}
 
 	tfile=`mktemp`
 	if [ ! -f "$tfile" ]; then
@@ -34,17 +34,17 @@ else
 	cat << EOF > $tfile
 USE mysql;
 
-UPDATE user SET PASSWORD=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user="root";
+UPDATE user SET PASSWORD=PASSWORD("$MARIADB_ROOT_PASSWORD") WHERE user="root";
 FLUSH PRIVILEGES;
 
 EOF
 
-	if [ "$MYSQL_DATABASE" != "" ]; then
-	  echo "[i] Creating database: $MYSQL_DATABASE"
-	  echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
-	  if [ "$MYSQL_USER" != "" ]; then
-	    echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-		  echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+	if [ "$MARIADB_DATABASE" != "" ]; then
+	  echo "[i] Creating database: $MARIADB_DATABASE"
+	  echo "CREATE DATABASE IF NOT EXISTS \`$MARIADB_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+	  if [ "$MARIADB_USER" != "" ]; then
+	    echo "[i] Creating user: $MARIADB_USER with password $MARIADB_PASSWORD"
+		  echo "GRANT ALL ON \`$MARIADB_DATABASE\`.* to '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';" >> $tfile
 	  fi
 		echo "FLUSH PRIVILEGES;" >> $tfile
 	fi
@@ -54,7 +54,7 @@ EOF
 
 	echo "[client]" >> /var/lib/mysql/.my.cnf
 	echo "user=root" >> /var/lib/mysql/.my.cnf
-	echo "password=$MYSQL_ROOT_PASSWORD"  >> /var/lib/mysql/.my.cnf
+	echo "password=$MARIADB_ROOT_PASSWORD"  >> /var/lib/mysql/.my.cnf
 
 fi
 
