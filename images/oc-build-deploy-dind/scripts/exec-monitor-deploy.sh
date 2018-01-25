84eb092f1603f504c7e72abfb2f1f07373584e16
Bastian Widmer <b.widmer@dasrecht.net>
2018-01-25 16:25:56 +0100

    Allow SERVICE_TYPE custom for having rollouts that don't need deployment template Adding SERVICE_ROLLOUT_TYPE to disable Post-Rollout jobs (i.e. when the deployment does not require services to be rolled out)

--- images/oc-build-deploy-dind/scripts/exec-monitor-deploy.sh
+++ images/oc-build-deploy-dind/scripts/exec-monitor-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-oc rollout --insecure-skip-tls-verify -n ${OPENSHIFT_PROJECT} status dc/${SERVICE_NAME} --watch
+oc rollout --insecure-skip-tls-verify -n ${OPENSHIFT_PROJECT} status ${SERVICE_ROLLOUT_TYPE} ${SERVICE_NAME} --watch
