46fe3f3f9cfdc768f889ce5a48b413d9f59a34d0
Elliot Schlegelmilch <elliot@amazee.io>
2018-01-26 14:51:59 -0800

    update socket location.

--- services/api-db/rerun_initdb.sh
+++ services/api-db/rerun_initdb.sh
@@ -2,4 +2,4 @@
 
 INITDB_DIR="/docker-entrypoint-initdb.d"
 
-for sql_file in `ls $INITDB_DIR`; do mysql -S /tmp/mysql.sock -uapi -papi infrastructure < "$INITDB_DIR/$sql_file" ; done
+for sql_file in `ls $INITDB_DIR`; do mysql -S /run/mysqld/mysqld.sock -uapi -papi infrastructure < "$INITDB_DIR/$sql_file" ; done
