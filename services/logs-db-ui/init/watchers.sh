6ea3cf77790408d42b534d8c64875ef419d4ad6f
Tyler Ward <twardnw@gmail.com>
2018-02-14 16:29:20 -0800

    #73 - change cwd before running scripts with curl and files

--- services/logs-db-ui/init/watchers.sh
+++ services/logs-db-ui/init/watchers.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -XPUT "http://logs-db:9200/_xpack/watcher/watch/rabbitmq_connection_error" -H 'Content-Type: application/json' -d @/kibana-init/rabbitmq_connection_error.json
+curl -XPUT "http://logs-db:9200/_xpack/watcher/watch/rabbitmq_connection_error" -H 'Content-Type: application/json' -d @rabbitmq_connection_error.json
