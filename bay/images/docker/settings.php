2c280896a711a222e80218ec9e66c21ee6b8dfff
Thom Toogood <thomtoogood@gmail.com>
2018-02-15 14:39:35 +1100

    Remove solr config overrides.

--- bay/images/docker/settings.php
+++ bay/images/docker/settings.php
@@ -20,19 +20,6 @@ $databases['default']['default'] = [
   'prefix' => '',
 ];
 
-// Solr connection.
-// WARNING: you have to create a search_api server having "solr" machine name at
-// /admin/config/search/search-api/add-server to make this work.
-$config['search_api.server.solr']['backend_config']['connector_config']['host'] = getenv('SOLR_HOST') ?: 'solr';
-$config['search_api.server.solr']['backend_config']['connector_config']['path'] = '/solr/';
-$config['search_api.server.solr']['backend_config']['connector_config']['core'] = getenv('SOLR_CORE') ?: 'drupal';
-$config['search_api.server.solr']['backend_config']['connector_config']['port'] = getenv('SOLR_PORT') ?: '8983';
-$config['search_api.server.solr']['backend_config']['connector_config']['http_user'] = (getenv('SOLR_USER') ?: '');
-$config['search_api.server.solr']['backend_config']['connector_config']['http']['http_user'] = (getenv('SOLR_USER') ?: '');
-$config['search_api.server.solr']['backend_config']['connector_config']['http_pass'] = (getenv('SOLR_PASSWORD') ?: '');
-$config['search_api.server.solr']['backend_config']['connector_config']['http']['http_pass'] = (getenv('SOLR_PASSWORD') ?: '');
-$config['search_api.server.solr']['name'] = 'Lagoon Solr - Environment: ' . getenv('LAGOON_PROJECT');
-
 // Varnish & Reverse proxy settings.
 $settings['reverse_proxy'] = TRUE;
 
