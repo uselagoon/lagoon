e1c3bea607bac78490347cf1a23f8509fb1a2cf9
Karl Horky <karl.horky@gmail.com>
2018-03-22 14:08:02 +0100

    Fix / simplify Jest config for Yarn Workspaces

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  projects: [
+    // Projects having their own config
+    "node-packages/commons",
+    "services/webhook-handler",
+    // Everything else (specified in object notation to avoid Jest duplicate config error)
+    {
+      testMatch: [
+        "<rootDir>/!(node-packages/commons/|services/webhook-handler/){**/__tests__/**/*.js?(x),**/?(*.)(spec|test).js?(x)}"
+      ],
+      testPathIgnorePatterns: ["node_modules", "flow-typed"]
+    }
+  ]
+};
