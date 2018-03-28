7e0f8fc57bfffbce320d7bb896d271d3931d7bb9
Elliot Schlegelmilch <elliot@amazee.io>
2018-03-28 12:33:29 -0700

    handle skipped message as information_source.

--- services/logs2slack/src/readFromRabbitMQ.js
+++ services/logs2slack/src/readFromRabbitMQ.js
@@ -54,6 +54,7 @@ async function readFromRabbitMQ (msg: RabbitMQMsg, channelWrapperLogs: ChannelWr
     case "gitlab:push:handled":
     case "rest:deploy:receive":
     case "rest:remove:receive":
+    case "github:push:skipped":
       sendToSlack(project, message, '#E8E8E8', ':information_source:', channelWrapperLogs, msg, appId)
       break;
 
