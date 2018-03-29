6d26bb2218461514211673d39c53fea64f3915fa
Karl Horky <karl.horky@gmail.com>
2018-03-29 19:06:02 +0200

    Note where disabled ESLint config comes from

--- services/auth-server/.eslintrc.js
+++ services/auth-server/.eslintrc.js
@@ -3,9 +3,9 @@ module.exports = {
   plugins: ['flowtype'],
   env: { es6: true, jest: true, node: true },
   rules: {
-    // Disable stylistic rule
+    // Disable Airbnb stylistic rule
     camelcase: 'off',
-    // Rule to enforce function return types. We disable this because Flow will check our function return types.
+    // Rule to enforce function return types. We disable this Airbnb setting because Flow will check our function return types.
     'consistent-return': 'off',
     // Fix issue with the way Prettier formats types
     'flowtype/generic-spacing': 'off',
@@ -13,33 +13,33 @@ module.exports = {
     'flowtype/space-after-type-colon': 'off',
     // Fix issue with the way Prettier formats function calls
     'function-paren-newline': 'off',
-    // Disable stylistic rule
+    // Disable Airbnb stylistic rule
     'global-require': 'off',
-    // Code style rule to enforce import ordering. We disable this because we use absolute imports for types sometimes after relative imports.
+    // Code style rule to enforce import ordering. We disable this Airbnb setting because we use absolute imports for types sometimes after relative imports.
     'import/first': 'off',
-    // Code style rule to prefer a default export instead of a single named export, currently we disable this to allow this behavior. We can decide later to turn this on again if we want.
+    // Code style rule to prefer a default export instead of a single named export, currently we disable this Airbnb setting to allow this behavior. We can decide later to turn this on again if we want.
     'import/prefer-default-export': 'off',
     // Prettier works better with its default 80 character max-length
     'max-len': 'off',
     // Conflicts with Prettier's stripping of unnecessary parentheses
     'no-confusing-arrow': 'off',
-    // Rule to restrict usage of confusing code style with mixed boolean operators. We disable this because Prettier removes "unnecessary parentheses" here and breaks this.
+    // Rule to restrict usage of confusing code style with mixed boolean operators. We disable this Airbnb setting because Prettier removes "unnecessary parentheses" here and breaks this.
     'no-mixed-operators': 'off',
-    // Disable stylistic rule
+    // Disable Airbnb stylistic rule
     'no-multi-assign': 'off',
-    // Disable stylistic rule
+    // Disable Airbnb stylistic rule
     'no-param-reassign': 'off',
-    // Disable stylistic rule
+    // Disable Airbnb stylistic rule
     'no-plusplus': 'off',
-    // Disable stylistic rule
+    // Disable Airbnb stylistic rule
     'no-restricted-globals': 'off',
-    // Rule to prevent prefixing of underscores on variable names. We disable this because we use some underscore prefixes in our code.
+    // Rule to prevent prefixing of underscores on variable names. We disable this Airbnb setting because we use some underscore prefixes in our code.
     'no-underscore-dangle': 'off',
-    // Disable stylistic rule
+    // Disable Airbnb stylistic rule
     'no-use-before-define': 'off',
-    // Disable stylistic rule
+    // Disable Airbnb stylistic rule
     'prefer-destructuring': 'off',
-    // Disable stylistic rule
+    // Disable Airbnb stylistic rule
     radix: 'off',
   },
 };
