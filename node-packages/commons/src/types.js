51ea5eb86cd13ff36d4ab99c1adda1afb7c1f799
Karl Horky <karl.horky@gmail.com>
2018-03-30 15:44:21 +0200

    Use common ESLint config in auth-server

--- node-packages/commons/src/types.js
+++ node-packages/commons/src/types.js
@@ -6,9 +6,14 @@
 
 export type ChannelWrapper = {
   sendToQueue: (queue: string, content: Buffer, options: Object) => void,
-  publish: (exchange: string, routingKey: string, content: Buffer, options: Object) => void,
+  publish: (
+    exchange: string,
+    routingKey: string,
+    content: Buffer,
+    options: Object,
+  ) => void,
   ack: (msg: Object) => void,
-}
+};
 
 export type Project = {
   slack: Object,
