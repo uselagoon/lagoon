cfc0700f9a3dbd2997f4e010e6db78f6c3568793
Karl Horky <karl.horky@gmail.com>
2018-03-30 14:28:31 +0200

    Add ESLint config from API to common config

--- node-packages/eslint-config-lagoon-node/index.js
+++ node-packages/eslint-config-lagoon-node/index.js
@@ -3,6 +3,8 @@ module.exports = {
   plugins: ['flowtype'],
   env: { es6: true, jest: true, node: true },
   rules: {
+    // Modify Airbnb stylistic rule to not apply to properties (often we use snake case in object properties because of communication with other services)
+    camelcase: ['error', { properties: 'never' }],
     // Rule to enforce function return types. We disable this Airbnb setting because Flow will check our function return types.
     'consistent-return': 'off',
     // Fix issue with the way Prettier formats types
@@ -11,6 +13,8 @@ module.exports = {
     'flowtype/space-after-type-colon': 'off',
     // Fix issue with the way Prettier formats function calls
     'function-paren-newline': 'off',
+    // Disable Airbnb stylistic rule
+    'global-require': 'off',
     // Code style rule to enforce import ordering. We disable this Airbnb setting because we use absolute imports for types sometimes after relative imports.
     'import/first': 'off',
     // Fix the erroring of dev dependencies in updateSchema script
@@ -26,9 +30,21 @@ module.exports = {
     'no-confusing-arrow': 'off',
     // Rule to restrict usage of confusing code style with mixed boolean operators. We disable this Airbnb setting because Prettier removes "unnecessary parentheses" here and breaks this.
     'no-mixed-operators': 'off',
+    // Disable Airbnb stylistic rule
+    'no-multi-assign': 'off',
+    // Disable Airbnb stylistic rule
+    'no-param-reassign': 'off',
+    // Disable Airbnb stylistic rule
+    'no-plusplus': 'off',
+    // Disable Airbnb stylistic rule
+    'no-restricted-globals': 'off',
     // Rule to prevent prefixing of underscores on variable names. We disable this Airbnb setting because we use some underscore prefixes in our code.
     'no-underscore-dangle': 'off',
-    // Disable  Airbnb stylistic rule
+    // Disable Airbnb stylistic rule
     'no-use-before-define': 'off',
+    // Disable Airbnb stylistic rule
+    'prefer-destructuring': 'off',
+    // Disable Airbnb stylistic rule
+    radix: 'off',
   },
 };
