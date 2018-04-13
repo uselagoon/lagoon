d5460255e74338c4412e7050f58b7d7af7a7c429
Karl Horky <karl.horky@gmail.com>
2018-04-13 13:54:12 +0200

    Fix Jest in CLI

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   projects: [
     // Projects having their own config
+    "cli",
     "node-packages/commons",
     "services/webhook-handler",
     // Everything else (specified in object notation to avoid Jest duplicate config error)
     {
       testMatch: [
-        "<rootDir>/!(node-packages/commons/|services/webhook-handler/){**/__tests__/**/*.js?(x),**/?(*.)(spec|test).js?(x)}"
+        "<rootDir>/!(cli|node-packages/commons/|services/webhook-handler/){**/__tests__/**/*.js?(x),**/?(*.)(spec|test).js?(x)}"
       ],
       testPathIgnorePatterns: ["node_modules", "flow-typed"]
     }
