34fc4469d656107d333341dcf03861503451babf
Schnitzel <michael@amazee.com>
2018-04-18 18:08:23 -0500

    Change root's home folder to /home it's /root by default and causes some issues

--- images/commons/lagoon/entrypoints/10-passwd.sh
+++ images/commons/lagoon/entrypoints/10-passwd.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
+if [ -w /etc/passwd ]; then
+  # Change root's home folder to /home
+  sed -i 's/root:\/root:/root:\/home:/' /etc/passwd
+
+  # If we don't know who we are (whoami returns false) add a new entry into the users list
+  if ! whoami &> /dev/null; then
     echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user}:${HOME}:/bin/sh" >> /etc/passwd
   fi
 fi
