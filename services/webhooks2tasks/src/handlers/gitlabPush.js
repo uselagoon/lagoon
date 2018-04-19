e55169c440ed159ae67bf43a7f725913a7f31686
Elliot Schlegelmilch <elliot@amazee.io>
2018-04-19 09:34:18 -0700

    simplify handler to only look at the most recent commit.

--- services/webhooks2tasks/src/handlers/gitlabPush.js
+++ services/webhooks2tasks/src/handlers/gitlabPush.js
@@ -19,10 +19,8 @@ async function gitlabPush(webhook: WebhookRequestData, project: Project) {
     const branchName = body.ref.toLowerCase().replace('refs/heads/','')
     const sha = body.after
 
-    // for each item in the body.commits array, search for our regular expression; this is an arroy of boolean
-    // which is .reduced into a single true if any are true.
-    const skip_deploy = body.commits.map( commit => Array.isArray(commit.message.match(/\[skip deploy\]|\[deploy skip\]/i)))
-                                    .reduce( function(a,i,x,z) { return a||i })
+    const skip_deploy = body.commits[0].message.match(/\[skip deploy\]|\[deploy skip\]/i)
+
     if (skip_deploy) {
       sendToLagoonLogs('info', project.name, uuid, `${webhooktype}:${event}:skipped`, meta,
         `*[${project.name}]* ${logMessage} pushed in <${body.repository.html_url}|${body.repository.full_name}> *deployment skipped*`
