c1379b820ebc0bf403a6aaef8c307d694e91c092
Elliot Schlegelmilch <elliot@amazee.io>
2018-04-23 12:06:21 -0700

    integrate unidle-targets snippets.

--- services/auto-idler/idle-services.sh
+++ services/auto-idler/idle-services.sh
@@ -83,6 +83,13 @@ echo "$DEVELOPMENT_ENVIRONMENTS" | jq -c '.data.developmentEnvironments[] | sele
             echo "$OPENSHIFT_URL - $PROJECT_NAME: $ENVIRONMENT_NAME had no hits in last hour, starting to idle"
             # actually idleing happens here
             oc --insecure-skip-tls-verify --token="$OPENSHIFT_TOKEN" --server="$OPENSHIFT_URL" -n "$ENVIRONMENT_OPENSHIFT_PROJECTNAME" idle --all
+
+            IDLING_ENDPOINTS=$(oc get endpoints -o json | jq  --raw-output '[ .items[] | select(.metadata.annotations | has("idling.alpha.openshift.io/unidle-targets")) | .metadata.name ] | join(" ")')
+            if [[ "${IDLING_ENDPOINTS}" ]]; then
+              ALL_IDLED_SERVICES_JSON=$(oc get endpoints -o json | jq --raw-output '[ .items[] | select(.metadata.annotations | has("idling.alpha.openshift.io/unidle-targets")) | .metadata.annotations."idling.alpha.openshift.io/unidle-targets" | fromjson | .[] ] | unique_by(.name) | tojson')
+              oc annotate --overwrite endpoints $IDLING_ENDPOINTS "idling.alpha.openshift.io/unidle-targets=${ALL_IDLED_SERVICES_JSON}"
+            fi
+
           fi
         done
       else
