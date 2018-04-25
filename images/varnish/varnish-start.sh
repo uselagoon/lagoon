73e88f1fc358c71d902975e5f33c44e459234384
Bastian Widmer <b.widmer@dasrecht.net>
2018-04-25 21:45:37 +0200

    fixes and moving all to sh

--- images/varnish/varnish-start.sh
+++ images/varnish/varnish-start.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 /usr/sbin/varnishd -a :8080 -T :6082 -F -f /etc/varnish/default.vcl -S /etc/varnish/secret -p http_resp_hdr_len=$HTTP_RESP_HDR_LEN -p http_resp_size=$HTTP_RESP_SIZE
