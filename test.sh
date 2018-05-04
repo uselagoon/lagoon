b1eeb7280d4549b5064f47965c68706263ea2555
Michael <michael@Michaels-MacBook-Pro.local>
2018-05-04 11:59:08 -0400

    removed unnecessary tests

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-IDLING_ENDPOINTS=$(oc get endpoints -o json | jq  --raw-output '[ .items[] | select(.metadata.annotations | has("idling.alpha.openshift.io/unidle-targets")) | .metadata.name ] | join(" ")')
-if [[ "${IDLING_ENDPOINTS}" ]]; then
-  ALL_IDLED_SERVICES_JSON=$(oc get endpoints -o json | jq --raw-output '[ .items[] | select(.metadata.annotations | has("idling.alpha.openshift.io/unidle-targets")) | .metadata.annotations."idling.alpha.openshift.io/unidle-targets" | fromjson | .[] ] | unique_by(.name) | tojson')
-  oc annotate --overwrite endpoints $IDLING_ENDPOINTS "idling.alpha.openshift.io/unidle-targets=${ALL_IDLED_SERVICES_JSON}"
-fi
