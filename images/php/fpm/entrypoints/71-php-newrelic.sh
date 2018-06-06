1fdb9ec0332ea22970a166a721cffd7e75714f0f
Schnitzel <michael@amazee.com>
2018-06-06 14:37:46 -0500

    do not automatically enable NewRelic

--- images/php/fpm/entrypoints/71-php-newrelic.sh
+++ images/php/fpm/entrypoints/71-php-newrelic.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Enable NewRelic automatically if there is a newrelic license set
-if [ ${NEWRELIC_LICENSE+x} ]; then
-  export NEWRELIC_ENABLED=true
-fi
-
 # envplate the newrelic ini file
 ep /usr/local/etc/php/conf.d/newrelic.ini
