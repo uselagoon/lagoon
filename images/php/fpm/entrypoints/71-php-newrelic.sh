8a6bc0b5849f1f3d3cdd5c5eb106e910fac9608b
Thom Toogood <thomtoogood@gmail.com>
2018-06-07 23:07:58 +1000

    Enable newrelic only if XDEBUG_ENABLE is set.

--- images/php/fpm/entrypoints/71-php-newrelic.sh
+++ images/php/fpm/entrypoints/71-php-newrelic.sh
@@ -3,5 +3,7 @@
 # envplate the newrelic ini file
 ep /usr/local/etc/php/conf.d/newrelic.disable
 
-# enable newrelic
-mv /usr/local/etc/php/conf.d/newrelic.disable /usr/local/etc/php/conf.d/newrelic.ini
+# enable newrelic only if XDEBUG_ENABLE is set
+if [ ${XDEBUG_ENABLE+x} ]; then
+  mv /usr/local/etc/php/conf.d/newrelic.disable /usr/local/etc/php/conf.d/newrelic.ini
+fi
