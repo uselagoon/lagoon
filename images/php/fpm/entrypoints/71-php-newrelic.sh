58e7af1e4b7821241ae8b51b7a749cc7625b3d0a
Michael Schmid <michael@amazee.com>
2018-06-07 19:54:11 -0500

    hotfix: xdebug != newrelic

--- images/php/fpm/entrypoints/71-php-newrelic.sh
+++ images/php/fpm/entrypoints/71-php-newrelic.sh
@@ -3,7 +3,7 @@
 # envplate the newrelic ini file
 ep /usr/local/etc/php/conf.d/newrelic.disable
 
-# enable newrelic only if XDEBUG_ENABLE is set
-if [ ${XDEBUG_ENABLE+x} ]; then
+# enable newrelic only if NEWRELIC_ENABLED is set
+if [ ${NEWRELIC_ENABLED+x} ]; then
   mv /usr/local/etc/php/conf.d/newrelic.disable /usr/local/etc/php/conf.d/newrelic.ini
 fi
