b367375d48780ce03d17a18e677bc9d7ad49440e
Karl Horky <karl.horky@gmail.com>
2018-06-20 15:05:07 +0200

    Move flow-typed to root, upgrade Flow

--- flow-typed/npm/body-parser_v1.x.x.js
+++ flow-typed/npm/body-parser_v1.x.x.js
@@ -1,13 +1,11 @@
 // flow-typed signature: f39e87ae093403a8f3c789273ccddbf6
 // flow-typed version: 7e7c66e29a/body-parser_v1.x.x/flow_>=v0.30.x
 
-import type { Middleware, $Request, $Response } from 'express';
-
 declare type bodyParser$Options = {
   inflate?: boolean;
   limit?: number | string;
-  type?: string | string[] | ((req: $Request) => any);
-  verify?: (req: $Request, res: $Response, buf: Buffer, encoding: string) => void;
+  type?: string | string[] | ((req: express$Request) => any);
+  verify?: (req: express$Request, res: express$Response, buf: Buffer, encoding: string) => void;
 };
 
 declare type bodyParser$OptionsText = bodyParser$Options & {
@@ -32,12 +30,12 @@ declare module "body-parser" {
     declare type OptionsJson = bodyParser$OptionsJson;
     declare type OptionsUrlencoded = bodyParser$OptionsUrlencoded;
 
-    declare function json(options?: OptionsJson): Middleware;
+    declare function json(options?: OptionsJson): express$Middleware;
 
-    declare function raw(options?: Options): Middleware;
+    declare function raw(options?: Options): express$Middleware;
 
-    declare function text(options?: OptionsText): Middleware;
+    declare function text(options?: OptionsText): express$Middleware;
 
-    declare function urlencoded(options?: OptionsUrlencoded): Middleware;
+    declare function urlencoded(options?: OptionsUrlencoded): express$Middleware;
 
 }
