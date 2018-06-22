21018301860c47a927ad6d56109435625ff1f7f1
Bastian Widmer <b.widmer@dasrecht.net>
2018-06-22 23:28:39 +0200

    ordering

--- images/varnish/varnish-start.sh
+++ images/varnish/varnish-start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/sbin/varnishd -a :8080 -T :6082 -F -f /etc/varnish/default.vcl -S /etc/varnish/secret -p http_resp_hdr_len=$HTTP_RESP_HDR_LEN -p http_resp_size=$HTTP_RESP_SIZE -s malloc,$CACHE_SIZE -p nuke_limit=$NUKE_LIMIT
+/usr/sbin/varnishd -a :8080 -T :6082 -F -f /etc/varnish/default.vcl -S /etc/varnish/secret -p http_resp_hdr_len=$HTTP_RESP_HDR_LEN -p http_resp_size=$HTTP_RESP_SIZE -p nuke_limit=$NUKE_LIMIT -s malloc,$CACHE_SIZE
