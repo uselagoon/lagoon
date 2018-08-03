cb63fd8a493d571938b132a660ebdb0fd0c7b484
Karl Horky <karl.horky@gmail.com>
2018-08-03 17:34:08 +0200

    Use simpler text-table for table formatter

--- cli/src/formatters/table.js
+++ cli/src/formatters/table.js
@@ -1,3 +1,26 @@
 // @flow
 
-export { table as default } from 'table';
+import chalk from 'chalk';
+import R from 'ramda';
+import stripAnsi from 'strip-ansi';
+import table from 'text-table';
+import type { FormatData } from '../types/Format';
+
+export default function (data: FormatData) {
+  return table(
+    // Transform data by...
+    R.over(
+      // ...modifying the heading row...
+      R.lensIndex(0),
+      // ...to be a middle gray color
+      R.map(chalk.hex('#848484')),
+      data,
+    ),
+    {
+      // Calculate string length correctly for the columns
+      stringLength(str) {
+        return stripAnsi(str).length;
+      },
+    },
+  );
+}
