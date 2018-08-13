967eb138331dc5e2acc674c11c02e8eae8cc74a4
Karl Horky <karl.horky@gmail.com>
2018-08-13 14:08:46 +0200

    Format

--- services/api/src/dao/utils.js
+++ services/api/src/dao/utils.js
@@ -21,7 +21,12 @@ const whereAnd = whereConds =>
       }
       return `${ret} AND ${str}`;
     }, ''),
-    R.filter(R.compose(R.not, R.isEmpty)),
+    R.filter(
+      R.compose(
+        R.not,
+        R.isEmpty,
+      ),
+    ),
   )(whereConds);
 
 // Creates an IN clause like this: $field IN (val1,val2,val3)
@@ -62,7 +67,10 @@ const query = (sqlClient, sql) =>
 // We use this just for consistency of the api calls
 const prepare = (sqlClient, sql) => sqlClient.prepare(sql);
 
-const isPatchEmpty = R.compose(R.isEmpty, R.propOr({}, 'patch'));
+const isPatchEmpty = R.compose(
+  R.isEmpty,
+  R.propOr({}, 'patch'),
+);
 
 // Tells if a user tries to modify an sshKey in the given patch
 // payload
