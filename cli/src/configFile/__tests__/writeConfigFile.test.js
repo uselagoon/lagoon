817f616afc553f68d1b8c8267d0778ffc92e842a
Karl Horky <karl.horky@gmail.com>
2018-08-14 19:19:13 +0200

    Write test for writing config to snake case

--- cli/src/configFile/__tests__/writeConfigFile.test.js
+++ cli/src/configFile/__tests__/writeConfigFile.test.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { configFileInputOptionsTypes } from '../../types/ConfigFile';
 import { writeConfigFile } from '../writeConfigFile';
 
 jest.mock('../../util/fs');
@@ -8,8 +9,13 @@ const fs = require('../../util/fs');
 // Flow does not know which objects are actual mocks
 // this function casts given parameter to JestMockFn
 const _castMockForFlow = (mockFn: any): JestMockFn<any, any> => mockFn;
+const writeFileMock = _castMockForFlow(fs.writeFile);
 
 describe('writeConfigFile', () => {
+  afterEach(() => {
+    writeFileMock.mockClear();
+  });
+
   it('should write default config to given path', async () => {
     writeConfigFile('lagoon.yml', {
       project: 'your_project',
@@ -17,7 +23,25 @@ describe('writeConfigFile', () => {
       ssh: '',
       token: '',
     });
-    const [filename, data] = _castMockForFlow(fs.writeFile).mock.calls[0];
+    const [filename, data] = writeFileMock.mock.calls[0];
+
+    expect(filename).toBe('lagoon.yml');
+    expect(data).toMatchSnapshot();
+  });
+
+  it('should convert camelcase keys to snake case before writing config', async () => {
+    // $FlowFixMe Jest can mutate exports https://stackoverflow.com/a/42979724/1268612
+    configFileInputOptionsTypes.arbitraryConfigKey = String;
+
+    // $FlowFixMe This test uses an invalid arbitrary key here on purpose
+    writeConfigFile('lagoon.yml', {
+      project: 'your_project',
+      arbitraryConfigKey: 'asdf',
+      api: '',
+      ssh: '',
+      token: '',
+    });
+    const [filename, data] = writeFileMock.mock.calls[0];
 
     expect(filename).toBe('lagoon.yml');
     expect(data).toMatchSnapshot();
