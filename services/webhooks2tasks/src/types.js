b566e14968789a6fdf5fcf4ce06acee675cc0441
Karl Horky <karl.horky@gmail.com>
2018-08-14 16:33:23 +0200

    Format with Prettier

--- services/webhooks2tasks/src/types.js
+++ services/webhooks2tasks/src/types.js
@@ -6,30 +6,34 @@
 
 export type ChannelWrapper = {
   sendToQueue: (queue: string, content: Buffer, options: Object) => void,
-  publish: (exchange: string, routingKey: string, content: Buffer, options: Object) => void,
-  ack: (msg: Object) => void,
-}
+  publish: (
+    exchange: string,
+    routingKey: string,
+    content: Buffer,
+    options: Object
+  ) => void,
+  ack: (msg: Object) => void
+};
 
 export type removeData = {
   projectName: string,
   branch: ?string,
   pullrequest: ?string,
-  type: string,
-}
+  type: string
+};
 
 export type deployData = {
   projectName: string,
   branchName: string,
   sha: string
-}
-
+};
 
 export type WebhookRequestData = {
   webhooktype: string,
   event: string,
   giturl: string,
   uuid?: string,
-  body?: Object,
+  body?: Object
 };
 
 export type Project = {
@@ -42,5 +46,5 @@ export type Project = {
 export type RabbitMQMsg = {
   content: Buffer,
   fields: Object,
-  properties: Object,
+  properties: Object
 };
