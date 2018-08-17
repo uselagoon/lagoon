b09a37dcb12b839a79b668530489344c93816352
Karl Horky <karl.horky@gmail.com>
2018-08-17 17:06:41 +0200

    Update views for users

--- services/api-db/docker-entrypoint-initdb.d/01-views.sql
+++ services/api-db/docker-entrypoint-initdb.d/01-views.sql
@@ -2,47 +2,62 @@ USE infrastructure;
 
 -- Views
 
-DROP VIEW IF EXISTS pid_skid;
-CREATE VIEW pid_skid
+-- View for aggregating allowed permissions for users on projects
+-- +-----+------+
+-- | pid | usid |
+-- +-----+------+
+-- |   1 |    1 |
+-- |   1 |    3 |
+-- |   2 |    1 |
+-- |   3 |    4 |
+-- |   3 |    5 |
+-- |   4 |    4 |
+-- | ... |  ... |
+-- +-----+------+
+DROP VIEW IF EXISTS pid_usid;
+CREATE VIEW pid_usid
 AS
+  -- Projects where the user has access over their access to the owner customer
   SELECT DISTINCT
     p.id AS pid,
-    csk.skid AS skid
+    cu.usid AS usid
   FROM
-    customer_ssh_key csk
-    INNER JOIN customer c ON csk.cid = c.id
+    customer_user cu
+    INNER JOIN customer c ON cu.cid = c.id
     INNER JOIN project p ON p.customer = c.id
   UNION DISTINCT
+  -- Projects where the user is directly assigned to the project
   SELECT
-    psk.pid AS pid,
-    psk.skid AS skid
-  FROM project_ssh_key psk;
+    pu.pid AS pid,
+    pu.usid AS usid
+  FROM project_user pu;
 
--- Permissions view for calculating what each SSH Key is allowed to view.
+-- Permissions view for calculating what each user is allowed to access.
 -- Example:
--- +--------+-------------------+-----------+------------------------------------+
--- | key_id | ssh_key           | customers | projects                           |
--- +--------+-------------------+-----------+------------------------------------+
--- |      1 | ssh-rsa <key>     | 1,2       | 2,1                                |
--- |      2 | ssh-rsa <key>     | NULL      | NULL                               |
--- |      3 | ssh-rsa <key>     | 1         | 1                                  |
--- |      4 | ssh-rsa <key>     | 3         | 17,16,15,14,12,11,10,9,8,7,6,5,4,3 |
--- |      5 | ssh-ed25519 <key> | 3         | 17,16,15,14,12,11,10,9,8,7,6,5,4,3 |
--- +--------+-------------------+-----------+------------------------------------+
+-- +---------+-----------+------------------------------------+
+-- | user_id | customers | projects                           |
+-- +---------+-----------+------------------------------------+
+-- |       1 | 1,2       | 2,1                                |
+-- |       2 | NULL      | NULL                               |
+-- |       3 | 1         | 1                                  |
+-- |       4 | 3         | 17,16,15,14,12,11,10,9,8,7,6,5,4,3 |
+-- |       5 | 3         | 17,16,15,14,12,11,10,9,8,7,6,5,4,3 |
+-- +---------+-----------+------------------------------------+
 DROP VIEW IF EXISTS permission;
 CREATE VIEW permission
 AS
   SELECT
-    sk.id AS key_id,
-    CONCAT(sk.key_type, ' ', sk.key_value) AS ssh_key,
+    user.id AS user_id,
+    -- Comma-separated list of all the customers a user is assigned to
     (
-      SELECT GROUP_CONCAT(DISTINCT csk.cid SEPARATOR ',')
-      FROM customer_ssh_key csk
-      WHERE csk.skid = sk.id
+      SELECT GROUP_CONCAT(DISTINCT cu.cid SEPARATOR ',')
+      FROM customer_user cu
+      WHERE cu.usid = user.id
     ) AS customers,
+    -- Comma-separated list of all the project a user has access to
     (
       SELECT GROUP_CONCAT(DISTINCT r.pid SEPARATOR ',')
-      FROM pid_skid AS r
-      WHERE r.skid = sk.id
+      FROM pid_usid AS r
+      WHERE r.usid = user.id
     ) AS projects
-  FROM ssh_key sk;
+  FROM user;
