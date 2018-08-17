b0fc31a03e0ea7241a30275ea52f63d3ebc8ec08
Karl Horky <karl.horky@gmail.com>
2018-08-17 16:27:23 +0200

    Document permissions view and example data

--- services/api-db/docker-entrypoint-initdb.d/01-views.sql
+++ services/api-db/docker-entrypoint-initdb.d/01-views.sql
@@ -18,6 +18,17 @@ AS
     psk.skid AS skid
   FROM project_ssh_key psk;
 
+-- Permissions view for calculating what each SSH Key is allowed to view.
+-- Example:
+-- +--------+-------------------+-----------+------------------------------------+
+-- | key_id | ssh_key           | customers | projects                           |
+-- +--------+-------------------+-----------+------------------------------------+
+-- |      1 | ssh-rsa <key>     | 1,2       | 2,1                                |
+-- |      2 | ssh-rsa <key>     | NULL      | NULL                               |
+-- |      3 | ssh-rsa <key>     | 1         | 1                                  |
+-- |      4 | ssh-rsa <key>     | 3         | 17,16,15,14,12,11,10,9,8,7,6,5,4,3 |
+-- |      5 | ssh-ed25519 <key> | 3         | 17,16,15,14,12,11,10,9,8,7,6,5,4,3 |
+-- +--------+-------------------+-----------+------------------------------------+
 DROP VIEW IF EXISTS permission;
 CREATE VIEW permission
 AS
