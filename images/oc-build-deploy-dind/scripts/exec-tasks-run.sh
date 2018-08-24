10372d21cf23543225219952d39d884177695ac1
Elliot Schlegelmilch <elliot@amazee.io>
2018-08-24 15:16:58 -0700

    if the deploymentconfig doesn't exist, don't even try run the command just bail. this fixes the case were pre-rollouts fail because the cli pods don't yet exist.

--- images/oc-build-deploy-dind/scripts/exec-tasks-run.sh
+++ images/oc-build-deploy-dind/scripts/exec-tasks-run.sh
@@ -16,19 +16,19 @@ if [[ $(oc -n ${OPENSHIFT_PROJECT} get deploymentconfigs --no-headers=true -o na
       sleep 1
     done
   fi
+
+  POD=$(oc -n ${OPENSHIFT_PROJECT} get pods -l service=${SERVICE_NAME} -o json | jq -r '.items[] | select(.metadata.deletionTimestamp == null) | select(.status.phase == "Running") | .metadata.name' | head -n 1)
+
+  if [[ ! $POD ]]; then
+    echo "No running pod found for ${SERVICE_NAME}"
+    exit 1
+  fi
+
+  if [[ $CONTAINER == "false" ]]; then
+    CONTAINER_PARAMETER=""
+  else
+    CONTAINER_PARAMETER="-c ${CONTAINER}"
+  fi
+
+  oc -n ${OPENSHIFT_PROJECT} exec ${POD} ${CONTAINER_PARAMETER} -i -- ${SHELL} -c "${COMMAND}"
 fi
-
-POD=$(oc -n ${OPENSHIFT_PROJECT} get pods -l service=${SERVICE_NAME} -o json | jq -r '.items[] | select(.metadata.deletionTimestamp == null) | select(.status.phase == "Running") | .metadata.name' | head -n 1)
-
-if [[ ! $POD ]]; then
-  echo "No running pod found for ${SERVICE_NAME}"
-  exit 1
-fi
-
-if [[ $CONTAINER == "false" ]]; then
-  CONTAINER_PARAMETER=""
-else
-  CONTAINER_PARAMETER="-c ${CONTAINER}"
-fi
-
-oc -n ${OPENSHIFT_PROJECT} exec ${POD} ${CONTAINER_PARAMETER} -i -- ${SHELL} -c "${COMMAND}"
