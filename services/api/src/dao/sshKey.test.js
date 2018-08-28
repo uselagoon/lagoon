ab11c7fa57d969a3799922600cc3c7b793810127
Karl Horky <karl.horky@gmail.com>
2018-08-28 18:39:09 -0300

    Fix CLI and tests

--- services/api/src/dao/sshKey.test.js
+++ services/api/src/dao/sshKey.test.js
@@ -3,14 +3,13 @@ const { Sql, validateSshKey } = require('./sshKey');
 describe('Sql', () => {
   describe('updateSshKey', () => {
     it('should create a proper query', () => {
-      const cred = {};
       const input = {
         id: 1,
         patch: {
           keyType: 'ssh-rsa',
         },
       };
-      const ret = Sql.updateSshKey(cred, input);
+      const ret = Sql.updateSshKey(input);
       expect(ret).toMatchSnapshot();
     });
   });
