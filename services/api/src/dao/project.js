ea364ebe5825d1de937915004a7c22934408cbb3
Elliot Schlegelmilch <elliot@amazee.io>
2018-09-17 14:04:45 -0700

    missed a comma in the merge.

--- services/api/src/dao/project.js
+++ services/api/src/dao/project.js
@@ -183,7 +183,7 @@ const addProject = ({ sqlClient }) => async (cred, input) => {
         ${input.pullrequests ? ':pullrequests' : '"true"'},
         ${input.productionEnvironment ? ':production_environment' : 'NULL'},
         ${input.autoIdle ? ':auto_idle' : '1'},
-        ${input.storageCalc ? ':storage_calc' : '1'}
+        ${input.storageCalc ? ':storage_calc' : '1'},
         ${input.environmentLimit ? ':environment_limit' : '5'}
 
       );
