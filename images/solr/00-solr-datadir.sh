08df5935881dc6915919f991c6aafc350f7f74be
Elliot Schlegelmilch <elliot@amazee.io>
2018-09-20 16:27:12 -0700

    refactor a bit.

--- images/solr/00-solr-datadir.sh
+++ images/solr/00-solr-datadir.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
 set -ex
 
-# checking legacy configs
-for core in $(ls /opt/solr/server/solr/mycores/); do
-  grep '<dataDir>/var/solr/${solr.core.name}</dataDir>' /opt/solr/server/solr/mycores/${core}/conf/solrconfig.xml
-
-  if [ $? -ge 1 ]; then
-    echo "${core}/conf/solrconfig.xml is missing updated dataDir path." > /dev/stderr
-    echo 'please ensure your config has the exact line: <dataDir>/var/solr/${solr.core.name}</dataDir>' > /dev/stderr
-    exit 2
-  fi
-
-done
-
-
 # old volume looks like this
 # $corename/data/-files-
 # new volume looks like this
 # $corename/-files-
 
-for datadir in /var/solr/* ; do
+for datadir in `ls -d /var/solr/*` ; do
+  corename=$(basename $datadir)
   if [ -d ${datadir}/data ]; then
     echo "${datadir}/data is in old format, moving to ${datadir}."
-    mv -v ${datadir}/data/* ${datadir}/
+    mv ${datadir}/data/* ${datadir}/
+    rm -Rf ${datadir}/data
   fi
+
+  mkdir -p /opt/solr/server/solr/${corename}
+  cp -R ${datadir}/conf /opt/solr/server/solr/${corename}/
+  touch /opt/solr/server/solr/${corename}/core.properties
+  sed -ibak 's/<dataDir>.*/<dataDir>\/var\/solr\/${solr.core.name}<\/dataDir>/' /opt/solr/server/solr/${corename}/conf/solrconfig.xml
+
 done
+
+exec solr-foreground
