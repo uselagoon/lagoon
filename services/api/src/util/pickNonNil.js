5ceeec0e6f6f5ab983b5c4230994bb1b472e0988
Karl Horky <karl.horky@gmail.com>
2018-10-02 11:28:29 +0200

    Fix types

--- services/api/src/util/pickNonNil.js
+++ services/api/src/util/pickNonNil.js
@@ -5,6 +5,6 @@ const R = require('ramda');
 // Returns a partial copy of an object containing only the keys specified, as long as they are non-nil.
 module.exports = R.curry((
   names /* : $ReadOnlyArray<any> */,
-  obj /* : Object */,
+  obj /* : { [string]: mixed } */,
 ) /* : Object */ =>
   R.pickBy((val, key) => R.contains(key, names) && !R.isNil(val))(obj));
