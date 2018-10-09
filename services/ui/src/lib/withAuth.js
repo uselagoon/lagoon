e95e7478a124b42fbab6026d76b57e21cc7b98db
Brandon Williams <brandon@amazeelabs.com>
2018-10-08 21:56:17 -0500

    Add ability to use local JWT auth token for ui development

--- services/ui/src/lib/withAuth.js
+++ services/ui/src/lib/withAuth.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import getConfig from 'next/config';
 import withKeycloak from './withKeycloak';
+import withLocalAuth from './withLocalAuth';
 
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
 export default App => {
-  return withKeycloak(App);
+  if (publicRuntimeConfig.GRAPHQL_API_TOKEN) {
+    return withLocalAuth(App);
+  } else {
+    return withKeycloak(App);
+  }
 };
