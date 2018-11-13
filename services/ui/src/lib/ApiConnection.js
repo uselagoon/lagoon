842e204e390279f992cefb2a978fcbeecf2c5638
Brandon Williams <brandon@amazeelabs.com>
2018-11-13 00:06:59 -0600

    Add support for apollo chrome dev tools extension

--- services/ui/src/lib/ApiConnection.js
+++ services/ui/src/lib/ApiConnection.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import getConfig from 'next/config';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 import { ApolloProvider } from 'react-apollo';
 import { AuthContext } from './withAuth';
 import NotAuthenticated from '../components/NotAuthenticated';
@@ -15,10 +19,24 @@ export default ({ children }) =>
       }
 
       const client = new ApolloClient({
-        uri: publicRuntimeConfig.GRAPHQL_API,
-        headers: {
-          authorization: `Bearer ${auth.apiToken}`
-        }
+        link: ApolloLink.from([
+          onError(({ graphQLErrors, networkError }) => {
+            if (graphQLErrors)
+              graphQLErrors.map(({ message, locations, path }) =>
+                console.log(
+                  `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+                ),
+              );
+            if (networkError) console.log(`[Network error]: ${networkError}`);
+          }),
+          new HttpLink({
+            uri: publicRuntimeConfig.GRAPHQL_API,
+            headers: {
+              authorization: `Bearer ${auth.apiToken}`,
+            },
+          })
+        ]),
+        cache: new InMemoryCache()
       });
 
       return (
