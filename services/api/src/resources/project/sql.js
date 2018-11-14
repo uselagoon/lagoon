ed190d04c21ff312d84f27ea1af6d1871a92ecc4
Schnitzel <michael@amazee.com>
2018-11-13 20:11:14 -0600

    hotfix: selectAllUserForProjectId != selectAllUsersForProjectId

--- services/api/src/resources/project/sql.js
+++ services/api/src/resources/project/sql.js
@@ -77,7 +77,7 @@ const Sql /* : SqlObj */ = {
       .whereIn('customer', customerIds)
       .toString(),
   // Returns all users that have access to a project, either directly or indirectly (through customer).
-  selectAllUserForProjectId: (projectId /* : number */) =>
+  selectAllUsersForProjectId: (projectId /* : number */) =>
     knex('user')
       .leftJoin('project_user as pu', 'pu.usid', 'user.id')
       .leftJoin('customer_user as cu', 'cu.usid', 'user.id')
