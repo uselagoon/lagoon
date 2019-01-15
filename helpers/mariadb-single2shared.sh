d1b2cac6bbe44c8485ab57da8066dcc7a906d831
Schnitzel <michael@amazee.com>
2019-01-15 00:05:22 -0600

    improve mariadb shared script

--- helpers/mariadb-single2shared.sh
+++ helpers/mariadb-single2shared.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+
+if [ ! "$1" ]; then
+  echo "please define openshift project as first argument"
+  exit 1;
+fi
+
 set -euo pipefail
 
 which shyaml > /dev/null
@@ -14,21 +21,38 @@ if [ $? -gt 0 ]; then
   exit 1
 fi
 
-PROJECT_NAME=$(oc project -q)
+PROJECT_NAME=$1
+
+echo "*** Starting mariadb-single --> mariadb-shared migration in ${PROJECT_NAME}"
 
 SERVICE_NAME=mariadb
 SERVICE_NAME_UPPERCASE=$(echo $SERVICE_NAME | tr [:lower:] [:upper:])
 SERVICE_TYPE=mariadb-shared
 
-ENVIRONMENT_TYPE=$(oc get configmap lagoon-env -o json | jq -r '.data.LAGOON_ENVIRONMENT_TYPE')
+ENVIRONMENT_TYPE=$(oc -n $1 get configmap lagoon-env -o json | jq -r '.data.LAGOON_ENVIRONMENT_TYPE')
+
+MARIADB_REPLICAS=$(oc -n $1 get dc/mariadb -o json | jq -r '.spec.replicas')
+
+if [ "$MARIADB_REPLICAS" == "0" ]; then
+  oc -n $1 scale dc/mariadb --replicas=1
+  oc -n $1 rollout status dc/mariadb
+fi
 
 # export old mariadb pod name
-OLD_POD=$(oc get pod -o  custom-columns=NAME:.metadata.name --no-headers -l service=$SERVICE_NAME)
-echo "found $SERVICE_NAME pod $OLD_POD"
+OLD_POD=$(oc -n $1 get pod -o  custom-columns=NAME:.metadata.name --no-headers -l service=$SERVICE_NAME)
+
+if [[ "$OLD_POD" ]]; then
+  echo "found $SERVICE_NAME pod $OLD_POD"
+else
+  echo "no running pod found for service '${SERVICE_NAME}'', is it running?"
+  exit 1
+fi
 
 echo "*** Pausing nginx and cli"
-oc scale dc/nginx --replicas=0
-oc scale dc/cli --replicas=0
+NGINX_REPLICAS=$(oc -n $1 get dc/nginx -o json | jq -r '.spec.replicas')
+CLI_REPLICAS=$(oc -n $1 get dc/cli -o json | jq -r '.spec.replicas')
+oc -n $1 scale dc/nginx --replicas=0
+oc -n $1 scale dc/cli --replicas=0
 
 
 # create service broker
@@ -37,7 +61,7 @@ oc scale dc/cli --replicas=0
 OPENSHIFT_TEMPLATE="$(git rev-parse --show-toplevel)/images/oc-build-deploy-dind/openshift-templates/${SERVICE_TYPE}/servicebroker.yml"
 
 # Only SERVICE_NAME is used in the template, fudge the other parameters.
-oc process  --local -o yaml --insecure-skip-tls-verify \
+oc -n $1 process  --local -o yaml --insecure-skip-tls-verify \
   -f ${OPENSHIFT_TEMPLATE} \
   -p SERVICE_NAME="${SERVICE_NAME}" \
   -p SERVICEBROKER_NAME="lagoon-dbaas-mariadb-apb" \
@@ -50,10 +74,10 @@ oc process  --local -o yaml --insecure-skip-tls-verify \
   -p SERVICE_ROUTER_URL="NULL" \
   -p REGISTRY="NULL" \
   -p OPENSHIFT_PROJECT="NULL" \
-| oc apply -f -
+| oc -n $1 apply -f -
 
 # ServiceBrokers take a bit, wait until the credentials secret is available
-until oc get --insecure-skip-tls-verify secret ${SERVICE_NAME}-servicebroker-credentials
+until oc -n $1 get --insecure-skip-tls-verify secret ${SERVICE_NAME}-servicebroker-credentials
 do
   echo "Secret ${SERVICE_NAME}-servicebroker-credentials not available yet, waiting for 10 secs"
   sleep 10
@@ -61,7 +85,7 @@ done
 
 # Load credentials out of secret
 SECRETS=/tmp/${PROJECT_NAME}-${OLD_POD}-migration.yaml
-oc get --insecure-skip-tls-verify secret ${SERVICE_NAME}-servicebroker-credentials -o yaml > $SECRETS
+oc -n $1 get --insecure-skip-tls-verify secret ${SERVICE_NAME}-servicebroker-credentials -o yaml > $SECRETS
 
 DB_HOST=$(cat $SECRETS | shyaml get-value data.DB_HOST | base64 -D)
 DB_USER=$(cat $SECRETS | shyaml get-value data.DB_USER | base64 -D)
@@ -71,30 +95,30 @@ DB_PORT=$(cat $SECRETS | shyaml get-value data.DB_PORT | base64 -D)
 
 echo "*** Transfering 'drupal' database from $OLD_POD to $DB_HOST"
 # transfer database between from old to new
-oc exec $OLD_POD -- bash -c "mysqldump --max-allowed-packet=500M --events --routines --quick --add-locks --no-autocommit --single-transaction --no-create-db drupal | mysql -h $DB_HOST -u $DB_USER -p${DB_PASSWORD} -P $DB_PORT $DB_NAME"
+oc -n $1 exec $OLD_POD -- bash -c "mysqldump --max-allowed-packet=500M --events --routines --quick --add-locks --no-autocommit --single-transaction --no-create-db drupal | mysql -h $DB_HOST -u $DB_USER -p${DB_PASSWORD} -P $DB_PORT $DB_NAME"
 
 CONFIG_BAK="/tmp/${PROJECT_NAME}-$(date +%F-%T)-lagoon-env.yaml"
 echo "*** Backing up configmap in case we need to revert: ${CONFIG_BAK}"
-oc get configmap lagoon-env -o yaml > $CONFIG_BAK
+oc -n $1 get configmap lagoon-env -o yaml > $CONFIG_BAK
 
 echo "*** updating configmap to point to ${DB_HOST}."
 # Add credentials to our configmap, prefixed with the name of the servicename of this servicebroker
-oc patch --insecure-skip-tls-verify configmap lagoon-env \
+oc -n $1 patch --insecure-skip-tls-verify configmap lagoon-env \
    -p "{\"data\":{\"${SERVICE_NAME_UPPERCASE}_HOST\":\"${DB_HOST}\", \"${SERVICE_NAME_UPPERCASE}_USERNAME\":\"${DB_USER}\", \"${SERVICE_NAME_UPPERCASE}_PASSWORD\":\"${DB_PASSWORD}\", \"${SERVICE_NAME_UPPERCASE}_DATABASE\":\"${DB_NAME}\", \"${SERVICE_NAME_UPPERCASE}_PORT\":\"${DB_PORT}\"}}"
 
 
 echo "*** Deleting mariadb service. Scaling old mariadb to 0; you can clean up the DC and pv later"
-oc delete service mariadb
-oc scale dc/mariadb --replicas=0
+oc -n $1 delete service mariadb
+oc -n $1 scale dc/mariadb --replicas=0
 
 # transfer complete, clean up
 rm -f $SECRETS
 
-oc scale dc/nginx --replicas=1
-oc scale dc/cli --replicas=1
+oc -n $1 scale dc/nginx --replicas=$NGINX_REPLICAS
+oc -n $1 scale dc/cli --replicas=$CLI_REPLICAS
 
-oc rollout latest dc/nginx
-oc rollout latest dc/cli
-oc rollout status dc/nginx
-oc rollout status dc/cli
+oc -n $1 rollout latest dc/nginx
+oc -n $1 rollout latest dc/cli
+oc -n $1 rollout status dc/nginx
+oc -n $1 rollout status dc/cli
 echo "*** done."
