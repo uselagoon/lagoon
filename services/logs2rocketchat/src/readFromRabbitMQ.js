37ba3a6fa42877ae4420a41ce7bb20e414361cb7
Tyler Ward <twardnw@gmail.com>
2019-01-30 20:20:05 -0800

    #858 - move linebreak

--- services/logs2rocketchat/src/readFromRabbitMQ.js
+++ services/logs2rocketchat/src/readFromRabbitMQ.js
@@ -131,11 +131,11 @@ async function readFromRabbitMQ (msg: RabbitMQMsg, channelWrapperLogs: ChannelWr
       } else {
         text = `${text} \`${meta.branchName}\``
       }
-      text = `${text} Build \`${meta.buildName}\` complete.\n`
+      text = `${text} Build \`${meta.buildName}\` complete.`
       if (meta.logLink){
         text = `${text} [Logs](${meta.logLink})\n`
       }
-      text = `${text}${meta.route}\n ${meta.routes.join("\n")}`
+      text = `\n${text}${meta.route}\n ${meta.routes.join("\n")}`
       sendToRocketChat(project, text, 'lawngreen', ':white_check_mark:', channelWrapperLogs, msg, appId)
       break;
 
