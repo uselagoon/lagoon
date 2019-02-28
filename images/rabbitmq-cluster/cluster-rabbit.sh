ee1e7291d9be0e72b65fac350ce038c6531eb2ca
Elliot Schlegelmilch <elliot@amazee.io>
2019-02-28 14:12:05 -0800

    set the k8s.servicename to match the SERVICE_NAME.

--- images/rabbitmq-cluster/cluster-rabbit.sh
+++ images/rabbitmq-cluster/cluster-rabbit.sh
@@ -3,6 +3,7 @@
 # clustering uses full hostnames, generate those
 echo NODENAME=rabbit@${HOSTNAME}.${SERVICE_NAME}.${POD_NAMESPACE}.svc.cluster.local > /etc/rabbitmq/rabbitmq-env.conf
 echo cluster_formation.k8s.hostname_suffix=.${SERVICE_NAME}.${POD_NAMESPACE}.svc.cluster.local >> /etc/rabbitmq/rabbitmq.conf
+echo cluster_formation.k8s.service_name=${SERVICE_NAME} >> /etc/rabbitmq/rabbitmq.conf
 
 # start the server
 docker-entrypoint.sh rabbitmq-server
