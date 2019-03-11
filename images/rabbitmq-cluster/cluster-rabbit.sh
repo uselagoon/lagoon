dc07d6e2d09cb12b2c94f2475f08ee1a564efe87
Schnitzel <michael@amazee.com>
2019-03-11 17:05:36 -0500

    use regular service for rabbitmq and create a specific headless one for cluster discovery

--- images/rabbitmq-cluster/cluster-rabbit.sh
+++ images/rabbitmq-cluster/cluster-rabbit.sh
@@ -11,7 +11,7 @@ fi
 # clustering uses full hostnames, generate those
 echo NODENAME=rabbit@${HOSTNAME}.${SERVICE_NAME}.${POD_NAMESPACE}.svc.cluster.local > /etc/rabbitmq/rabbitmq-env.conf
 echo cluster_formation.k8s.hostname_suffix=.${SERVICE_NAME}.${POD_NAMESPACE}.svc.cluster.local >> /etc/rabbitmq/rabbitmq.conf
-echo cluster_formation.k8s.service_name=${SERVICE_NAME} >> /etc/rabbitmq/rabbitmq.conf
+echo cluster_formation.k8s.service_name=${SERVICE_NAME}-headless >> /etc/rabbitmq/rabbitmq.conf
 
 # start the server
 docker-entrypoint.sh rabbitmq-server
