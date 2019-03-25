56a3d1c3b45c25f8d4ef487af6b02e512516fb79
Tyler Ward <twardnw@gmail.com>
2019-03-25 13:39:14 -0700

    #979 - leave original file in place to keep restarts happy

--- images/php/fpm/entrypoints/71-php-newrelic.sh
+++ images/php/fpm/entrypoints/71-php-newrelic.sh
@@ -5,5 +5,5 @@ if [ ${NEWRELIC_ENABLED+x} ]; then
   # envplate the newrelic ini file
   ep /usr/local/etc/php/conf.d/newrelic.disable
 
-  mv /usr/local/etc/php/conf.d/newrelic.disable /usr/local/etc/php/conf.d/newrelic.ini
+  cp /usr/local/etc/php/conf.d/newrelic.disable /usr/local/etc/php/conf.d/newrelic.ini
 fi
