876842be868eb8fd83b6807e4a5c5e54a6b8d47a
Elliot Schlegelmilch <elliot@amazee.io>
2019-03-27 14:24:55 -0700

    rename TEMPLATE_IMAGES_PARAMETERS to TEMPLATE_ADDITIONAL_PARAMETERS and add ENVIRONMENT_TYPE to it.

--- images/oc-build-deploy-dind/scripts/exec-openshift-resources.sh
+++ images/oc-build-deploy-dind/scripts/exec-openshift-resources.sh
@@ -6,6 +6,12 @@ else
   SERVICE_ROUTER_URL=""
 fi
 
+TEMPLATE_ADDITIONAL_PARAMETERS=()
+
+if [[ $(oc process --local -f ${OPENSHIFT_TEMPLATE} --parameters | grep ENVIRONMENT_TYPE) ]]; then
+  TEMPLATE_ADDITIONAL_PARAMETERS+=(-p "ENVIRONMENT_TYPE=${ENVIRONMENT_TYPE}")
+fi
+
 oc process  --local -o yaml --insecure-skip-tls-verify \
   -n ${OPENSHIFT_PROJECT} \
   -f ${OPENSHIFT_TEMPLATE} \
@@ -14,10 +20,10 @@ oc process  --local -o yaml --insecure-skip-tls-verify \
   -p SAFE_PROJECT="${SAFE_PROJECT}" \
   -p BRANCH="${BRANCH}" \
   -p PROJECT="${PROJECT}" \
-  $(grep -q -s ENVIRONMENT_TYPE ${OPENSHIFT_TEMPLATE} && echo -p ENVIRONMENT_TYPE="${ENVIRONMENT_TYPE}" ) \
   -p LAGOON_GIT_SHA="${LAGOON_GIT_SHA}" \
   -p SERVICE_ROUTER_URL="${SERVICE_ROUTER_URL}" \
   -p REGISTRY="${OPENSHIFT_REGISTRY}" \
   -p OPENSHIFT_PROJECT=${OPENSHIFT_PROJECT} \
   "${TEMPLATE_PARAMETERS[@]}" \
+  "${TEMPLATE_ADDITIONAL_PARAMETERS[@]}" \
   | outputToYaml
