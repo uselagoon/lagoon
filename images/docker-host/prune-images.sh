8d3f57c85b7549c7927f441212094fcf90743b4f
Bastian Widmer <b.widmer@dasrecht.net>
2019-04-26 14:18:09 +0200

    Better docker pruning - removing images older than 7 days instead of pruning the whole cache

--- images/docker-host/prune-images.sh
+++ images/docker-host/prune-images.sh
@@ -4,4 +4,5 @@ if ! docker -H ${DOCKER_HOST} info &> /dev/null; then
     echo "could not connect to ${DOCKER_HOST}"; exit 1
 fi
 
-docker image prune -a -f
+# prune all images older than 7 days
+docker image prune -a --filter "until=168h"
