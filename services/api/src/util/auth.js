9f5857f5a3406f4b67afa156c49b1584361c6e18
Brandon Williams <brandon@amazeelabs.com>
2019-06-13 14:52:06 -0500

    Send group claims to keycloak authz

--- services/api/src/util/auth.js
+++ services/api/src/util/auth.js
@@ -3,10 +3,30 @@ const jwt = require('jsonwebtoken');
 const logger = require('../logger');
 const { keycloakGrantManager } = require('../clients/keycloakClient');
 const User = require('../models/user');
+const Group = require('../models/group');
 
 const UserModel = User.User();
+const GroupModel = Group.Group();
 
 const { JWTSECRET, JWTAUDIENCE } = process.env;
+
+const sortRolesByWeight = (a, b) => {
+  const roleWeights = {
+    guest: 1,
+    reporter: 2,
+    developer: 3,
+    maintainer: 4,
+    owner: 5,
+  };
+
+  if (roleWeights[a] < roleWeights[b]) {
+    return -1;
+  } else if (roleWeights[a] > roleWeights[b]) {
+    return 1;
+  }
+
+  return 0;
+};
 
 const getGrantForKeycloakToken = async (sqlClient, token) => {
   let grant = '';
@@ -66,6 +86,7 @@ const legacyHasPermission = (legacyCredentials) => {
 const keycloakHasPermission = (grant) => {
   return async (resource, scopeInput, attributes = {}) => {
     const scopes = (typeof scopeInput === 'string') ? [scopeInput] : scopeInput;
+    const currentUserId = grant.access_token.content.sub;
     let claims = {};
 
     const serviceAccount = await keycloakGrantManager.obtainFromClientCredentials();
@@ -80,7 +101,7 @@ const keycloakHasPermission = (grant) => {
             R.prop('users'),
           )(attributes),
         ],
-        currentUser: [grant.access_token.content.sub],
+        currentUser: [currentUserId],
       };
     }
 
@@ -92,7 +113,7 @@ const keycloakHasPermission = (grant) => {
         };
 
         const userProjects = await UserModel.getAllProjectsIdsForUser({
-          id: grant.access_token.content.sub,
+          id: currentUserId,
         });
 
         if (userProjects.length) {
@@ -103,26 +124,8 @@ const keycloakHasPermission = (grant) => {
         }
 
         const roles = await UserModel.getUserRolesForProject({
-          id: grant.access_token.content.sub,
+          id: currentUserId,
         }, R.prop('project', attributes));
-
-        const sortRolesByWeight = (a, b) => {
-          const roleWeights = {
-            guest: 1,
-            reporter: 2,
-            developer: 3,
-            maintainer: 4,
-            owner: 5,
-          };
-
-          if (roleWeights[a] < roleWeights[b]) {
-            return -1;
-          } else if (roleWeights[a] > roleWeights[b]) {
-            return 1;
-          }
-
-          return 0;
-        };
 
         const highestRoleForProject = R.pipe(
           R.uniq,
@@ -140,6 +143,36 @@ const keycloakHasPermission = (grant) => {
         }
       } catch (err) {
         logger.error(`Could not submit project (${R.prop('project', attributes)}) claims for authz request: ${err.message}`);
+      }
+    }
+
+    if (R.prop('group', attributes)) {
+      try {
+        const group = await GroupModel.loadGroupById(R.prop('group', attributes));
+
+        const groupRoles = R.pipe(
+          R.filter(membership =>
+            R.pathEq(['user', 'id'], currentUserId, membership),
+          ),
+          R.pluck('role'),
+        )(group.members);
+
+        const highestRoleForGroup = R.pipe(
+          R.uniq,
+          R.reject(R.isEmpty),
+          R.reject(R.isNil),
+          R.sort(sortRolesByWeight),
+          R.last,
+        )(groupRoles);
+
+        if (highestRoleForGroup) {
+          claims = {
+            ...claims,
+            userGroupRole: [highestRoleForGroup],
+          };
+        }
+      } catch (err) {
+        logger.error(`Could not submit group (${R.prop('group', attributes)}) claims for authz request: ${err.message}`);
       }
     }
 
