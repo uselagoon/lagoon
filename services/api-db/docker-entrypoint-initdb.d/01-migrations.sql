afcc2c61da5335dde00ff8e9ecf55932dbe03e74
Chris Davis <cdchris12@gmail.com>
2019-07-02 14:37:23 -0500

    Updating per code review suggestions

--- services/api-db/docker-entrypoint-initdb.d/01-migrations.sql
+++ services/api-db/docker-entrypoint-initdb.d/01-migrations.sql
@@ -646,20 +646,21 @@ CREATE OR REPLACE PROCEDURE
 $$
 
 CREATE OR REPLACE PROCEDURE
-  update_env_vars_from_varchar_to_text()
+  convert_env_vars_from_varchar_to_text()
 
   BEGIN
-    IF NOT EXISTS (
-      SELECT NULL
-      FROM INFORMATION_SCHEMA.COLUMNS
-      WHERE
-        table_name = 'env_vars'
-        AND table_schema = 'infrastructure'
-        AND column_name = 'value'
-    ) THEN
+    DECLARE column_type varchar(50);
+
+    SELECT DATA_TYPE INTO column_type
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE
+      table_name = 'env_vars'
+      AND table_schema = 'infrastructure'
+      AND column_name = 'value';
+
+    IF (column_type = 'varchar') THEN
       ALTER TABLE `env_vars`
-      MODIFY `value` TEXT NOT NULL;
-      UPDATE env_vars
+      MODIFY `value` text NOT NULL;
     END IF;
   END;
 $$
@@ -697,7 +698,7 @@ CALL convert_task_command_to_text();
 CALL add_key_fingerprint_to_ssh_key();
 CALL add_autoidle_to_environment();
 CALL add_deploy_base_head_ref_title_to_environment();
-CALL update_env_vars_from_varchar_to_text();
+CALL convert_env_vars_from_varchar_to_text();
 
 -- Drop legacy SSH key procedures
 DROP PROCEDURE IF EXISTS CreateProjectSshKey;
