4fce4409d6977ef846403875ba1fae1c87ff8d4e
Chris Davis <cdchris12@gmail.com>
2019-07-31 15:37:27 -0500

    Added override option via the production_notices env variable.

--- images/php/fpm/entrypoints/51-production-detection.sh
+++ images/php/fpm/entrypoints/51-production-detection.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [[ ${LAGOON_ENVIRONMENT_TYPE} == "production" ]]; then
+if [[ ${LAGOON_ENVIRONMENT_TYPE} == "production" && ( -z "${production_notices}" || $production_notices == "true" ) ]]; then
     export PHP_ERROR_REPORTING="E_ALL & ~E_DEPRECATED & ~E_STRICT & ~E_NOTICE"
 else
-    export PHP_ERROR_REPORTING="E_ALL"
+    export PHP_ERROR_REPORTING="E_ALL & ~E_DEPRECATED & ~E_STRICT"
 fi
