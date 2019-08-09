f56a79e49511881ad95c97d4debf3ca027eea2fd
Alex Skrypnyk <alex.designworks@gmail.com>
2019-08-10 09:45:38 +1000

    Changed Xdebug enabling when XDEBUG_ENABLE is not empty rather then not set.

--- images/php/fpm/entrypoints/60-php-xdebug.sh
+++ images/php/fpm/entrypoints/60-php-xdebug.sh
@@ -25,12 +25,12 @@ get_dockerhost() {
   return
 }
 
-# Only if XDEBUG_ENABLE is set
-if [ ${XDEBUG_ENABLE+x} ]; then
+# Only if XDEBUG_ENABLE is not empty
+if [ ! -z ${XDEBUG_ENABLE} ]; then
   # remove first line and all comments
   sed -i '1d; s/;//' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
   # add comment that explains how we have xdebug enabled
-  sed -i '1s/^/;xdebug enabled as XDEBUG_ENABLE is set, see \/lagoon\/entrypoints\/60-php-xdebug.sh \n/' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  sed -i '1s/^/;xdebug enabled as XDEBUG_ENABLE is not empty, see \/lagoon\/entrypoints\/60-php-xdebug.sh \n/' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 
   # Only if DOCKERHOST is not already set, allows to set a DOCKERHOST via environment variables
   if [[ -z ${DOCKERHOST+x} ]]; then
