c0c2cf60c06f88bd1935e8e3abdae748de4a153f
Brandon Williams <brandon@amazee.io>
2019-09-04 07:02:29 -0500

    resticbackup:snapshot:finished webhook is now only one snapshot per webhook

--- services/webhooks2tasks/src/handlers/resticbackupSnapshotFinished.js
+++ services/webhooks2tasks/src/handlers/resticbackupSnapshotFinished.js
@@ -3,94 +3,57 @@
 const moment = require('moment');
 
 const { sendToLagoonLogs } = require('@lagoon/commons/src/logs');
+const { logger } = require('@lagoon/commons/src/local-logging');
 const {
   addBackup,
-  getEnvironmentBackups
+  getEnvironmentByOpenshiftProjectName
 } = require('@lagoon/commons/src/api');
 const R = require('ramda');
 
 import type { WebhookRequestData } from '../types';
 
-const saveSnapshotAsBackup = async (snapshot, environmentId) => {
-  const hostname = R.prop('hostname', snapshot);
-  const backupId = R.prop('id', snapshot);
-  // mariadb expects a specific timeformat of 'YYYY-MM-DD HH:mm:ss'
-  const created = moment(R.prop('time', snapshot)).format(
-    'YYYY-MM-DD HH:mm:ss'
-  );
-
-  // Determine source from the snapshot path.
-  // 1: `/data/*` --> means this is a PVC we use whatever is after `/data/` as the name of the backup
-  // 2: `*.tar` or `*.sql` --> current implementation with the name of the backup right before the ending, for example: foo.bar.tar (the backup is called `bar`)
-  let source;
-  const paths = R.prop('paths', snapshot);
-  const pattern = /^\/data\/([\w-]+)|([\w-]+).(?:sql|tar)$/;
-  if (R.isEmpty(paths)) {
-    source = 'unknown';
-  } else {
-    const path = R.head(paths);
-    if (!R.test(pattern, path)) {
-      source = 'unknown';
-    } else {
-      const matches = R.match(pattern, path);
-      if (R.prop(1, matches)) {
-        source = R.prop(1, matches);
-      } else if (R.prop(2, matches)) {
-        source = R.prop(2, matches);
-      } else {
-        source = 'unknown';
-      }
-    }
-  }
-
-  return addBackup(null, environmentId, source, backupId, created);
-};
-
 async function resticbackupSnapshotFinished(webhook: WebhookRequestData) {
   const { webhooktype, event, uuid, body } = webhook;
 
   try {
-    // The webhook contains all existing and new snapshots made for all
-    // environments. Group by hostname.
-    const incomingSnapshots = R.prop('snapshots', body);
-    const snapshotsByHostname = R.pipe(
-      // Remove pod names suffix from hostnames.
-      R.map(R.over(R.lensProp('hostname'), R.replace(/(-cli|-mariadb|-nginx|-solr|-node|-elasticsearch|-redis|-[\w]+-prebackuppod)$/, ''))),
-      R.groupBy(snapshot => snapshot.hostname),
-      R.toPairs()
-    )(incomingSnapshots);
+    const { name, bucket_name, backup_metrics } = body;
+    const environmentResult = await getEnvironmentByOpenshiftProjectName(bucket_name);
+    const environment = R.prop('environmentByOpenshiftProjectName', environmentResult)
 
-    for (const [hostname, snapshots] of snapshotsByHostname) {
-      // Get environment and existing backups.
-      const environmentResult = await getEnvironmentBackups(hostname);
-      const environment = R.prop('environmentByOpenshiftProjectName', environmentResult)
-
-      if (!environment) {
-        continue;
-      }
-
-      // Filter out snapshots that have already been recorded.
-      const existingBackupIds = R.pluck('backupId', environment.backups);
-      const newSnapshots = R.reject(snapshot => R.contains(snapshot.id, existingBackupIds), snapshots);
-
-      for (const snapshot of newSnapshots) {
-        try {
-          const newBackupResult = await saveSnapshotAsBackup(
-            snapshot,
-            environment.id
-          );
-          const newBackup = R.prop('addBackup', newBackupResult);
-          const meta = {
-            data: newBackup,
-            backupId: newBackup.backupId,
-            project: environment.project.name,
-            source: newBackup.source
-          };
-        } catch (error) {
-          // No error logging for now
-        }
-      }
+    if (!environment) {
+      logger.warn(`Skipping ${webhooktype}:${event}. Error: environment ${bucket_name} not found.`);
+      return;
     }
+
+    const backupId = R.prop('id', backup_metrics);
+    // mariadb expects a specific timeformat of 'YYYY-MM-DD HH:mm:ss'
+    const created = R.ifElse(
+      R.propSatisfies(timestamp => timestamp == 0, 'backup_end_timestamp'),
+      R.always(moment().format(
+        'YYYY-MM-DD HH:mm:ss'
+      )),
+      R.always(moment(R.prop('backup_end_timestamp', backup_metrics)).format(
+        'YYYY-MM-DD HH:mm:ss'
+      ))
+    )(backup_metrics);
+
+    let source;
+    const mountedPvc = R.path(['mounted_PVCs', 0], backup_metrics);
+    const folderName = R.pipe(
+      R.prop('Folder'),
+      R.replace(`${name}-`, ''),
+      R.replace('-prebackuppod', ''),
+    )(backup_metrics);
+
+    if (mountedPvc) {
+      source = mountedPvc;
+    } else if (folderName) {
+      source = folderName;
+    } else {
+      source = 'unknown';
+    }
+
+    await addBackup(null, environment.id, source, backupId, created);
 
     return;
   } catch (error) {
