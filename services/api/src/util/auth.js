86ed756bd4a3e01a771b761ea395bbb244a397c6
Brandon Williams <brandon@amazee.io>
2019-09-04 03:08:10 -0500

    Cache permission lookups for the duration of an api query

--- services/api/src/util/auth.js
+++ services/api/src/util/auth.js
@@ -83,9 +83,19 @@ const legacyHasPermission = (legacyCredentials) => {
   };
 };
 
-const keycloakHasPermission = (grant) => {
+const keycloakHasPermission = (grant, requestCache) => {
   return async (resource, scope, attributes = {}) => {
     const currentUserId = grant.access_token.content.sub;
+
+    // Check if the same set of permissions has been granted already for this
+    // api query.
+    // TODO Is it possible to ask keycloak for ALL permissions (given a project
+    // or group context) and cache a single query instead?
+    const cacheKey = `${currentUserId}:${resource}:${scope}:${JSON.stringify(attributes)}`;
+    const cachedPermissions = requestCache.get(cacheKey);
+    if (cachedPermissions !== undefined) {
+      return cachedPermissions;
+    }
 
     const serviceAccount = await keycloakGrantManager.obtainFromClientCredentials();
 
@@ -207,6 +217,7 @@ const keycloakHasPermission = (grant) => {
       const newGrant = await keycloakGrantManager.checkPermissions(authzRequest, request);
 
       if (newGrant.access_token.hasPermission(resource, scope)) {
+        requestCache.set(cacheKey, true);
         return;
       }
     } catch (err) {
@@ -214,6 +225,7 @@ const keycloakHasPermission = (grant) => {
       // denied conditions.
     }
 
+    requestCache.set(cacheKey, false);
     throw new Error(`Unauthorized: You don't have permission to "${scope}" on "${resource}".`);
   };
 };
