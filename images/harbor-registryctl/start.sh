c50c6980d6ca19fbf6d879c92193c25ffe10ceaf
Chris Davis <cdchris12@gmail.com>
2019-11-15 10:20:31 -0600

    Updating how Harbor's "EXT_ENDPOINT" is defined so that it can support being pulled from "LAGOON_ROUTES".

--- images/harbor-registryctl/start.sh
+++ images/harbor-registryctl/start.sh
@@ -18,6 +18,19 @@ set -e
 #fi
 
 if [[ -f "/etc/registry/pre_config.yml" ]]; then
+
+    # if HARBOR_NGINX_ENDPOINT is not defined, we try to load it from LAGOON_ROUTES
+    if [[ -z ${HARBOR_NGINX_ENDPOINT+x} ]]; then
+        REGEX="(https?://harbor[0-9A-Za-z\.-]+)"
+
+        if [[ $LAGOON_ROUTES =~ $REGEX ]]; then
+            export HARBOR_NGINX_ENDPOINT=${BASH_REMATCH[1]}
+        else
+            echo "Could not load harbor URL from LAGOON_ROUTES, please define via HARBOR_NGINX_ENDPOINT env variable"
+            exit 1
+        fi
+    fi
+    
     sed -e "s|\$HARBOR_NGINX_ENDPOINT|$HARBOR_NGINX_ENDPOINT|g" -e "s|\$STORAGE_AMAZON_REGION|$STORAGE_AMAZON_REGION|g" -e "s|\$STORAGE_AMAZON_BUCKET|$STORAGE_AMAZON_BUCKET|g" /etc/registry/pre_config.yml > /etc/registry/config.yml
 fi
 
