df1d4c2291db00b4e7aeedbdd6c98967e431587d
Chris Davis <cdchris12@gmail.com>
2019-12-02 12:55:06 -0600

    Removing errant comments, adding Postgres container docs, updating Harbor env variables, switching Harbor to amazeeio's Postgres 11 dockerfile, and changing postgres liveness probes from exec to TCP.

--- services/harborregistry/entrypoint.sh
+++ services/harborregistry/entrypoint.sh
@@ -31,7 +31,7 @@ if [[ -f "/etc/registry/pre_config.yml" ]]; then
         fi
     fi
     
-    sed -e "s|\$HARBOR_NGINX_ENDPOINT|$HARBOR_NGINX_ENDPOINT|g" -e "s|\$STORAGE_AMAZON_REGION|$STORAGE_AMAZON_REGION|g" -e "s|\$STORAGE_AMAZON_BUCKET|$STORAGE_AMAZON_BUCKET|g" /etc/registry/pre_config.yml > /etc/registry/config.yml
+    sed -e "s|\$HARBOR_NGINX_ENDPOINT|$HARBOR_NGINX_ENDPOINT|g" -e "s|\$HARBOR_REGISTRY_STORAGE_AMAZON_REGION|$HARBOR_REGISTRY_STORAGE_AMAZON_REGION|g" -e "s|\$HARBOR_REGISTRY_STORAGE_AMAZON_BUCKET|$HARBOR_REGISTRY_STORAGE_AMAZON_BUCKET|g" /etc/registry/pre_config.yml > /etc/registry/config.yml
 fi
 
 /harbor/install_cert.sh
