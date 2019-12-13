c069545127d96d5767a17dca72f6801de66e766a
Chris Davis <cdchris12@gmail.com>
2019-12-13 14:16:47 -0600

    Updating Clair container to possibly work with 1.10.0

--- services/harbor-clair/docker-entrypoint.sh
+++ services/harbor-clair/docker-entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 
 /harbor/install_cert.sh
-exec "/dumb-init" "--" "/clair/clair" "-config" "/etc/clair/config.yaml"
+exec "/dumb-init" "--" "/home/clair/clair" "-config" "/etc/clair/config.yaml"
 set +e
