908b0df6792cc8fad8846e2d3d248e65a7f437c8
Michael Schmid <michael@amazee.com>
2020-01-06 16:43:32 +0100

    run archive on 7th day 6-10am

--- helpers/k8up-initiate-archive.sh
+++ helpers/k8up-initiate-archive.sh
@@ -48,7 +48,7 @@ if oc get customresourcedefinition schedules.backup.appuio.ch > /dev/null; then
       TEMPLATE_PARAMETERS=()
 
       # Run Archive on Monday at 0300-0600
-      ARCHIVE_SCHEDULE=$( $(git rev-parse --show-toplevel)/images/oc-build-deploy-dind/scripts/convert-crontab.sh "${OPENSHIFT_PROJECT}" "M H(6-12) 2 * *")
+      ARCHIVE_SCHEDULE=$( $(git rev-parse --show-toplevel)/images/oc-build-deploy-dind/scripts/convert-crontab.sh "${OPENSHIFT_PROJECT}" "M H(3-9) 7 * *")
       TEMPLATE_PARAMETERS+=(-p ARCHIVE_SCHEDULE="${ARCHIVE_SCHEDULE}")
 
       TEMPLATE_PARAMETERS+=(-p ARCHIVE_BUCKET="${ARCHIVE_BUCKET}")
