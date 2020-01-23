9b0b4c97963757412805818c78b212026520e410
Tyler Ward <twardnw@gmail.com>
2020-01-23 08:41:31 -0600

    allow multiple container in single pod to generate correct deploy config

--- images/kubectl-build-deploy-dind/scripts/exec-kubectl-resources-with-images.sh
+++ images/kubectl-build-deploy-dind/scripts/exec-kubectl-resources-with-images.sh
@@ -5,14 +5,15 @@ else
   SERVICE_ROUTER_URL=""
 fi
 
-# Inject Pullable Images into Template
-TEMPLATE_ADDITIONAL_PARAMETERS=()
+
 # First check if we need multiple Images in this Template (check for `_SERVICE_IMAGE` - see underline)
 if [[ $(helm show values /kubectl-build-deploy/helmcharts/${SERVICE_TYPE} | grep images) ]]; then
+  # Inject Pullable Images into Template
+  HELM_IMAGE_VALUES=()
   while read line
   do
     # SERVICE_IMAGE Parameters are configured like "[SERVICETYPE]_SERVICE_IMAGE", we split the servicetype away and lowercase it
-    DEPLOYMENT_SERVICETYPE=$(echo $line | awk -F_ '{print $1}'  | tr '[:upper:]' '[:lower:]');
+    DEPLOYMENT_SERVICETYPE=$line
 
     # Load which pushed image matches this servicetype of this service name
     DEPLOYMENT_SERVICETYPE_IMAGE_NAME="${MAP_DEPLOYMENT_SERVICETYPE_TO_IMAGENAME[${SERVICE_NAME}:${DEPLOYMENT_SERVICETYPE}]}"
@@ -20,9 +21,10 @@ if [[ $(helm show values /kubectl-build-deploy/helmcharts/${SERVICE_TYPE} | grep
     DEPLOYMENT_SERVICETYPE_IMAGE_NAME_HASH="${IMAGE_HASHES[${DEPLOYMENT_SERVICETYPE_IMAGE_NAME}]}"
 
     # Add the Image Hash as Parameter of "[SERVICETYPE]_SERVICE_IMAGE"
-    TEMPLATE_ADDITIONAL_PARAMETERS+=(-p "${line}=${DEPLOYMENT_SERVICETYPE_IMAGE_NAME_HASH}")
-    helm template ${SERVICE_NAME} /kubectl-build-deploy/helmcharts/${SERVICE_TYPE} -f /kubectl-build-deploy/values.yaml --set image="${SERVICE_NAME_IMAGE_HASH}" | outputToYaml
-  done < <(helm show values /kubectl-build-deploy/helmcharts/${SERVICE_TYPE} | grep images | awk '{ print $1 }')
+    HELM_IMAGE_VALUES+=(--set "images.${line}=${DEPLOYMENT_SERVICETYPE_IMAGE_NAME_HASH}")
+  done < <(cat /kubectl-build-deploy/helmcharts/${SERVICE_TYPE}/values.yaml | shyaml keys images)
+  helm template ${SERVICE_NAME} /kubectl-build-deploy/helmcharts/${SERVICE_TYPE} -f /kubectl-build-deploy/values.yaml --set service_name="${SERVICE_NAME}"  "${HELM_IMAGE_VALUES[@]}" | outputToYaml
+
 # check if we need a single image to inject
 elif [[ $(helm show values /kubectl-build-deploy/helmcharts/${SERVICE_TYPE} | grep image) ]]; then
   SERVICE_NAME_IMAGE="${MAP_SERVICE_NAME_TO_IMAGENAME[${SERVICE_NAME}]}"
