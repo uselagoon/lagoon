6c5abb6ac9bb40bf33823c8a6afe89a7642612ff
Alanna <alanna.burke@amazee.com>
2020-01-24 14:30:42 -0600

    Adding code examples directory.

---
+++ docs/docs/examples/drupal8-composer-mariadb/web/sites/default/production.settings.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Lagoon Drupal 8 production environment configuration file.
+ *
+ * This file will only be included on production environments.
+ *
+ * It contains some defaults that the Lagoon team suggests, please edit them as required.
+ */
+
+// Don't show any error messages on the site (will still be shown in watchdog)
+$config['system.logging']['error_level'] = 'hide';
+
+// Expiration of cached pages on Varnish to 15 min.
+$config['system.performance']['cache']['page']['max_age'] = 900;
+
+// Aggregate CSS files on.
+$config['system.performance']['css']['preprocess'] = 1;
+
+// Aggregate JavaScript files on.
+$config['system.performance']['js']['preprocess'] = 1;
+
+// Disabling stage file proxy on production, with that the module can be enabled even on production.
+$config['stage_file_proxy.settings']['origin'] = FALSE;
