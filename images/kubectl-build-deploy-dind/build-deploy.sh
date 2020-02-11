e94c62359dc52829203cb7fd1709e51e422f42e5
Chris Davis <cdchris12@gmail.com>
2020-02-11 16:25:11 -0600

    Missed a few files

--- images/kubectl-build-deploy-dind/build-deploy.sh
+++ images/kubectl-build-deploy-dind/build-deploy.sh
@@ -43,12 +43,20 @@ else
 fi
 
 REGISTRY_SECRETS=()
+PRIVATE_REGISTRY_COUNTER=0
 
 set +x
 
+DEPLOYER_TOKEN=$(cat /var/run/secrets/lagoon/deployer/token)
+
+kubectl config set-credentials lagoon/kubernetes.default.svc --token="${DEPLOYER_TOKEN}"
+kubectl config set-cluster kubernetes.default.svc --insecure-skip-tls-verify=true --server=https://kubernetes.default.svc
+kubectl config set-context default/lagoon/kubernetes.default.svc --user=lagoon/kubernetes.default.svc --namespace="${NAMESPACE}" --cluster=kubernetes.default.svc
+kubectl config use-context default/lagoon/kubernetes.default.svc
+
 if [ ! -z ${INTERNAL_REGISTRY_URL} ] && [ ! -z ${INTERNAL_REGISTRY_USERNAME} ] && [ ! -z ${INTERNAL_REGISTRY_PASSWORD} ] ; then
   echo "docker login -u '${INTERNAL_REGISTRY_USERNAME}' -p '${INTERNAL_REGISTRY_PASSWORD}' ${INTERNAL_REGISTRY_URL}" | /bin/bash
-  kubectl create secret docker-registry lagoon-internal-registry-secret --docker-server=35.180.127.175 --docker-username=admin                       --docker-password=Harbor12345  --docker-email=oktayesgul@blabla.com
+  kubectl create secret docker-registry lagoon-internal-registry-secret --docker-server=${INTERNAL_REGISTRY_URL} --docker-username=${INTERNAL_REGISTRY_USERNAME} --docker-password=${INTERNAL_REGISTRY_PASSWORD} --dry-run -o yaml | kubectl apply -f -
   REGISTRY_SECRETS+=("lagoon-internal-registry-secret")
   #docker login "-u '{$INTERNAL_REGISTRY_USERNAME}' -p '{$INTERNAL_REGISTRY_PASSWORD}' '{$INTERNAL_REGISTRY_URL}'"
   REGISTRY=$INTERNAL_REGISTRY_URL # This will handle pointing Lagoon at the correct registry for non local builds
@@ -59,12 +67,57 @@ if [ ! -z ${INTERNAL_REGISTRY_URL} ] && [ ! -z ${INTERNAL_REGISTRY_USERNAME} ] &
 #  docker login -u=jenkins -p="${DOCKER_REGISTRY_TOKEN}" ${REGISTRY}
 fi
 
-DEPLOYER_TOKEN=$(cat /var/run/secrets/lagoon/deployer/token)
-
-kubectl config set-credentials lagoon/kubernetes.default.svc --token="${DEPLOYER_TOKEN}"
-kubectl config set-cluster kubernetes.default.svc --insecure-skip-tls-verify=true --server=https://kubernetes.default.svc
-kubectl config set-context default/lagoon/kubernetes.default.svc --user=lagoon/kubernetes.default.svc --namespace="${NAMESPACE}" --cluster=kubernetes.default.svc
-kubectl config use-context default/lagoon/kubernetes.default.svc
+##############################################
+### PRIVATE REGISTRY LOGINS
+##############################################
+# we want to be able to support private container registries
+# grab all the container-registries that are defined in the `.lagoon.yml` file
+PRIVATE_CONTAINER_REGISTRIES=($(cat .lagoon.yml | shyaml keys container-registries || echo ""))
+for PRIVATE_CONTAINER_REGISTRY in "${PRIVATE_CONTAINER_REGISTRIES[@]}"
+do
+  # check if a url is set, if none set proceed against docker hub
+  PRIVATE_CONTAINER_REGISTRY_URL=$(cat .lagoon.yml | shyaml get-value container-registries.$PRIVATE_CONTAINER_REGISTRY.url false)
+  if [ $PRIVATE_CONTAINER_REGISTRY_URL == "false" ]; then
+    echo "No 'url' defined for registry $PRIVATE_CONTAINER_REGISTRY, will proceed against docker hub";
+  fi
+  # check the username and passwords are defined in yaml
+  PRIVATE_CONTAINER_REGISTRY_USERNAME=$(cat .lagoon.yml | shyaml get-value container-registries.$PRIVATE_CONTAINER_REGISTRY.username false)
+  if [ $PRIVATE_CONTAINER_REGISTRY_USERNAME == "false" ]; then
+    echo "No 'username' defined for registry $PRIVATE_CONTAINER_REGISTRY"; exit 1;
+  fi
+  PRIVATE_CONTAINER_REGISTRY_PASSWORD=$(cat .lagoon.yml | shyaml get-value container-registries.$PRIVATE_CONTAINER_REGISTRY.password false)
+  if [[ $PRIVATE_CONTAINER_REGISTRY_PASSWORD == "false" ]]; then
+    echo "No 'password' defined for registry $PRIVATE_CONTAINER_REGISTRY"; exit 1;
+  fi
+  # if we have everything we need, we can proceed to logging in
+  if [ $PRIVATE_CONTAINER_REGISTRY_PASSWORD != "false" ]; then
+    PRIVATE_REGISTRY_CREDENTIAL=""
+    # check if we have a password defined anywhere in the api first
+    if [ ! -z "$LAGOON_PROJECT_VARIABLES" ]; then
+      PRIVATE_REGISTRY_CREDENTIAL=($(echo $LAGOON_PROJECT_VARIABLES | jq -r '.[] | select(.scope == "container_registry" and .name == "'$PRIVATE_CONTAINER_REGISTRY_PASSWORD'") | "\(.value)"'))
+    fi
+    if [ ! -z "$LAGOON_ENVIRONMENT_VARIABLES" ]; then
+      PRIVATE_REGISTRY_CREDENTIAL=($(echo $LAGOON_ENVIRONMENT_VARIABLES | jq -r '.[] | select(.scope == "container_registry" and .name == "'$PRIVATE_CONTAINER_REGISTRY_PASSWORD'") | "\(.value)"'))
+    fi
+    if [ -z $PRIVATE_REGISTRY_CREDENTIAL ]; then
+      #if no password defined in the lagoon api, pass the one in `.lagoon.yml` as a password
+      PRIVATE_REGISTRY_CREDENTIAL=$PRIVATE_CONTAINER_REGISTRY_PASSWORD
+    fi
+    if [ $PRIVATE_CONTAINER_REGISTRY_URL != "false" ]; then
+      echo "Attempting to log in to $PRIVATE_CONTAINER_REGISTRY_URL with user $PRIVATE_CONTAINER_REGISTRY_USERNAME - $PRIVATE_CONTAINER_REGISTRY_PASSWORD"
+      docker login --username $PRIVATE_CONTAINER_REGISTRY_USERNAME --password $PRIVATE_REGISTRY_CREDENTIAL $PRIVATE_CONTAINER_REGISTRY_URL
+      kubectl create secret docker-registry "lagoon-private-registry-${PRIVATE_REGISTRY_COUNTER}-secret" --docker-server=$PRIVATE_CONTAINER_REGISTRY_URL --docker-username=PRIVATE_CONTAINER_REGISTRY_USERNAME --docker-password=$PRIVATE_REGISTRY_REGISTRY_PASSWORD --dry-run -o yaml | kubectl apply -f -
+      REGISTRY_SECRETS+=("lagoon-private-registry-${PRIVATE_REGISTRY_COUNTER}-secret")
+      let PRIVATE_REGISTRY_COUNTER++
+    else
+      echo "Attempting to log in to docker hub with user $PRIVATE_CONTAINER_REGISTRY_USERNAME - $PRIVATE_CONTAINER_REGISTRY_PASSWORD"
+      docker login --username $PRIVATE_CONTAINER_REGISTRY_USERNAME --password $PRIVATE_REGISTRY_CREDENTIAL
+      kubectl create secret docker-registry "lagoon-private-registry-${PRIVATE_REGISTRY_COUNTER}-secret" --docker-server="https://index.docker.io/v1/" --docker-username=PRIVATE_CONTAINER_REGISTRY_USERNAME --docker-password=$PRIVATE_REGISTRY_REGISTRY_PASSWORD --dry-run -o yaml | kubectl apply -f -
+      REGISTRY_SECRETS+=("lagoon-private-registry-${PRIVATE_REGISTRY_COUNTER}-secret")
+      let PRIVATE_REGISTRY_COUNTER++
+    fi
+  fi
+done
 
 set -x
 
