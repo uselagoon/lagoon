c0ae6ada54f23be59c3a897d62336a4a2bcd2261
Justin Winter <justinlevi@gmail.com>
2020-02-24 17:41:36 -0500

    Finalizing basic conversion

--- services/kubernetesjobsmonitor/src/index.ts
+++ services/kubernetesjobsmonitor/src/index.ts
@@ -88,7 +88,7 @@ const getConfig = (url, token) => ({
   }
 });
 
-const projectExists = async (client, namespace) => {
+const projectExists = async (client: Api.ApiRoot, namespace: string) => {
   const namespaces = await client.api.v1.namespaces(namespace).get();
   if (
     namespaces.statusCode !== 200 &&
@@ -99,6 +99,146 @@ const projectExists = async (client, namespace) => {
 
   return true;
 };
+
+const jobsLogGet = async (client: Api.ApiRoot, namespace: string, jobName: string) => {
+  const pods = client.api.v1.namespaces(namespace).pods.get({ 
+    qs: { labelSelector: `job-name=${jobName}` } 
+  });
+  const podNames = pods.items.map(pod => pod.metadata.name);
+
+  // Combine all logs from all pod(s)
+  let finalLog = '';
+  for (const podName of podNames) {
+    const podLog = client.api.v1.namespaces(namespace).pods(podName).log.get();
+
+    finalLog =
+      finalLog +
+      `
+========================================
+Logs on pod ${podName}
+========================================
+${podLog}`;
+    }
+
+    return finalLog;
+}
+
+const deleteJob = async (client: Api.ApiRoot, namespace: string, jobName: string) => {
+  try {
+    // const jobDelete = promisify(
+    //   batchApi.namespaces(openshiftProject).jobs(jobName).delete
+    // );
+    const jobDelete = await client.apis.batch.v1.namespaces(namespace).jobs(jobName).get()
+    await jobDelete({
+      body: {
+        kind: 'DeleteOptions',
+        apiVersion: 'v1',
+        propagationPolicy: 'Foreground',
+      },
+    });
+  } catch (err) {
+    logger.error(`Couldn't delete job ${jobName}. Error: ${err}`);
+  }
+};
+
+const getJobInfo = async (client: Api.ApiRoot, namespace: string, jobName: string, taskId: string) => {
+  try {
+    return client.apis.batch.v1.namespaces(namespace).jobs(jobName).get()
+  } catch (err) {
+    if (err.code == 404) {
+      logger.error(`Job ${jobName} does not exist, bailing`);
+      failTask(taskId);
+      return;
+    } else {
+      logger.error(err);
+      throw new Error();
+    }
+  }
+}
+
+const updateLagoonTask = async (jobInfo, jobStatus, taskId, project, jobName) => {
+  // Update lagoon task
+  try {
+    const convertDateFormat = R.init;
+    const dateOrNull = R.unless(R.isNil, convertDateFormat);
+    let completedDate = dateOrNull(jobInfo.status.completionTime);
+
+    if (jobStatus === 'failed') {
+      completedDate = dateOrNull(jobInfo.status.conditions[0].lastTransitionTime);
+    }
+
+    await updateTask(taskId, {
+      status: jobStatus.toUpperCase(),
+      created: convertDateFormat(jobInfo.metadata.creationTimestamp),
+      started: dateOrNull(jobInfo.status.startTime),
+      completed: completedDate
+    });
+  } catch (error) {
+    logger.error(
+      `Could not update task ${project.name} ${jobName}. Message: ${error}`
+    );
+  }
+}
+
+const saveAndLog = async (client, jobStatus, project, task, namespace, jobName, jobInfo, meta) => {
+  switch (jobStatus) {
+    case 'active':
+      sendToLagoonLogs(
+        'info',
+        project.name,
+        '',
+        `task:job-openshift:${jobStatus}`,
+        meta,
+        `*[${project.name}]* Task \`${task.id}\` *${task.name}* active`
+      );
+      throw new JobNotCompletedYet(
+        `*[${project.name}]* Task \`${task.id}\` *${task.name}* active`
+      );
+
+    case 'failed':
+      await saveTaskLog(jobName, project.name, jobInfo, await jobsLogGet(client, namespace, jobName));
+      await deleteJob(client, name, jobName);
+      sendToLagoonLogs(
+        'error',
+        project.name,
+        '',
+        `task:job-openshift:${jobStatus}`,
+        meta,
+        `*[${project.name}]* Task \`${task.id}\` *${task.name}* failed.`
+      );
+      break;
+
+    case 'succeeded':
+      await saveTaskLog(jobName, project.name, jobInfo, await jobsLogGet(client, namespace, jobName));
+      await deleteJob(client, namespace, jobName);
+      sendToLagoonLogs(
+        'info',
+        project.name,
+        '',
+        `task:job-openshift:${jobStatus}`,
+        meta,
+        `*[${project.name}]* Task \`${task.id}\` *${task.name}* succeeded.`
+      );
+      break;
+
+    default:
+      sendToLagoonLogs(
+        'info',
+        project.name,
+        '',
+        `task:job-openshift:${jobStatus}`,
+        meta,
+        `*[${project.name}]* Task \`${task.id}\` *${
+          task.name
+        }* phase ${jobStatus}`
+      );
+      throw new JobNotCompletedYet(
+        `*[${project.name}]* Task \`${task.id}\` *${
+          task.name
+        }* phase ${jobStatus}`
+      );
+  }
+}
 
 const messageConsumer = async msg => {
   const { project, task, environment } = JSON.parse(msg.content.toString());
@@ -119,25 +259,13 @@ const messageConsumer = async msg => {
   }
 
   const jobName = `${namespace}-${task.id}`;
+  const jobInfo = getJobInfo(client, namespace, jobName, taskId);
+  const jobStatus = getJobStatus(jobInfo);
 
-  let jobInfo;
-  try {
-    jobInfo = await client.apis.batch.v1.namespaces(namespace).jobs(jobName).get()
-  } catch (err) {
-    if (err.code == 404) {
-      logger.error(`Job ${jobName} does not exist, bailing`);
-      failTask(taskId);
-      return;
-    } else {
-      logger.error(err);
-      throw new Error();
-    }
-  }
+  await updateLagoonTask(jobInfo, jobStatus, taskId, project, jobName);
+  const meta = JSON.parse(msg.content.toString());
 
-  
-
-
-
+  saveAndLog(client, jobStatus, project, task, namespace, jobName, jobInfo, meta);
 };
 
 const saveTaskLog = async (jobName, projectName, jobInfo, log) => {
