ef7a44ffeda9617c3e1b4b46fae74bca09811dd8
Justin Winter <justinlevi@gmail.com>
2020-03-03 08:40:24 -0500

    (WIP) Checking if Job exists before trying to cancel and also updating lagoon

--- services/kubernetesmisc/src/handlers/kubernetesBuildCancel.ts
+++ services/kubernetesmisc/src/handlers/kubernetesBuildCancel.ts
@@ -1,3 +1,4 @@
+import * as R from 'ramda';
 import { getOpenShiftInfoForProject, updateTask } from '@lagoon/commons/src/api';
 import { logger } from "@lagoon/commons/src/local-logging";
 import { sendToLagoonLogs } from '@lagoon/commons/src/logs';
@@ -24,6 +25,36 @@ const generateSanitizedNames = (project, environment, projectInfo) => {
   }
 };
 
+const getJobStatus = jobInfo => {
+  if (R.isEmpty(jobInfo.body.status)) {
+    return 'active';
+  }
+
+  if (R.propOr(false, 'active', jobInfo.body.status)) {
+    return 'active';
+  }
+
+  if (R.propOr(false, 'failed', jobInfo.body.status)) {
+    return 'failed';
+  }
+
+  if (R.propOr(false, 'succeeded', jobInfo.body.status)) {
+    return 'succeeded';
+  }
+
+  return 'unknown';
+};
+
+const failTask = async taskId => {
+  try {
+    await updateTask(taskId, {
+      status: 'FAILED'
+    });
+  } catch (error) {
+    logger.error(`Could not fail task ${taskId}. Message: ${error}`);
+  }
+};
+
 const getUrlTokenFromProjectInfo = (projectOpenShift, name) => {
   try {
     const url = projectOpenShift.openshift.consoleUrl.replace(/\/$/, '');
@@ -45,6 +76,43 @@ const getConfig = (url, token) => ({
   }
 });
 
+const getJobInfo = async (client: Api.ApiRoot, namespace: string, jobName: string, taskId: string) => {
+  try {
+    return client.apis.batch.v1.namespaces(namespace).jobs(jobName).get()
+  } catch (err) {
+    if (err.code == 404) {
+      return undefined;
+    } else {
+      logger.error(err);
+      throw new Error();
+    }
+  }
+}
+
+const updateLagoonTask = async (jobInfo, jobStatus, taskId, project, jobName) => {
+  // Update lagoon task
+  try {
+    const convertDateFormat = R.init;
+    const dateOrNull = R.unless(R.isNil, convertDateFormat);
+    let completedDate = dateOrNull(jobInfo.body.status.completionTime);
+
+    if (jobStatus === 'failed') {
+      completedDate = dateOrNull(jobInfo.body.status.conditions[0].lastTransitionTime);
+    }
+
+    await updateTask(taskId, {
+      status: jobStatus.toUpperCase(),
+      created: convertDateFormat(jobInfo.body.metadata.creationTimestamp),
+      started: dateOrNull(jobInfo.body.status.startTime),
+      completed: completedDate
+    });
+  } catch (error) {
+    logger.error(
+      `Could not update task ${project.name} ${jobName}. Message: ${error}`
+    );
+  }
+}
+
 const deleteJob = async (client: Api.ApiRoot, namespace: string, jobName: string) => {
   try {
     const options = {
@@ -62,15 +130,28 @@ const deleteJob = async (client: Api.ApiRoot, namespace: string, jobName: string
 };
 
 const kubernetesBuildCancel = async (data: any) => {
-  const { build: { name: buildName }, project, environment } = data;
+  const { build: { name: buildName }, project, task, environment } = data;
+  // const { project, task, environment } = JSON.parse(msg.content.toString());
 
+  const taskId = typeof task.id === 'string' ? parseInt(task.id, 10) : task.id;
   const { project: projectInfo } = await getOpenShiftInfoForProject(project.name);
   const { url, token } = getUrlTokenFromProjectInfo(projectInfo, project.name);
   const config: ClientConfiguration = getConfig(url, token);
   const client = new Client({ config });
   const { namespace, safeProjectName } = generateSanitizedNames(project, environment, projectInfo);
 
-  const deleteResult = deleteJob(client, namespace, buildName);
+  // Check that job is still active
+  const jobName = `${namespace}-${task.id}`;
+  const jobInfo = await getJobInfo(client, namespace, jobName, taskId);
+  if (!jobInfo) {
+    logger.error(`Job ${jobName} does not exist, bailing`);
+    failTask(taskId);
+    return;
+  }
+  // const jobStatus = getJobStatus(jobInfo);
+  
+  await deleteJob(client, namespace, buildName);
+  await updateLagoonTask(jobInfo, 'CANCELLED', taskId, project, jobName);
 
   logger.verbose(`${namespace}: Cancelling build: ${buildName}`);
 
