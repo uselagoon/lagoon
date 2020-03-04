3139c6e3c6844b93e62022fba71f97090d9420ee
Ben Jackson <b@benjackson.email>
2020-03-05 09:39:27 +1100

    fix up route migration task, this needs a bit of work on the actual resource creation

--- services/openshiftmisc/src/handlers/routeMigration.js
+++ services/openshiftmisc/src/handlers/routeMigration.js
@@ -7,12 +7,12 @@ const { logger } = require('@lagoon/commons/src/local-logging');
 const { sendToLagoonLogs } = require('@lagoon/commons/src/logs');
 const {
   getOpenShiftInfoForProject,
+  updateProject,
 } = require('@lagoon/commons/src/api');
 const { RouteMigration } = require('@lagoon/commons/src/openshiftApi');
 
-
 async function routeMigration (data: Object) {
-  const { projectName, sourceBranchName, destinationBranchName } = data;
+  const { projectName, activeProductionEnvironment, standbyProductionEnvironment } = data;
 
   const result = await getOpenShiftInfoForProject(projectName);
   const projectOpenShift = result.project;
@@ -21,8 +21,8 @@ async function routeMigration (data: Object) {
     string.toLocaleLowerCase().replace(/[^0-9a-z-]/g, '-');
 
   try {
-    var safeBranchName = ocsafety(sourceBranchName);
-    var safeDestinationBranchName = ocsafety(destinationBranchName);
+    var safeActiveProductionEnvironment = ocsafety(activeProductionEnvironment);
+    var safeStandbyProductionEnvironment = ocsafety(standbyProductionEnvironment);
     var safeProjectName = ocsafety(projectName);
     var openshiftConsole = projectOpenShift.openshift.consoleUrl.replace(
       /\/$/,
@@ -31,15 +31,15 @@ async function routeMigration (data: Object) {
     var openshiftToken = projectOpenShift.openshift.token || '';
     var openshiftProject = projectOpenShift.openshiftProjectPattern
       ? projectOpenShift.openshiftProjectPattern
-          .replace('${branch}', safeBranchName)
+          .replace('${branch}', safeActiveProductionEnvironment)
           .replace('${project}', safeProjectName)
-      : `${safeProjectName}-${safeBranchName}`;
+      : `${safeProjectName}-${safeActiveProductionEnvironment}`;
     // create the destination openshift project name
     var destinationOpenshiftProject = projectOpenShift.openshiftProjectPattern
       ? projectOpenShift.openshiftProjectPattern
-          .replace('${branch}', safeDestinationBranchName)
+          .replace('${branch}', safeStandbyProductionEnvironment)
           .replace('${project}', safeProjectName)
-      : `${safeProjectName}-${safeDestinationBranchName}`;
+      : `${safeProjectName}-${safeStandbyProductionEnvironment}`;
   } catch (error) {
     logger.error(`Error while loading information for project ${projectName}`);
     logger.error(error);
@@ -76,21 +76,54 @@ async function routeMigration (data: Object) {
   });
 
 
-  // @TODO: check if the routemigrate resource already exists and we just patch the annotation and destination instead
+  // @TODO: this seems a bit silly, might be a better way to do it. but `.patch` on the routemigrates resource fails with
+  // `message=the body of the request was in an unknown format - accepted media types include: application/json-patch+json`
   try {
-    const migrateRoutesPost = promisify(
-        dioscuri.ns(openshiftProject).routemigrates.post
+    const cancelBuildPatch = promisify(
+      dioscuri.ns(openshiftProject).routemigrates(openshiftProject).delete
     );
-    await migrateRoutesPost({
-      body: migrateRoutes(openshiftProject, destinationOpenshiftProject)
+    await cancelBuildPatch({
+      body: {}
     });
-    logger.verbose(`${openshiftProject}: created routeMigration`);
+    await sleep(2000); // sleep a bit after deleting
+    try {
+      const migrateRoutesPost = promisify(
+          dioscuri.ns(openshiftProject).routemigrates.post
+      );
+      await migrateRoutesPost({
+        body: migrateRoutes(openshiftProject, destinationOpenshiftProject)
+      });
+      logger.verbose(`${openshiftProject}: created routeMigration resource`);
+    } catch (err) {
+        logger.error(err);
+        throw new Error();
+    }
   } catch (err) {
-    logger.error(err);
-    throw new Error();
+    try {
+      const migrateRoutesPost = promisify(
+          dioscuri.ns(openshiftProject).routemigrates.post
+      );
+      await migrateRoutesPost({
+        body: migrateRoutes(openshiftProject, destinationOpenshiftProject)
+      });
+      logger.verbose(`${openshiftProject}: created routeMigration resource`);
+    } catch (err) {
+        logger.error(err);
+        throw new Error();
+    }
   }
 
-  logger.verbose(`${openshiftProject}: Creating routeMigration: ${destinationOpenshiftProject}`);
+  // swap the active/standby in lagoon by updating the project
+  try {
+    const response = await updateProject(projectOpenShift.id, {
+      activeProductionEnvironment: projectOpenShift.standbyProductionEnvironment,
+      standbyProductionEnvironment: projectOpenShift.activeProductionEnvironment,
+    });
+  } catch (err) {
+      logger.error(err);
+      throw new Error();
+  }
+  logger.verbose(`${openshiftProject}: Active/Standby switch updated in lagoon`);
 
   sendToLagoonLogs(
     'start',
