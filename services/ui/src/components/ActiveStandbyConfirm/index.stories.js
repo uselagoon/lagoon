b2cd99dfbfce0a613633cc33c777736b36d387a9
Justin Winter <justinlevi@gmail.com>
2020-03-23 18:02:43 -0400

    Storybook Cleanup

--- services/ui/src/components/ActiveStandbyConfirm/index.stories.js
+++ services/ui/src/components/ActiveStandbyConfirm/index.stories.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { action } from '@storybook/addon-actions';
-import DeleteConfirm, { DeleteConfirm as DeleteConfirmBaseComponent } from './index';
+import ActiveStandby, { ActiveStandby as ActiveStandbyBaseComponent } from './index';
 
 export default {
-  component: DeleteConfirmBaseComponent,
-  title: 'Components/Delete and Confirm',
+  component: ActiveStandbyBaseComponent,
+  title: 'Components/ActiveStandby and Confirm',
   decorators: [
     storyFn => {
       const [open, setOpen] = useState(true);
@@ -14,41 +14,36 @@ export default {
         openBoolean: open,
         openModalFunction: () => { actionOpen(); setOpen(true); },
         closeModalFunction: () => { actionClose(); setOpen( false); },
-        onDeleteFunction: action('delete-button-pressed'),
-        setInputValueFunction: action('input-value-update-requested'),
+        onProceedFunction: action('active-standby-button-pressed'),
       });
     },
   ],
 }
 
-export const Default = ({ onDeleteFunction, setInputValueFunction }) => (
-  <DeleteConfirm
-    deleteType="environment"
-    deleteName="Forty-two"
-    onDelete={onDeleteFunction}
+export const Default = ({ onProceedFunction, setInputValueFunction }) => (
+  <ActiveStandby
+    activeEnvironment="Master-a"
+    standbyEnvironment="Master-b"
+    onProceed={onProceedFunction}
   />
 );
 
-export const WithConfirmationBlocked = ({ onDeleteFunction, setInputValueFunction, openBoolean, openModalFunction, closeModalFunction }) => (
-  <DeleteConfirmBaseComponent
+export const WithConfirmationBlocked = ({ onProceedFunction, setInputValueFunction, openBoolean, openModalFunction, closeModalFunction }) => (
+  <ActiveStandbyBaseComponent
     deleteType="environment"
     deleteName="Forty-two"
-    onDelete={onDeleteFunction}
-    inputValue=""
-    setInputValue={setInputValueFunction}
+    onProceed={onProceedFunction}
     open={openBoolean}
     openModal={openModalFunction}
     closeModal={closeModalFunction}
   />
 );
 
-export const WithConfirmationAllowed = ({ onDeleteFunction, setInputValueFunction, openBoolean, openModalFunction, closeModalFunction }) => (
-  <DeleteConfirmBaseComponent
+export const WithConfirmationAllowed = ({ onProceedFunction, setInputValueFunction, openBoolean, openModalFunction, closeModalFunction }) => (
+  <ActiveStandbyBaseComponent
     deleteType="environment"
     deleteName="Forty-two"
-    onDelete={onDeleteFunction}
-    inputValue="Forty-two"
-    setInputValue={setInputValueFunction}
+    onProceed={onProceedFunction}
     open={openBoolean}
     openModal={openModalFunction}
     closeModal={closeModalFunction}
