e88df80df1f06c980982ec7183cff110ce88e09a
Ben Jackson <b@benjackson.email>
2020-03-26 22:02:08 +1100

    refactor the update task function

--- services/openshiftmisc/src/handlers/routeMigration.js
+++ services/openshiftmisc/src/handlers/routeMigration.js
@@ -13,6 +13,15 @@ const {
 const { RouteMigration } = require('@lagoon/commons/src/openshiftApi');
 const convertDateFormat = R.init;
 
+const pause = duration => new Promise(res => setTimeout(res, duration));
+const retry = (retries, fn, delay = 1000) =>
+  fn().catch(
+    err =>
+      retries > 1
+        ? pause(delay).then(() => retry(retries - 1, fn, delay))
+        : Promise.reject(err)
+  );
+
 async function routeMigration (data: Object) {
   const { projectName, productionEnvironment, standbyProductionEnvironment, task } = data;
 
@@ -84,6 +93,31 @@ async function routeMigration (data: Object) {
     }
   });
 
+  const routeMigratesGet = promisify(
+    dioscuri.ns(openshiftProject).routemigrates.get
+  );
+
+  const routeMigrateDelete = async name => {
+    const deleteFn = promisify(dioscuri.ns(openshiftProject).routemigrates(openshiftProject).delete);
+    return deleteFn({
+      body: {}
+    });
+  };
+
+  const hasNoRouteMigrate = () =>
+    new Promise(async (resolve, reject) => {
+      const routeMigrates = await routeMigratesGet();
+      if (routeMigrates.items.length === 0) {
+        logger.info(`${openshiftProject}: RouteMigrate deleted`);
+        resolve();
+      } else {
+        logger.info(
+          `${openshiftProject}: RouteMigrate not deleted yet, will try again in 2sec`
+        );
+        reject();
+      }
+    });
+
   // check that the namespaces exist for source and destination before we try and move any routes
   try {
     // check source
@@ -100,45 +134,40 @@ async function routeMigration (data: Object) {
     throw new Error
   }
 
-  // @TODO: this seems a bit silly, might be a better way to do it. but `.patch` on the routemigrates resource fails with,
-  // research says this is because crd is not supported to be patched
-  // `message=the body of the request was in an unknown format - accepted media types include: application/json-patch+json`
+  // check if there is already a route migrate resource, delete it if there is
   try {
-    const migrateRoutesDelete = promisify(
-      dioscuri.ns(openshiftProject).routemigrates(openshiftProject).delete
-    );
-    await migrateRoutesDelete({
-      body: {}
-    });
-    await new Promise(resolve => setTimeout(resolve, 10000)); // sleep a bit after deleting
-    try {
-      const migrateRoutesPost = promisify(
-          dioscuri.ns(openshiftProject).routemigrates.post
+    const routeMigrates = await routeMigratesGet();
+    for (let routeMigrate of routeMigrates.items) {
+      await routeMigrateDelete(routeMigrate.metadata.name);
+      logger.info(
+        `${openshiftProject}: Deleting RouteMigrate ${routeMigrate.metadata.name}`
       );
-      await migrateRoutesPost({
-        body: migrateRoutes(openshiftProject, destinationOpenshiftProject)
-      });
-      logger.verbose(`${openshiftProject}: created routeMigration resource`);
+    }
+    // RouteMigrates are deleted quickly, but we still have to wait before we attempt to create the new one
+    try {
+      await retry(10, hasNoRouteMigrate, 2000);
     } catch (err) {
-        logger.error(err);
-        throw new Error();
+      throw new Error(
+        `${openshiftProject}: RouteMigrate not deleted`
+      );
     }
   } catch (err) {
-    await new Promise(resolve => setTimeout(resolve, 1000)); // sleep a bit before creating
-    try {
-      const migrateRoutesPost = promisify(
-          dioscuri.ns(openshiftProject).routemigrates.post
-      );
-      await migrateRoutesPost({
-        body: migrateRoutes(openshiftProject, destinationOpenshiftProject)
-      });
-      logger.verbose(`${openshiftProject}: created routeMigration resource`);
-    } catch (err) {
-        logger.error(err);
-        throw new Error();
-    }
+      logger.info(`${openshiftProject}: RouteMigrate doesn't exist`); // proceed if it doesn't exist
   }
 
+  // add the routemigrate resource
+  try {
+    const routeMigratePost = promisify(
+        dioscuri.ns(openshiftProject).routemigrates.post
+    );
+    await routeMigratePost({
+      body: migrateRoutes(openshiftProject, destinationOpenshiftProject)
+    });
+    logger.verbose(`${openshiftProject}: RouteMigrate resource created`);
+  } catch (err) {
+      logger.error(err);
+      throw new Error();
+  }
 
   sendToLagoonLogs(
     'info',
@@ -148,110 +177,102 @@ async function routeMigration (data: Object) {
     data,
     `*[${projectName}]* Route Migration between environments *${destinationOpenshiftProject}* started`
   );
-  // check the route migrate resource for the status conditions, only update lagoon on a completed task
-  var whileCount = 0;
-  var breakLoop = false;
-  try {
-    while (whileCount < 10 && !breakLoop) {
-      const migrateRoutesGet = promisify(
-          dioscuri.ns(openshiftProject).routemigrates(openshiftProject).get
-      );
-      routeMigrateStatus = await migrateRoutesGet();
-      try {
-        for (i = 0; i < routeMigrateStatus.status.conditions.length; i++) {
-          logger.verbose(`${openshiftProject}: active/standby switch status: ${routeMigrateStatus.status.conditions[i].type}`);
+
+  const routeMigrateGet = promisify(
+    dioscuri.ns(openshiftProject).routemigrates(openshiftProject).get
+  );
+
+  // this will check the resource in openshift, then updates the task in the api
+  const updateActiveStandbyTask = () => {
+    return (new Promise(async (resolve, reject) => {
+      var exitResolve = false;
+      routeMigrateStatus = await routeMigrateGet();
+      if (routeMigrateStatus === undefined || routeMigrateStatus.status === undefined || routeMigrateStatus.status.conditions === undefined) {
+        logger.info(`${openshiftProject}: active/standby switch not ready, will try again in 2sec`);
+      } else {
+        for (let i = 0; i < routeMigrateStatus.status.conditions.length; i++) {
           switch (routeMigrateStatus.status.conditions[i].type ) {
             case 'started':
-                try {
-                  // update the task to started
-                  var created = convertDateFormat(routeMigrateStatus.status.conditions[i].lastTransitionTime)
-                  await updateTask(task.id, {
-                    status: 'ACTIVE',
-                    created: created,
-                  });
-                } catch (err) {
-                    logger.error(err);
-                    throw new Error();
-                }
+              // update the task to started
+              var created = convertDateFormat(routeMigrateStatus.status.conditions[i].lastTransitionTime)
+              await updateTask(task.id, {
+                status: 'ACTIVE',
+                created: created,
+              });
               break;
             case 'failed':
-                try {
-                  // update the task to failed
-                  var created = convertDateFormat(routeMigrateStatus.status.conditions[i].lastTransitionTime)
-                  await updateTask(task.id, {
-                    status: 'FAILED',
-                    completed: created,
-                  });
-                  var condition = new Object();
-                  // send a log off with the status information
-                  condition.condition = routeMigrateStatus.status.conditions[i].condition
-                  condition.activeRoutes = routeMigrateStatus.spec.routes.activeRoutes
-                  condition.standbyRoutes = routeMigrateStatus.spec.routes.standbyRoutes
-                  var conditionStr= JSON.stringify(condition);
-                  await saveTaskLog(
-                    'active-standby-switch',
-                    projectOpenShift.name,
-                    'failed',
-                    task.uuid,
-                    conditionStr,
-                  );
-                } catch (err) {
-                    logger.error(err);
-                    throw new Error();
-                }
-              return breakLoop = true;
+              // update the task to failed
+              var created = convertDateFormat(routeMigrateStatus.status.conditions[i].lastTransitionTime)
+              await updateTask(task.id, {
+                status: 'FAILED',
+                completed: created,
+              });
+              var condition = new Object();
+              // send a log off with the status information
+              condition.condition = routeMigrateStatus.status.conditions[i].condition
+              condition.activeRoutes = routeMigrateStatus.spec.routes.activeRoutes
+              condition.standbyRoutes = routeMigrateStatus.spec.routes.standbyRoutes
+              var conditionStr= JSON.stringify(condition);
+              await saveTaskLog(
+                'active-standby-switch',
+                projectOpenShift.name,
+                'failed',
+                task.uuid,
+                conditionStr,
+              );
+              logger.info(`${openshiftProject}: active/standby switch failed`);
+              exitResolve = true;
+              break;
             case 'completed':
               // swap the active/standby in lagoon by updating the project
-              try {
-                const response = await updateProject(projectOpenShift.id, {
-                  productionEnvironment: safeStandbyProductionEnvironment,
-                  standbyProductionEnvironment: safeActiveProductionEnvironment,
-                  productionRoutes: routeMigrateStatus.spec.routes.activeRoutes,
-                  standbyRoutes: routeMigrateStatus.spec.routes.standbyRoutes,
-                });
-                // update the task to completed
-                var created = convertDateFormat(routeMigrateStatus.status.conditions[i].lastTransitionTime)
-                await updateTask(task.id, {
-                  status: 'SUCCEEDED',
-                  completed: created,
-                });
-                // send a log off with the status information
-                var condition = new Object();
-                condition.condition = routeMigrateStatus.status.conditions[i].condition
-                condition.activeRoutes = routeMigrateStatus.spec.routes.activeRoutes
-                condition.standbyRoutes = routeMigrateStatus.spec.routes.standbyRoutes
-                var conditionStr= JSON.stringify(condition);
-                await saveTaskLog(
-                  'active-standby-switch',
-                  projectOpenShift.name,
-                  'succeeded',
-                  task.uuid,
-                  conditionStr,
-                );
-              } catch (err) {
-                  logger.error(err);
-                  throw new Error();
-              }
-              logger.verbose(`${openshiftProject}: active/standby switch updated in lagoon`);
-              return breakLoop = true;
+              const response = await updateProject(projectOpenShift.id, {
+                productionEnvironment: safeStandbyProductionEnvironment,
+                standbyProductionEnvironment: safeActiveProductionEnvironment,
+                productionRoutes: routeMigrateStatus.spec.routes.activeRoutes,
+                standbyRoutes: routeMigrateStatus.spec.routes.standbyRoutes,
+              });
+              // update the task to completed
+              var created = convertDateFormat(routeMigrateStatus.status.conditions[i].lastTransitionTime)
+              await updateTask(task.id, {
+                status: 'SUCCEEDED',
+                completed: created,
+              });
+              // send a log off with the status information
+              var condition = new Object();
+              condition.condition = routeMigrateStatus.status.conditions[i].condition
+              condition.activeRoutes = routeMigrateStatus.spec.routes.activeRoutes
+              condition.standbyRoutes = routeMigrateStatus.spec.routes.standbyRoutes
+              var conditionStr= JSON.stringify(condition);
+              await saveTaskLog(
+                'active-standby-switch',
+                projectOpenShift.name,
+                'succeeded',
+                task.uuid,
+                conditionStr,
+              );
+              logger.info(`${openshiftProject}: active/standby switch completed`);
+              exitResolve = true;
+              break;
           }
         }
-      } catch (err) {
-        try {
-          await updateTask(task.id, {
-            status: 'ACTIVE',
-          });
-        } catch (err) {
-            logger.error(err);
-            throw new Error();
-        }
-        logger.verbose(`${openshiftProject}: active/standby switch waiting still`);
       }
-      await new Promise(resolve => setTimeout(resolve, 5000)); // wait for a bit between getting the resource
-    }
+      // handle the exit here
+      if (exitResolve == true) {
+        resolve();
+      } else {
+        logger.info(`${openshiftProject}: active/standby switch not ready, will try again in 2sec`);
+        reject();
+      }
+    })).catch((errorMessage) => {return exitResolve});
+  }
+
+  try {
+    // actually run the task that updates the task
+    await retry(10, updateActiveStandbyTask, 2000);
   } catch (err) {
-      logger.error(err);
-      throw new Error();
+    throw new Error(
+      `${openshiftProject}: active/standby task is taking too long ${err}`
+    );
   }
 }
 
