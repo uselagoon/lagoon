3512fac84cdb13562e8cad10bd63664b88781176
Tim Clifford <tim.clifford@amazee.com>
2020-04-01 20:58:41 +0100

    ALBA-152: Problems components and graphql

---
+++ services/ui/src/lib/fragment/Problem.js
@@ -0,0 +1,15 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  fragment problemFields on Problem {
+      id
+      identifier
+      data
+      severity
+      source
+      service
+      created
+      deleted
+      severityScore
+  }
+`;
