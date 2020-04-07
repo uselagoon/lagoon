8e2e7af866fd08e82d1412dba7a2f4a659736e9a
Tim Clifford <tim.clifford@amazee.com>
2020-04-07 10:34:18 +0100

    Refactoring problems

---
+++ services/ui/src/components/Accordion/index.js
@@ -0,0 +1,36 @@
+import React, { useState, Fragment } from "react";
+import PropTypes from "prop-types";
+import moment from 'moment';
+
+const Accordion = ({ children, defaultValue = true, className = "", onToggle, heading }) => {
+    const [visibility, setVisibility] = useState(defaultValue);
+    return (
+        <div className={className}>
+            <div className="accordion-heading" onClick={() => {
+                setVisibility(!visibility);
+                if (onToggle) onToggle(!visibility);
+            }}>
+                <div className="problemid">{heading.identifier}</div>
+                <div className="created">
+                    {moment
+                        .utc(heading.created)
+                        .local()
+                        .format('DD MMM YYYY, HH:mm:ss (Z)')}
+                </div>
+                <div className="source">{heading.source}</div>
+                <div className="severity">{heading.severity}</div>
+                <div className="severityscore">{heading.severityScore}</div>
+            </div>
+
+            {visibility ? <Fragment>{children}</Fragment> : null}
+        </div>
+    );
+};
+
+Accordion.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.any.isRequired,
+    onToggle: PropTypes.func,
+};
+
+export default Accordion;
