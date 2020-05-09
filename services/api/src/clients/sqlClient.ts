e38818cc3aff63e3a32600d07ec260f6f38f83fa
Chris Davis <chris.davis@amazee.io>
2020-05-09 17:12:25 -0500

    AIOSEC-41: Adding Keycloak DB and API DB password generation. (#4)
    * Adding Keycloak DB and API DB password generation.
    
    Co-authored-by: Michael Schmid <michael@amazee.com>

--- services/api/src/clients/sqlClient.ts
+++ services/api/src/clients/sqlClient.ts
@@ -2,19 +2,21 @@ import Client from 'mariasql';
 
 import * as logger from '../logger';
 
+const { API_DB_PASSWORD, API_DB_HOST, API_DB_PORT, API_DB_USER, API_DB_DATABASE } = process.env;
+
 export const getSqlClient = () => {
   const sqlClient = new Client({
-    host: 'api-db',
-    port: 3306,
-    user: 'api',
-    password: 'api',
-    db: 'infrastructure',
+    host: API_DB_HOST || 'api-db',
+    port: API_DB_PORT || 3306,
+    user: API_DB_USER || 'api',
+    password: API_DB_PASSWORD || 'api',
+    db: API_DB_DATABASE || 'infrastructure',
   });
 
   sqlClient.on('error', error => {
     logger.error(error.message);
   });
-  
+
   return sqlClient;
 };
 
