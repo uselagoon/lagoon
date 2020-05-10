efb4c6bf97cf58a31b617590a934b012f6712a96
Blaize Kaye <blaize.kaye@amazee.com>
2020-05-10 19:27:51 +1200

    Moves Axios code into common api from harbor vulnerability scan pull

--- services/webhooks2tasks/src/handlers/problems/harborScanningCompleted.js
+++ services/webhooks2tasks/src/handlers/problems/harborScanningCompleted.js
@@ -2,20 +2,18 @@
 
 const { logger } = require('@lagoon/commons/src/local-logging');
 const { sendToLagoonLogs } = require('@lagoon/commons/src/logs');
+const { getVulnerabilitiesPayloadFromHarbor } = require('@lagoon/commons/src/harborApi');
 const testData = require('./test_data');
 const uuid4 = require('uuid4');
-const axios = require('axios');
-const harborpassword = require('./harborpassword');
+
+
 const {
   getProjectByName,
   getEnvironmentByName,
 } = require('@lagoon/commons/src/api');
 
-const HARBOR_USERNAME = process.env.HARBOR_USERNAME || "admin";
-const HARBOR_PASSWORD = process.env.HARBOR_PASSWORD || harborpassword;
-const HARBOUR_BASE_API_URL = process.env.HARBOUR_BASE_API_URL || "https://harbor-nginx-lagoon-master.ch.amazee.io/api/repositories/";
-const HARBOR_BASE_URL_POSTFIX = "/tags/latest/scan";
-const HARBOR_ACCEPT_HEADER = "application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0";
+const harborpassword = require('./harborpassword');
+
 
 async function harborScanningCompleted(
   webhook: WebhookRequestData,
@@ -36,17 +34,14 @@ async function harborScanningCompleted(
       lagoonServiceName = null,
     ] = extractRepositoryDetails(repository.repo_full_name);
 
-    //TODO: check this comes back right - throw malformed error if not
-    throw generateError('InvalidHarborInput', "Unable to find repo_full_name in body.event_data.repository");
     if(!repository.repo_full_name) {
       throw generateError('InvalidHarborInput', "Unable to find repo_full_name in body.event_data.repository");
     }
 
-    let harborEndpoint = HARBOUR_BASE_API_URL + repository.repo_full_name + HARBOR_BASE_URL_POSTFIX;
+    let harborScanId =repository.repo_full_name;
 
-    let vulnerabilities = await getVulnerabilitiesFromHarbor(harborEndpoint);
+    let vulnerabilities = await getVulnerabilitiesFromHarbor(harborScanId);
 
-    //TODO: try/catch/log end
     let { id: lagoonProjectId } = await getProjectByName(lagoonProjectName);
 
     let { environmentByName: environmentDetails } = await getEnvironmentByName(
@@ -60,7 +55,7 @@ async function harborScanningCompleted(
       vulnerabilities,
     };
 
-    //TODO: ensure that the giturl is set/can be set ...
+
     const webhookData = generateWebhookData(
       webhook.giturl,
       'problems',
@@ -69,13 +64,20 @@ async function harborScanningCompleted(
     );
 
     const buffer = new Buffer(JSON.stringify(webhookData));
-
-    //TODO: this _could_potentially fail, let's wrap it.
     await channelWrapperWebhooks.publish(`lagoon-webhooks`, '', buffer, {
       persistent: true,
     });
   } catch (error) {
-    throw error;
+    sendToLagoonLogs(
+      'error',
+      '',
+      uuid,
+      `${webhooktype}:${event}:unhandled`,
+      { data: body },
+      `Could not fetch Harbor scan results, reason: ${error}`
+    );
+
+    return;
   }
 }
 
@@ -111,32 +113,28 @@ const generateWebhookData = (
   };
 };
 
-const getVulnerabilitiesFromHarbor = async (endpoint) => {
-  const options = {
-    headers: {
-      'Accept': HARBOR_ACCEPT_HEADER,
-      'Authorization': 'Basic ' + Buffer.from(HARBOR_USERNAME + ':' + HARBOR_PASSWORD).toString('base64'),
-    }
-  };
-
-  try {
-    const response = await axios.get(endpoint, options);
-    return extractVulnerabilities(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-
-}
-
 const extractVulnerabilities = (harborScanResponse) => {
   for (let [key, value] of Object.entries(harborScanResponse)) {
+    console.log(key);
     if (value.hasOwnProperty('vulnerabilities')) {
       return value.vulnerabilities;
     }
   }
-  //TODO: should we throw an error here? And how best do we identify the webhook details that come through?
+  throw new ProblemsHarborConnectionError('Scan response from Harbor does not contain a \'vulnerabilities\' key');
 };
 
+
+const getVulnerabilitiesFromHarbor = async (scanId) => {
+  let harborPayload = null;
+  try {
+    harborPayload = await getVulnerabilitiesPayloadFromHarbor(scanId, harborpassword);
+  }
+  catch(error) {
+    throw error;
+  }
+
+  return extractVulnerabilities(harborPayload);
+}
 
 class ProblemsHarborConnectionError extends Error {
   constructor(message) {
