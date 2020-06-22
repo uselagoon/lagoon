677fa0e8b564eacea8ccf064ed1451a5cbf2da79
Ben Jackson <b@benjackson.email>
2020-06-22 19:09:39 +1000

    add branch_regex for autogenerated routes

--- images/kubectl-build-deploy-dind/build-deploy-docker-compose.sh
+++ images/kubectl-build-deploy-dind/build-deploy-docker-compose.sh
@@ -352,6 +352,10 @@ ROUTES_AUTOGENERATE_ALLOW_PRS=$(cat .lagoon.yml | shyaml get-value routes.autoge
 if [[ "$TYPE" == "pullrequest" && "$ROUTES_AUTOGENERATE_ALLOW_PRS" == "true" ]]; then
   ROUTES_AUTOGENERATE_ENABLED=true
 fi
+ROUTES_AUTOGENERATE_BRANCH_REGEX=$(cat .lagoon.yml | shyaml get-value routes.autogenerate.branch_regex)
+if [[ "${BRANCH//./\\.}" =~ $ROUTES_AUTOGENERATE_BRANCH_REGEX  ]]; then
+  ROUTES_AUTOGENERATE_ENABLED=true
+fi
 
 touch /kubectl-build-deploy/values.yaml
 
