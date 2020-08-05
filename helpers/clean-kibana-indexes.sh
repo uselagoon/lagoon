41bd22f426bd693773830432dd78f3d425435fb8
Vincenzo De Naro Papa <vincenzo.denaropapa@gmail.com>
2020-08-05 17:19:21 +0200

    Added fix for edge case of .kibana index

--- helpers/clean-kibana-indexes.sh
+++ helpers/clean-kibana-indexes.sh
@@ -39,6 +39,14 @@ DRYRUN="${DRYRUN:-"false"}"
 
 # Optional set a tenant to check and close/delete indexes
 TENANT="${TENANT:-"*"}"
+if [ "$TENANT" = "*" ]; then
+	REGEX="*"
+# This is a special tenant that requires a special REGEX
+elif [ "$TENANT" = "kibana" ]; then
+	REGEX=""
+else
+	REGEX="_*_$TENANT"
+fi
 
 # Switch context to the selected cluster
 echo -e "You're switching context to ${CLUSTER}"
@@ -63,7 +71,7 @@ cat << EOF |
 IFS=$'\n'
 
 # Generate array with kibana aliases for a specific tenant if specified, otherwise get all aliases
-ELASTICSEARCH_ALIASES=(\$(es-curl GET "_cat/aliases/.kibana_*_$TENANT?h=alias,index"))
+ELASTICSEARCH_ALIASES=(\$(es-curl GET "_cat/aliases/.kibana$REGEX?h=alias,index"))
 
 # For each row, get current alias and index
 for ALIAS_ROW in "\${ELASTICSEARCH_ALIASES[@]}"
@@ -73,26 +81,31 @@ do
 	echo "\$ALIAS is the alias for \$INDEX"
 	
 	# Generate an arry with old indexes to remove
-	OLD_INDEXES=(\$(es-curl GET "_cat/indices/\$ALIAS*?h=index" -s |grep -v \$INDEX))
-		for OLD_INDEX in "\${OLD_INDEXES[@]}"
-		do
-			if [ "$ACTION" = "CLOSE" ]; then
-				echo "Going to close \$OLD_INDEX with es-curl POST \$OLD_INDEX/_close"
-				if [ "$DRYRUN" = "false" ]; then
-					echo "CLOSE INDEX"
-					es-curl POST \$OLD_INDEX/_close
-				fi
-			elif [ "$ACTION" = "DELETE" ]; then
-				echo "Going to delete \$OLD_INDEX with es-curl DELETE \$OLD_INDEX"
-				if [ "$DRYRUN" = "false" ]; then
-					echo "DELETE INDEX"
-					es-curl DELETE \$OLD_INDEX
-				fi
-			else
-				echo "Wrong action choosen"
-				exit 1
+	if [ "$TENANT" = "kibana" ]; then
+		# This fix avoids the "catch-all" .kibana* regex
+		OLD_INDEXES=(\$(es-curl GET "_cat/indices/.kibana*?h=index" -s |grep -E '^.kibana_?[0-9]$'| grep -v -w \$INDEX))
+	else
+		OLD_INDEXES=(\$(es-curl GET "_cat/indices/\$ALIAS*?h=index" -s |grep -v -w \$INDEX))
+	fi
+	for OLD_INDEX in "\${OLD_INDEXES[@]}"
+	do
+		if [ "$ACTION" = "CLOSE" ]; then
+			echo "Going to close \$OLD_INDEX with es-curl POST \$OLD_INDEX/_close"
+			if [ "$DRYRUN" = "false" ]; then
+				echo "CLOSE INDEX"
+				es-curl POST \$OLD_INDEX/_close
 			fi
-		done
+		elif [ "$ACTION" = "DELETE" ]; then
+			echo "Going to delete \$OLD_INDEX with es-curl DELETE \$OLD_INDEX"
+			if [ "$DRYRUN" = "false" ]; then
+				echo "DELETE INDEX"
+				es-curl DELETE \$OLD_INDEX
+			fi
+		else
+			echo "Wrong action choosen"
+			exit 1
+		fi
+	done
 done
 unset IFS
 EOF
