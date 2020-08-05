72e45917dcb8caac2c9c9ecdbbb7d3f51edf7ad3
Vincenzo De Naro Papa <vincenzo.denaropapa@gmail.com>
2020-08-05 15:25:17 +0200

    Added DRYRUN mode option
    Added ACTION option (it might be CLOSE or DELETE)
    Added DEBUG option to run script with set -x
    Added TENANT option to restrict the index cleanup to specific tenants

--- helpers/clean-kibana-indexes.sh
+++ helpers/clean-kibana-indexes.sh
@@ -6,9 +6,17 @@
 # The aim of the script is to keep only the latest index with the latest 
 # configuration, and delete the old ones.
 
-# Usage is: CLUSTER="amazeeio-test" ELASTICSEARCH_NAMESPACE="elasticsearch" ./clean-kibana-indexes.sh"
+# Usage is: 
+# CLUSTER="amazeeio-test" ELASTICSEARCH_NAMESPACE="elasticsearch" ./clean-kibana-indexes.sh"
+# CLUSTER="amazeeio-test" ELASTICSEARCH_NAMESPACE="elasticsearch" TENANT="drupalexample" ./clean-kibana-indexes.sh"
 
-set -eu -o pipefail
+set -eu
+
+# Set DEBUG variable to true, to start bash in debug mode
+DEBUG="${DEBUG:-"false"}"
+if [ "$DEBUG" = "true" ]; then
+        set -x
+fi
 
 # Cluster to connect to
 CLUSTER="${CLUSTER:-""}"
@@ -17,6 +25,20 @@ if [ -z "$CLUSTER" ]; then
 	echo -e "Usage is: CLUSTER="amazeeio-test" ELASTICSEARCH_NAMESPACE="elasticsearch" ./clean-kibana-indexes.sh"
 	exit 1
 fi
+
+# Set the ACTION of the script between CLOSE or DELETE
+ACTION="${ACTION:-""}"
+if [ -z "$ACTION" ]; then
+	echo -e "Set ACTION variable to CLOSE or DELETE\n"
+	echo -e "Usage is: CLUSTER="amazeeio-test" ELASTICSEARCH_NAMESPACE="elasticsearch" ACTION=CLOSE ./clean-kibana-indexes.sh"
+	exit 1
+fi
+
+# Set DRYRUN option to "true" to run in a dry-run mode
+DRYRUN="${DRYRUN:-"false"}"
+
+# Optional set a tenant to check and close/delete indexes
+TENANT="${TENANT:-"*"}"
 
 # Switch context to the selected cluster
 echo -e "You're switching context to ${CLUSTER}"
@@ -40,8 +62,8 @@ cat << EOF |
 #/bin/bash
 IFS=$'\n'
 
-# Generate array with kibana aliases
-ELASTICSEARCH_ALIASES=(\$(es-curl GET "_cat/aliases/.kibana*?h=alias,index"))
+# Generate array with kibana aliases for a specific tenant if specified, otherwise get all aliases
+ELASTICSEARCH_ALIASES=(\$(es-curl GET "_cat/aliases/.kibana_*_$TENANT?h=alias,index"))
 
 # For each row, get current alias and index
 for ALIAS_ROW in "\${ELASTICSEARCH_ALIASES[@]}"
@@ -54,8 +76,22 @@ do
 	OLD_INDEXES=(\$(es-curl GET "_cat/indices/\$ALIAS*?h=index" -s |grep -v \$INDEX))
 		for OLD_INDEX in "\${OLD_INDEXES[@]}"
 		do
-			echo "Going to remove \$OLD_INDEX with es-curl DELETE \$OLD_INDEX"
-			es-curl DELETE \$OLD_INDEX
+			if [ "$ACTION" = "CLOSE" ]; then
+				echo "Going to close \$OLD_INDEX with es-curl POST \$OLD_INDEX/_close"
+				if [ "$DRYRUN" = "false" ]; then
+					echo "CLOSE INDEX"
+					es-curl POST \$OLD_INDEX/_close
+				fi
+			elif [ "$ACTION" = "DELETE" ]; then
+				echo "Going to delete \$OLD_INDEX with es-curl DELETE \$OLD_INDEX"
+				if [ "$DRYRUN" = "false" ]; then
+					echo "DELETE INDEX"
+					es-curl DELETE \$OLD_INDEX
+				fi
+			else
+				echo "Wrong action choosen"
+				exit 1
+			fi
 		done
 done
 unset IFS
