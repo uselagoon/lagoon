4f41deb5d0f5dadbe2ed777488e32d56c1ea0fae
Ben Jackson <b@benjackson.email>
2020-08-26 10:49:11 +1000

    add support for tasks to the operator handler

--- services/operatorhandler/src/index.ts
+++ services/operatorhandler/src/index.ts
@@ -16,7 +16,10 @@ import {
   updateDeployment,
   getDeploymentByName,
   getDeploymentByRemoteId,
-  deleteEnvironment
+  setEnvironmentServices,
+  deleteEnvironment,
+  updateTask,
+  updateProject
 } from '@lagoon/commons/dist/api';
 
 initSendToLagoonLogs();
@@ -24,6 +27,49 @@ initSendToLagoonTasks();
 
 const ocsafety = string =>
   string.toLocaleLowerCase().replace(/[^0-9a-z-]/g, '-');
+
+const decode = (str: string):string => Buffer.from(str, 'base64').toString('binary');
+
+const updateLagoonTask = async (meta) => {
+  // Update lagoon task
+  try {
+    const convertDateFormat = R.init;
+    const dateOrNull = R.unless(R.isNil, convertDateFormat) as any;
+    let completedDate = dateOrNull(meta.endTime) as any;
+
+    if (meta.jobStatus === 'failed') {
+      completedDate = dateOrNull(meta.endTime);
+    }
+
+    // transform the jobstatus into one the API knows about
+    let jobStatus = 'active';
+    switch (meta.jobStatus) {
+      case 'pending':
+        jobStatus = 'active'
+        break;
+      case 'running':
+        jobStatus = 'active'
+        break;
+      case 'complete':
+        jobStatus = 'succeeded'
+        break;
+      default:
+        jobStatus = meta.jobStatus
+        break;
+    }
+    // update the actual task now
+    await updateTask(Number(meta.task.id), {
+      remoteId: meta.remoteId,
+      status: jobStatus.toUpperCase(),
+      started: dateOrNull(meta.startTime),
+      completed: completedDate
+    });
+  } catch (error) {
+    logger.error(
+      `Could not update task ${meta.project} ${meta.jobName} ${meta.remoteId}. Message: ${error}`
+    );
+  }
+}
 
 const messageConsumer = async function(msg) {
   const {
@@ -82,21 +128,23 @@ const messageConsumer = async function(msg) {
       }
 
       try {
-        const updateEnvironmentResult = await updateEnvironment(
+        await updateEnvironment(
           environment.id,
           `{
             openshiftProjectName: "${namespace}",
           }`
         );
       } catch (err) {
-        logger.warn(`${namespace} ${meta.buildName}: Error while updating routes in API, Error: ${err}. Continuing without update`)
+        logger.warn(`${namespace} ${meta.buildName}: Error while updating openshiftProjectName in API, Error: ${err}. Continuing without update`)
       }
       // Update GraphQL API if the Environment has completed or failed
       switch (meta.buildPhase) {
         case 'complete':
         case 'failed':
+        case 'cancelled':
           try {
-            const updateEnvironmentResult = await updateEnvironment(
+            // update the environment with the routes etc
+            await updateEnvironment(
               environment.id,
               `{
                 route: "${meta.route}",
@@ -105,6 +153,8 @@ const messageConsumer = async function(msg) {
                 project: ${project.id}
               }`
             );
+            // update the environment with the services available
+            await setEnvironmentServices(environment.id, meta.services);
           } catch (err) {
             logger.warn(`${namespace} ${meta.buildName}: Error while updating routes in API, Error: ${err}. Continuing without update`)
           }
@@ -133,6 +183,45 @@ const messageConsumer = async function(msg) {
         logger.warn(`${namespace}: Error while deleting environment, Error: ${err}. Continuing without update`)
       }
       break;
+    case 'task':
+      logger.verbose(
+        `Received task result for ${meta.task.name} from ${meta.project} - ${meta.environment} - ${meta.jobStatus}`
+      );
+      // if we want to be able to do something else when a task result comes through,
+      // we can use the task key
+      switch (meta.key) {
+        // since the route migration uses the `advanced task` system, we can do something with the data
+        // that we get back from the operator
+        case "kubernetes:route:migrate":
+          switch (meta.jobStatus) {
+            case "succeeded":
+              try {
+                // get the project ID
+                const projectResult = await getOpenShiftInfoForProject(meta.project);
+                const project = projectResult.projectName
+                // since the advanceddata contains a base64 encoded value, we have to decode it first
+                var decodedData = new Buffer(meta.advancedData, 'base64').toString('ascii')
+                const taskResult = JSON.parse(decodedData)
+                // the returned data for a route migration is specific to this task, so we use the values contained
+                // to do something in the api
+                // in the response, we want to swap these around, so production becomes standby
+                // standby becomes production
+                const response = await updateProject(project.id, {
+                  productionEnvironment: taskResult.standbyProductionEnvironment,
+                  standbyProductionEnvironment: taskResult.productionEnvironment,
+                  productionRoutes: taskResult.productionRoutes,
+                  standbyRoutes: taskResult.standbyRoutes,
+                });
+              } catch (err) {
+                logger.warn(`${namespace}: Error while updating project, Error: ${err}. Continuing without update`)
+              }
+          }
+          break;
+        }
+        // since the logging and other stuff is all sent via the operator directly to message queues
+        // we only need to update the task here with the status
+        await updateLagoonTask(meta)
+      break;
   }
 };
 
@@ -147,7 +236,7 @@ const deathHandler = async (msg, lastError) => {
     'error',
     meta.project,
     '',
-    'task:remove-kubernetes:error', //@TODO: this probably needs to be changed to a new event type for the operator to use
+    'task:remove-kubernetes:error', //@TODO: this probably needs to be changed to a new event type for the operator to use?
     {},
     `*[${meta.project}]* remove \`${namespace}\` ERROR:
 \`\`\`
