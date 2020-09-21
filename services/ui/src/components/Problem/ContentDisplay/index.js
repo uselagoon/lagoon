8abb59b8d912829dfefe6806e627c61208a14680
Tim Clifford <tim.clifford@amazee.com>
2020-09-21 10:49:18 +0100

    UI improvements, features, loading states etc

--- services/ui/src/components/Problem/ContentDisplay/index.js
+++ services/ui/src/components/Problem/ContentDisplay/index.js
@@ -2,6 +2,21 @@ import React from "react";
 import { bp, color, fontSize } from 'lib/variables';
 import DrutinyContent from "components/Problem/ContentDisplay/DrutinyContent";
 import DefaultDisplay from "components/Problem/ContentDisplay/DefaultContent";
+// import ProblemLink from "components/link/Problem";
+
+// {problem.environment.project &&
+// <>
+//     <label>History</label>
+//     <ProblemLink
+//         problemSlug={problem.identifier.toLowerCase()}
+//         environmentSlug={problem.environment.openshiftProjectName}
+//         projectSlug={problem.environment.project.name}
+//         className="more"
+//     >
+//         See history of this problem
+//     </ProblemLink>
+// </>
+// }
 
 const ContentDisplay = ({ problem }) => (
     <>
@@ -12,6 +27,5 @@ const ContentDisplay = ({ problem }) => (
         )}
     </>
 );
-
 
 export default ContentDisplay;
