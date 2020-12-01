b0f9a7f28ab326c84f468836f9865fa12351a6e8
Chris Davis <cdchris12@gmail.com>
2020-12-01 14:03:26 -0600

    Added baasBucketName logic.

--- images/kubectl-build-deploy-dind/build-deploy-docker-compose.sh
+++ images/kubectl-build-deploy-dind/build-deploy-docker-compose.sh
@@ -783,7 +783,12 @@ if [[ "${CAPABILITIES[@]}" =~ "backup.appuio.ch/v1alpha1/Schedule" ]]; then
   TEMPLATE_PARAMETERS=()
 
   # Check for custom baas bucket name
-  BAAS_BUCKET_NAME=$(echo $LAGOON_PROJECT_VARIABLES | jq -r '.[] | select(.name == "baasBucketName") | "\(.value)"')
+  if [ ! -z "$LAGOON_PROJECT_VARIABLES" ]; then
+    BAAS_BUCKET_NAME=$(echo $LAGOON_PROJECT_VARIABLES | jq -r '.[] | select(.name == "LAGOON_BAAS_BUCKET_NAME") | "\(.value)"')
+  fi
+  if [ -z $BAAS_BUCKET_NAME ]; then
+    BAAS_BUCKET_NAME=baas-${PROJECT}
+  fi
   TEMPLATE_PARAMETERS+=(-p BAAS_BUCKET_NAME="${BAAS_BUCKET_NAME}")
 
   # Run Backups every day at 2200-0200
@@ -803,7 +808,7 @@ if [[ "${CAPABILITIES[@]}" =~ "backup.appuio.ch/v1alpha1/Schedule" ]]; then
     -f /kubectl-build-deploy/values.yaml \
     --set backup.schedule="${BACKUP_SCHEDULE}" \
     --set check.schedule="${CHECK_SCHEDULE}" \
-    --set prune.schedule="${PRUNE_SCHEDULE}" "${HELM_ARGUMENTS[@]}"\
+    --set prune.schedule="${PRUNE_SCHEDULE}" "${HELM_ARGUMENTS[@]}" \
     --set baasBucketName="${BAAS_BUCKET_NAME}"  > $YAML_FOLDER/k8up-lagoon-backup-schedule.yaml
 fi
 
