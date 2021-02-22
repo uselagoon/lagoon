5684ec9909074f180dc0393134ab042654c5adb6
Tim Clifford <tim.clifford@amazee.com>
2021-02-22 08:54:14 +0000

    additional selectors to problem filters tab on environments

--- services/api/src/resources/problem/sql.ts
+++ services/api/src/resources/problem/sql.ts
@@ -38,7 +38,7 @@ export const Sql = {
     let q = knex('environment_problem as p')
     .join('environment as e', {environment: 'e.id'}, '=', {environment: 'p.environment'})
     .where('p.deleted', '=', '0000-00-00 00:00:00')
-    .select('p.*', {environment: 'e.id'}, { name: 'e.name', project: 'e.project',
+    .select('p.*', {service: 'p.lagoon_service'}, {environment: 'e.id'}, { name: 'e.name', project: 'e.project',
         environmentType: 'e.environment_type', openshiftProjectName: 'e.openshift_project_name'});
 
     if (environmentType.length > 0) {
