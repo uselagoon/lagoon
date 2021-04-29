c1954bc8eaf8a07f9de436bfc73d0727983a94ee
Tim Clifford <tim.clifford@amazee.com>
2021-04-29 15:58:54 +0100

    fixing broken stories

--- services/ui/src/components/BillingModifiers/index.stories.js
+++ services/ui/src/components/BillingModifiers/index.stories.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import BillingModifiers from './AllBillingModifiers';
+import ApiConnection from "lib/ApiConnection";
+import mocks, { seed } from 'api/src/mocks';
 
-import { data } from './data.json';
+seed();
+const modifiers = mocks.Query().allBillingModifiers();
 
 export default {
   component: BillingModifiers,
-  title: 'Components/BillingGroup/BillingModifiers',
+  title: 'Components/BillingModifiers/BillingModifiers',
 }
 
-export const Default = () => (
-  <BillingModifiers group="Lorem" modifiers={data.allBillingModifiers} month="2020-04" />
-);
+export const Default = () => {
+  return (
+    <ApiConnection>
+      <BillingModifiers group="Lorem" modifiers={modifiers} month="2020-04" />
+    </ApiConnection>
+  );
+}
