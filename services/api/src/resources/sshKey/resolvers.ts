b2b2e83c6eb7bf33280e889fa5d382ecf5c3aba9
Brandon Williams <brandon@amazee.io>
2021-05-05 14:30:31 -0500

    Switch sshkey resolvers to use mariadb library

--- services/api/src/resources/sshKey/resolvers.ts
+++ services/api/src/resources/sshKey/resolvers.ts
@@ -1,6 +1,6 @@
 import * as R from 'ramda';
 import { ResolverFn } from '../';
-import { isPatchEmpty, prepare, query } from '../../util/db';
+import { mQuery, isPatchEmpty } from '../../util/db';
 import { validateSshKey, getSshKeyFingerprint } from '.';
 import { Sql } from './sql';
 
@@ -9,36 +9,35 @@ const formatSshKey = ({ keyType, keyValue }) => `${keyType} ${keyValue}`;
 const sshKeyTypeToString = R.cond([
   [R.equals('SSH_RSA'), R.always('ssh-rsa')],
   [R.equals('SSH_ED25519'), R.always('ssh-ed25519')],
-  [R.T, R.identity],
+  [R.T, R.identity]
 ]);
 
 export const getUserSshKeys: ResolverFn = async (
   { id: userId },
   args,
-  { sqlClient, hasPermission },
+  { sqlClientPool, hasPermission }
 ) => {
   await hasPermission('ssh_key', 'view:user', {
-    users: [userId],
+    users: [userId]
   });
 
-  const queryString = Sql.selectSshKeysByUserId(userId);
-  const rows = await query(sqlClient, queryString);
-  return rows;
+  return mQuery(sqlClientPool, Sql.selectSshKeysByUserId(userId));
 };
 
 export const addSshKey: ResolverFn = async (
   root,
   {
-    input: {
-      id, name, keyValue, keyType: unformattedKeyType, user: userInput
-    },
+    input: { id, name, keyValue, keyType: unformattedKeyType, user: userInput }
   },
-  { sqlClient, hasPermission, models },
+  { sqlClientPool, hasPermission, models }
 ) => {
   const keyType = sshKeyTypeToString(unformattedKeyType);
   // handle key being sent as "ssh-rsa SSHKEY foo@bar.baz" as well as just the SSHKEY
   const keyValueParts = keyValue.split(' ');
-  const keyFormatted = formatSshKey({ keyType, keyValue: keyValueParts.length > 1 ? keyValueParts[1] : keyValue });
+  const keyFormatted = formatSshKey({
+    keyType,
+    keyValue: keyValueParts.length > 1 ? keyValueParts[1] : keyValue
+  });
 
   if (!validateSshKey(keyFormatted)) {
     throw new Error('Invalid SSH key format! Please verify keyType + keyValue');
@@ -46,27 +45,28 @@ export const addSshKey: ResolverFn = async (
 
   const user = await models.UserModel.loadUserByIdOrUsername({
     id: R.prop('id', userInput),
-    username: R.prop('email', userInput),
+    username: R.prop('email', userInput)
   });
 
   await hasPermission('ssh_key', 'add', {
-    users: [user.id],
+    users: [user.id]
   });
 
-  const {
-    info: { insertId },
-  } = await query(
-    sqlClient,
+  const { insertId } = await mQuery(
+    sqlClientPool,
     Sql.insertSshKey({
       id,
       name,
       keyValue,
       keyType,
-      keyFingerprint: getSshKeyFingerprint(keyFormatted),
-    }),
+      keyFingerprint: getSshKeyFingerprint(keyFormatted)
+    })
   );
-  await query(sqlClient, Sql.addSshKeyToUser({ sshKeyId: insertId, userId: user.id }));
-  const rows = await query(sqlClient, Sql.selectSshKey(insertId));
+  await mQuery(
+    sqlClientPool,
+    Sql.addSshKeyToUser({ sshKeyId: insertId, userId: user.id })
+  );
+  const rows = await mQuery(sqlClientPool, Sql.selectSshKey(insertId));
 
   return R.prop(0, rows);
 };
@@ -77,18 +77,18 @@ export const updateSshKey: ResolverFn = async (
     input: {
       id,
       patch,
-      patch: { name, keyType: unformattedKeyType, keyValue },
-    },
+      patch: { name, keyType: unformattedKeyType, keyValue }
+    }
   },
-  { sqlClient, hasPermission },
+  { sqlClientPool, hasPermission }
 ) => {
   const keyType = sshKeyTypeToString(unformattedKeyType);
 
-  const perms = await query(sqlClient, Sql.selectUserIdsBySshKeyId(id));
+  const perms = await mQuery(sqlClientPool, Sql.selectUserIdsBySshKeyId(id));
   const userIds = R.map(R.prop('usid'), perms);
 
   await hasPermission('ssh_key', 'update', {
-    users: userIds,
+    users: userIds
   });
 
   if (isPatchEmpty({ patch })) {
@@ -101,23 +101,26 @@ export const updateSshKey: ResolverFn = async (
 
     if (!validateSshKey(keyFormatted)) {
       throw new Error(
-        'Invalid SSH key format! Please verify keyType + keyValue',
+        'Invalid SSH key format! Please verify keyType + keyValue'
       );
     }
 
     keyFingerprint = getSshKeyFingerprint(keyFormatted);
   }
 
-  await query(
-    sqlClient,
+  await mQuery(
+    sqlClientPool,
     Sql.updateSshKey({
       id,
       patch: {
-        name, keyType, keyValue, keyFingerprint,
-      },
-    }),
+        name,
+        keyType,
+        keyValue,
+        keyFingerprint
+      }
+    })
   );
-  const rows = await query(sqlClient, Sql.selectSshKey(id));
+  const rows = await mQuery(sqlClientPool, Sql.selectSshKey(id));
 
   return R.prop(0, rows);
 };
@@ -125,15 +128,18 @@ export const updateSshKey: ResolverFn = async (
 export const deleteSshKey: ResolverFn = async (
   root,
   { input: { name } },
-  { sqlClient, hasPermission },
+  { sqlClientPool, hasPermission }
 ) => {
   // Map from sshKey name to id and throw on several error cases
-  const skidResult = await query(sqlClient, Sql.selectSshKeyIdByName(name));
+  const skidResult = await mQuery(
+    sqlClientPool,
+    Sql.selectSshKeyIdByName(name)
+  );
 
   const amount = R.length(skidResult);
   if (amount > 1) {
     throw new Error(
-      `Multiple sshKey candidates for '${name}' (${amount} found). Do nothing.`,
+      `Multiple sshKey candidates for '${name}' (${amount} found). Do nothing.`
     );
   }
 
@@ -141,15 +147,17 @@ export const deleteSshKey: ResolverFn = async (
     throw new Error(`Not found: '${name}'`);
   }
 
-  const perms = await query(sqlClient, Sql.selectUserIdsBySshKeyId(R.path(['0', 'id'], skidResult)));
+  const perms = await mQuery(
+    sqlClientPool,
+    Sql.selectUserIdsBySshKeyId(R.path(['0', 'id'], skidResult))
+  );
   const userIds = R.map(R.prop('usid'), perms);
 
   await hasPermission('ssh_key', 'delete', {
     users: userIds
   });
 
-  const prep = prepare(sqlClient, 'CALL DeleteSshKey(:name)');
-  await query(sqlClient, prep({ name }));
+  await mQuery(sqlClientPool, 'CALL DeleteSshKey(:name)', { name });
 
   return 'success';
 };
@@ -157,29 +165,29 @@ export const deleteSshKey: ResolverFn = async (
 export const deleteSshKeyById: ResolverFn = async (
   root,
   { input: { id } },
-  { sqlClient, hasPermission },
+  { sqlClientPool, hasPermission }
 ) => {
-  const perms = await query(sqlClient, Sql.selectUserIdsBySshKeyId(id));
+  const perms = await mQuery(sqlClientPool, Sql.selectUserIdsBySshKeyId(id));
   const userIds = R.map(R.prop('usid'), perms);
 
   await hasPermission('ssh_key', 'delete', {
     users: userIds
   });
 
-  const prep = prepare(sqlClient, 'CALL DeleteSshKeyById(:id)');
-  await query(sqlClient, prep({ id }));
+  await mQuery(sqlClientPool, 'CALL DeleteSshKeyById(:id)', { id });
 
+  // TODO: Check rows for success
   return 'success';
 };
 
 export const deleteAllSshKeys: ResolverFn = async (
   root,
   args,
-  { sqlClient, hasPermission },
+  { sqlClientPool, hasPermission }
 ) => {
   await hasPermission('ssh_key', 'deleteAll');
 
-  await query(sqlClient, Sql.truncateSshKey());
+  await mQuery(sqlClientPool, Sql.truncateSshKey());
 
   // TODO: Check rows for success
   return 'success';
@@ -188,11 +196,11 @@ export const deleteAllSshKeys: ResolverFn = async (
 export const removeAllSshKeysFromAllUsers: ResolverFn = async (
   root,
   args,
-  { sqlClient, hasPermission },
+  { sqlClientPool, hasPermission }
 ) => {
   await hasPermission('ssh_key', 'removeAll');
 
-  await query(sqlClient, Sql.truncateUserSshKey());
+  await mQuery(sqlClientPool, Sql.truncateUserSshKey());
 
   // TODO: Check rows for success
   return 'success';
