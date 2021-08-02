3c562ba23c490a74ed74f19e993128d590e73ed0
Justin Levi Winter <justinlevi@users.noreply.github.com>
2021-08-02 13:15:00 -0400

    autoformatting issues

--- services/api/src/resources/index.ts
+++ services/api/src/resources/index.ts
@@ -9,16 +9,16 @@ export interface ResolverFn {
     parent,
     args,
     context: {
-      sqlClientPool: Pool;
-      hasPermission: hasPermission;
-      keycloakGrant: any | null;
-      userActivityLogger: any | null;
+      sqlClientPool: Pool,
+      hasPermission: hasPermission,
+      keycloakGrant: any | null,
+      userActivityLogger: any | null
       models: {
-        UserModel;
-        GroupModel;
-        ProjectModel;
-        EnvironmentModel;
-      };
+        UserModel,
+        GroupModel,
+        ProjectModel,
+        EnvironmentModel
+      },
     },
     info?
   ): any;
