546c622472e04b85b47bd3350a6b389251dfbfe0
Scott Leggett <scott@sl.id.au>
2021-08-26 22:59:05 +0800

    fix: refactor logs-tee
    Now it actually works properly and can be benchmarked.

--- services/logs-tee/entrypoint.sh
+++ services/logs-tee/entrypoint.sh
@@ -5,25 +5,22 @@
 # - assumes the remaining arguments are UDP endpoints
 # - duplicates received traffic to each UDP endpoints
 # - ensures that each defined endpoint resolves before starting
-# - echoes to STDOUT if $DEBUG is set to "true"
+# - echoes to STDOUT if $TRACE is set to "true"
+# - enables debug logging in the sending socats if $DEBUG is set to "true"
 
 set -euo pipefail
 set -x
 
-socat -b 65507 -u "udp-recvfrom:$1,fork" udp-sendto:127.255.255.255:9999,broadcast &
+socat -b 65507 -u "udp-recv:$1,reuseaddr" udp-sendto:127.255.255.255:9999,broadcast &
 
 shift
 
 for endpoint in "$@"; do
-	while ! nslookup "${endpoint/:[0-9]*/}" &> /dev/null; do
-		echo "${endpoint/:[0-9]*/} doesn't resolve. retrying in 2 seconds.."
-		sleep 2
-	done
-	socat -b 65507 -u udp-recvfrom:9999,reuseaddr,fork udp-sendto:$endpoint &
+	socat ${DEBUG:+-dd} -b 65507 -u udp-recv:9999,reuseaddr udp-sendto:$endpoint &
 done
 
-if [[ ${DEBUG:-} = true ]]; then
-	socat -b 65507 -u udp-recvfrom:9999,reuseaddr,fork - &
+if [[ ${TRACE:-} = true ]]; then
+	socat -b 65507 -u udp-recv:9999,reuseaddr - &
 fi
 
 wait -n
