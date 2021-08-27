6e6bc6461a908bbb27146c805df9f101d6921c74
Toby Bellwood <toby.bellwood@amazee.com>
2021-08-27 10:38:56 +1000

    make users and non-ci projects persist across retests

--- local-dev/api-data-watcher-pusher/api-watch-push.sh
+++ local-dev/api-data-watcher-pusher/api-watch-push.sh
@@ -3,9 +3,9 @@
 # inject variables from environment into the GQL template
 envsubst '$GIT_HOST $GIT_PORT $INGRESS_IP $CONSOLE_URL $TOKEN' < /api-data/03-populate-api-data-kubernetes.gql | sponge /api-data/03-populate-api-data-kubernetes.gql
 
-clear_gql_file_path="/api-data/00-clear-api-data.gql"
+# clear_gql_file_path="/api-data/00-clear-api-data.gql"
 populate_general_gql_file_path="/api-data/01-populate-api-data-general.gql"
-populate_openshift_gql_file_path="/api-data/02-populate-api-data-openshift.gql"
+# populate_openshift_gql_file_path="/api-data/02-populate-api-data-openshift.gql"
 populate_kubernetes_gql_file_path="/api-data/03-populate-api-data-kubernetes.gql"
 
 send_graphql_query() {
@@ -25,33 +25,33 @@ send_graphql_query() {
 }
 
 watch_apidatafolder() {
-    chsum_clear_prev=""
+    # chsum_clear_prev=""
     chsum_populate_general_prev=""
-    chsum_populate_openshift_prev=""
+    # chsum_populate_openshift_prev=""
     chsum_populate_kubernetes_prev=""
 
     while [[ true ]]
     do
-        chsum_clear_curr=`md5sum $clear_gql_file_path`
+        # chsum_clear_curr=`md5sum $clear_gql_file_path`
         chsum_populate_general_curr=`md5sum $populate_general_gql_file_path`
-        chsum_populate_openshift_curr=`md5sum $populate_openshift_gql_file_path`
+        # chsum_populate_openshift_curr=`md5sum $populate_openshift_gql_file_path`
         chsum_populate_kubernetes_curr=`md5sum $populate_kubernetes_gql_file_path`
 
         if
-            [[ $chsum_clear_prev != $chsum_clear_curr ]] ||
+            # [[ $chsum_clear_prev != $chsum_clear_curr ]] ||
             [[ $chsum_populate_general_prev != $chsum_populate_general_curr ]] ||
-            [[ $chsum_populate_openshift_prev != $chsum_populate_openshift_curr ]] ||
+            # [[ $chsum_populate_openshift_prev != $chsum_populate_openshift_curr ]] ||
             [[ $chsum_populate_kubernetes_prev != $chsum_populate_kubernetes_curr ]];
         then
             echo "******* Found changes in gql files in /api-data/, clearing and re-populating"
 
-            if
-                send_graphql_query $clear_gql_file_path;
-            then
-                chsum_clear_prev=$chsum_clear_curr
-            else
-                echo '**** ERROR while clearing, will try again.'
-            fi
+            # if
+            #     send_graphql_query $clear_gql_file_path;
+            # then
+            #     chsum_clear_prev=$chsum_clear_curr
+            # else
+            #     echo '**** ERROR while clearing, will try again.'
+            # fi
 
             if
                 send_graphql_query $populate_general_gql_file_path;
@@ -61,13 +61,13 @@ watch_apidatafolder() {
                 echo "**** ERROR while re-populating $populate_general_gql_file_path, will try again."
             fi
 
-            if
-                send_graphql_query $populate_openshift_gql_file_path;
-            then
-                chsum_populate_openshift_prev=$chsum_populate_openshift_curr
-            else
-                echo "**** ERROR while re-populating $populate_openshift_gql_file_path, will try again."
-            fi
+            # if
+            #     send_graphql_query $populate_openshift_gql_file_path;
+            # then
+            #     chsum_populate_openshift_prev=$chsum_populate_openshift_curr
+            # else
+            #     echo "**** ERROR while re-populating $populate_openshift_gql_file_path, will try again."
+            # fi
 
             if
                 send_graphql_query $populate_kubernetes_gql_file_path;
