1cbb08c594f6624fb7585969b68a9d8fe8ce3c3e
Scott Leggett <scott@sl.id.au>
2021-11-01 23:40:27 +0800

    feat: make the linter errors clearer
    Put the linter inside the "bash debug disabled" section.

--- images/kubectl-build-deploy-dind/build-deploy-docker-compose.sh
+++ images/kubectl-build-deploy-dind/build-deploy-docker-compose.sh
@@ -76,6 +76,8 @@ function featureFlag() {
 ### PREPARATION
 ##############################################
 
+set +x # reduce noise in build logs
+
 # validate .lagoon.yml
 if ! lagoon-linter; then
 	echo "https://docs.lagoon.sh/lagoon/using-lagoon-the-basics/lagoon-yml#restrictions describes some possible reasons for this build failure."
@@ -84,7 +86,6 @@ if ! lagoon-linter; then
 fi
 
 # Load path of docker-compose that should be used
-set +x # reduce noise in build logs
 DOCKER_COMPOSE_YAML=($(cat .lagoon.yml | shyaml get-value docker-compose-yaml))
 
 DEPLOY_TYPE=$(cat .lagoon.yml | shyaml get-value environments.${BRANCH//./\\.}.deploy-type default)
