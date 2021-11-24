4d3fb54df2edabe95abaafe7f7819719cfdd3cd9
cdchris12 <chris.davis@amazee.io>
2021-11-24 11:12:07 -0600

    Forced update

--- images/docker-host/prune-images.sh
+++ images/docker-host/prune-images.sh
@@ -4,5 +4,5 @@ if ! docker -H ${DOCKER_HOST} info &> /dev/null; then
     echo "could not connect to ${DOCKER_HOST}"; exit 1
 fi
 
-# prune all images older than 7 days
-docker image prune -af --filter "until=168h"
+# prune all images older than 3 days
+docker image prune -af --filter "until=36h"
