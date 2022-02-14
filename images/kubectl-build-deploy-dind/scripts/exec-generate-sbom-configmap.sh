a9305c8ea0c7911ac45f1be114c195f3eb4aef1b
Tim Clifford <tim.clifford@amazee.com>
2022-02-14 21:57:22 +0000

    file size function

--- images/kubectl-build-deploy-dind/scripts/exec-generate-sbom-configmap.sh
+++ images/kubectl-build-deploy-dind/scripts/exec-generate-sbom-configmap.sh
@@ -11,60 +11,62 @@ echo "Running sbom scan using syft"
 echo "Image being scanned: ${IMAGE_FULL}"
 set +x
 
-
 DOCKER_HOST=docker-host.lagoon.svc docker run --rm -v /var/run/docker.sock:/var/run/docker.sock imagecache.amazeeio.cloud/anchore/syft packages ${IMAGE_FULL} -o ${SBOM_OUTPUT} | gzip > ${SBOM_OUTPUT_FILE}
 
-# FILESIZE=$(stat -f%z "$IMAGE_FULL")
-FILESIZE=$(stat -c%s "$SBOM_OUTPUT_FILE")
+IMAGE_FULL="/Users/tim.clifford/playground/fleet-portal.ch/fleet-portal-ch.gz"
+SBOM_OUTPUT_FILE="/Users/tim.clifford/playground/fleet-portal.ch/fleet-portal-ch.gz"
+
+FILESIZE=$(stat -f%z "$IMAGE_FULL")
+# FILESIZE=$(stat -c%s "$SBOM_OUTPUT_FILE")
 echo "Size of ${SBOM_OUTPUT_FILE} = $FILESIZE bytes."
 
 checkFileSizeLimit() {
   if (( $FILESIZE > 950000 )); then
     echo "$SBOM_OUTPUT_FILE is too large, skipping pushing to configmap"
     return
+  else
+    if [ -f "${SBOM_OUTPUT_FILE}" ]; then
+        echo "Successfully generated SBOM for ${IMAGE_FULL}"
+        SBOM_CONFIGMAP=lagoon-sbom-${IMAGE_NAME}
+
+        set -x
+        # If sbom configmap already exists then we need to update, else create new
+        if kubectl --insecure-skip-tls-verify -n ${NAMESPACE} get configmap $SBOM_CONFIGMAP &> /dev/null; then
+            kubectl --insecure-skip-tls-verify \
+                -n ${NAMESPACE} \
+                create configmap $SBOM_CONFIGMAP \
+                --from-file=${SBOM_OUTPUT_FILE} \
+                -o json \
+                --dry-run=client | kubectl replace -f -
+            kubectl --insecure-skip-tls-verify \
+                -n ${NAMESPACE} \
+                label configmap ${SBOM_CONFIGMAP} \
+                lagoon.sh/insightsType=sbom \
+                lagoon.sh/buildName=${LAGOON_BUILD_NAME} \
+                lagoon.sh/project=${PROJECT} \
+                lagoon.sh/environment=${ENVIRONMENT} \
+                lagoon.sh/service=${IMAGE_NAME}
+
+        else
+            # Create configmap and add label (#have to add label separately: https://github.com/kubernetes/kubernetes/issues/60295)
+            kubectl --insecure-skip-tls-verify \
+                -n ${NAMESPACE} \
+                create configmap ${SBOM_CONFIGMAP} \
+                --from-file=${SBOM_OUTPUT_FILE}
+            kubectl --insecure-skip-tls-verify \
+                -n ${NAMESPACE} \
+                label configmap ${SBOM_CONFIGMAP} \
+                lagoon.sh/insightsType=sbom \
+                lagoon.sh/buildName=${LAGOON_BUILD_NAME} \
+                lagoon.sh/project=${PROJECT} \
+                lagoon.sh/environment=${ENVIRONMENT} \
+                lagoon.sh/service=${IMAGE_NAME}
+        fi
+        set +x
+    else
+        echo "Failed to generate SBOM for ${IMAGE_FULL}"
+    fi
   fi
 }
 
 checkFileSizeLimit
-
-if [ -f "${SBOM_OUTPUT_FILE}" ]; then
-    echo "Successfully generated SBOM for ${IMAGE_FULL}"
-    SBOM_CONFIGMAP=lagoon-sbom-${IMAGE_NAME}
-
-    set -x
-    # If sbom configmap already exists then we need to update, else create new
-    if kubectl --insecure-skip-tls-verify -n ${NAMESPACE} get configmap $SBOM_CONFIGMAP &> /dev/null; then
-        kubectl --insecure-skip-tls-verify \
-            -n ${NAMESPACE} \
-            create configmap $SBOM_CONFIGMAP \
-            --from-file=${SBOM_OUTPUT_FILE} \
-            -o json \
-            --dry-run=client | kubectl replace -f -
-        kubectl --insecure-skip-tls-verify \
-            -n ${NAMESPACE} \
-            label configmap ${SBOM_CONFIGMAP} \
-            lagoon.sh/insightsType=sbom \
-            lagoon.sh/buildName=${LAGOON_BUILD_NAME} \
-            lagoon.sh/project=${PROJECT} \
-            lagoon.sh/environment=${ENVIRONMENT} \
-            lagoon.sh/service=${IMAGE_NAME}
-
-    else
-        # Create configmap and add label (#have to add label separately: https://github.com/kubernetes/kubernetes/issues/60295)
-        kubectl --insecure-skip-tls-verify \
-            -n ${NAMESPACE} \
-            create configmap ${SBOM_CONFIGMAP} \
-            --from-file=${SBOM_OUTPUT_FILE}
-        kubectl --insecure-skip-tls-verify \
-            -n ${NAMESPACE} \
-            label configmap ${SBOM_CONFIGMAP} \
-            lagoon.sh/insightsType=sbom \
-            lagoon.sh/buildName=${LAGOON_BUILD_NAME} \
-            lagoon.sh/project=${PROJECT} \
-            lagoon.sh/environment=${ENVIRONMENT} \
-            lagoon.sh/service=${IMAGE_NAME}
-    fi
-    set +x
-else
-    echo "Failed to generate SBOM for ${IMAGE_FULL}"
-fi
