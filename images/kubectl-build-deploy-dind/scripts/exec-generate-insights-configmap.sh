425b71fede2f93cee7002b7e371c8ee125f5d315
Tim Clifford <tim.clifford@amazee.com>
2022-02-17 13:55:35 +0000

    adding docker inspect insights gathering

--- images/kubectl-build-deploy-dind/scripts/exec-generate-insights-configmap.sh
+++ images/kubectl-build-deploy-dind/scripts/exec-generate-insights-configmap.sh
@@ -1,10 +1,50 @@
 #!/bin/bash
 
-# SBOM config
 TMP_DIR="${TMP_DIR:-/tmp}"
 SBOM_OUTPUT="cyclonedx-json"
 SBOM_OUTPUT_FILE="${TMP_DIR}/${IMAGE_NAME}.cyclonedx.json.gz"
-SBOM_CONFIGMAP="lagoon-sbom-${IMAGE_NAME}"
+SBOM_CONFIGMAP="lagoon-insights-sbom-${IMAGE_NAME}"
+
+DOCKER_INSPECT_CONFIGMAP="lagoon-insights-docker-inpsect-${IMAGE_NAME}"
+DOCKER_INSPECT_OUTPUT_FILE="${TMP_DIR}/${IMAGE_NAME}.docker-inspect.json.gz"
+
+set -x
+# Run docker inspect scan on image
+echo "Running docker inspect on: ${IMAGE_FULL}"
+set +x
+
+DOCKER_HOST=docker-host.lagoon.svc docker run --rm -v /var/run/docker.sock:/var/run/docker.sock docker inspect ${IMAGE_FULL} | gzip > ${DOCKER_INSPECT_OUTPUT_FILE}
+
+processDockerInspect() {
+  echo "Successfully generated image inspection data for ${IMAGE_FULL}"
+
+  set -x
+  # If docker-inspect configmap already exists then we need to update, else create new
+  if kubectl --insecure-skip-tls-verify -n ${NAMESPACE} get configmap $DOCKER_INSPECT_CONFIGMAP &> /dev/null; then
+      kubectl --insecure-skip-tls-verify \
+          -n ${NAMESPACE} \
+          create configmap $DOCKER_INSPECT_CONFIGMAP \
+          --from-file=${DOCKER_INSPECT_OUTPUT_FILE} \
+          -o json \
+          --dry-run=client | kubectl replace -f -
+  else
+      kubectl --insecure-skip-tls-verify \
+          -n ${NAMESPACE} \
+          create configmap ${DOCKER_INSPECT_CONFIGMAP} \
+          --from-file=${DOCKER_INSPECT_OUTPUT_FILE}
+  fi
+  kubectl --insecure-skip-tls-verify \
+      -n ${NAMESPACE} \
+      label configmap ${DOCKER_INSPECT_CONFIGMAP} \
+      lagoon.sh/insightsType=docker-inspect-gz \
+      lagoon.sh/buildName=${LAGOON_BUILD_NAME} \
+      lagoon.sh/project=${PROJECT} \
+      lagoon.sh/environment=${ENVIRONMENT} \
+      lagoon.sh/service=${IMAGE_NAME}
+  set +x
+}
+
+processDockerInspect
 
 set -x
 # Run sbom and dump to file
