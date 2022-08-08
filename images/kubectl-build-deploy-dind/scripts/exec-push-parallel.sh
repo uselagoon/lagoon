c60cb2c0975c91c7cff1ee087fca7a6b96a3de09
shreddedbacon <b@benjackson.email>
2022-08-09 07:42:09 +1000

    chore: hide output of docker push

--- images/kubectl-build-deploy-dind/scripts/exec-push-parallel.sh
+++ images/kubectl-build-deploy-dind/scripts/exec-push-parallel.sh
@@ -2,5 +2,6 @@
 
 docker tag ${TEMPORARY_IMAGE_NAME} ${REGISTRY}/${PROJECT}/${ENVIRONMENT}/${IMAGE_NAME}:${IMAGE_TAG:-latest}
 
-echo "docker push ${REGISTRY}/${PROJECT}/${ENVIRONMENT}/${IMAGE_NAME}:${IMAGE_TAG:-latest}" >> /kubectl-build-deploy/lagoon/push
+# only show final output as push steps are not required
+echo "docker push ${REGISTRY}/${PROJECT}/${ENVIRONMENT}/${IMAGE_NAME}:${IMAGE_TAG:-latest} | grep -v -E 'Waiting|Layer already exists|Preparing'" >> /kubectl-build-deploy/lagoon/push
 
