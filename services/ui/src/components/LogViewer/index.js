b6eab5b9ab3cda67325c65ffa6214076cdba4161
Blaize Kaye <blaize.kaye@amazee.com>
2022-08-17 04:44:43 +1200

    Edge cases for parsing and some styling

--- services/ui/src/components/LogViewer/index.js
+++ services/ui/src/components/LogViewer/index.js
@@ -17,27 +17,31 @@ const LogViewer = ({ logs }) => (
           font-family: 'Monaco', monospace;
           font-size: 12px;
           font-weight: 400;
-          min-height: 600px;
           margin: 0;
           overflow-wrap: break-word;
           overflow-x: scroll;
-          padding: calc((100vw / 16) * 0.5) calc(100vw / 16);
           white-space: pre-wrap;
           will-change: initial;
           word-break: break-all;
           word-wrap: break-word;
-
-          @media ${bp.xs_smallUp} {
-            padding: 30px;
-          }
         }
       }
+    `}</style>
+    <style jsx global>{`
+        .accordion-heading {
+          color: black;
+          border-color: lightgrey !important;
+        }
+        .log-text {
+          padding: 30px;
+        }
     `}</style>
   </React.Fragment>
 );
 
 
 const logPreprocessor = (logs) => {
+  let ret = null;
   try {
     let tokens = logPreprocessorTokenize(logs);
     let AST = logPreprocessorProcessParse(tokens);
@@ -45,19 +49,19 @@ const logPreprocessor = (logs) => {
   } catch (e) {
     // if there are any errors parsing and transforming, we just return the logs as is.
     console.log("Error processing logs for display: " + e);
-    return logs;
+    return (<div className="processed-logs"><div key='logerror' className="log-text">{logs}</div></div>);
   }
 }
 
 
 const logPreprocessorRenderLogNode = (node) => {
-  if (node.type === "logText") {
-    return <div key={node.key} className="logText">{node.text}</div>;
+  if (node.type === "log-text") {
+    return <div key={node.key} className="log-text">{node.text}</div>;
   }
   if (node.type === "section") {
     return (
       <Accordion key={node.key} minified={true} columns={{sectionName:node.details}} className="data-row row-heading">
-        <div key={node.key + "section"} className="sectionDetails">
+        <div key={node.key + "section"} className="section-details">
           {node.nodes.map((element) => {
             return logPreprocessorRenderLogNode(element);
           })}
@@ -73,7 +77,7 @@ const logPreprocessorProcessASTToReact = (ast) => {
     throw "Expecting root node to be of type 'root'";
   }
 
-  return (<div className="processedLogs">
+  return (<div className="processed-logs">
   {ast.nodes.map((element) => {
     return logPreprocessorRenderLogNode(element);
   })}
@@ -86,25 +90,16 @@ const logPreprocessorProcessParse = (tokens) => {
 
   for(let i = 0; i < tokens.length; i++) {
     switch(tokens[i].type) {
-      case("logText"):
+      case("log-text"):
         root.nodes.push(tokens[i]);
       break;
-      case("sectionOpener"):
-        //two cases here - either the next token is a logText and then a sectionCloser, or it's a sectionCloser
+      case("section-opener"):
         let node = {type:"section", key: tokens[i].key, details: tokens[i].details, nodes: []};
-        if(tokens[i + 1].type == "logText") {
+        if(tokens[i + 1].type == "log-text") {
           node.nodes.push(tokens[i+1]);
           i++; //increment `i` so that we're dealing with the _next_ token
         }
-        if(tokens[i+1].type != "sectionCloser") { //something has gone wrong
-          throw "Expected a 'sectionCloser'";
-        }
-        i++; //will bring us over the section closer
         root.nodes.push(node);
-      break;
-      case("sectionCloser"):
-        //this should give us an error
-        throw "A 'sectionCloser' should not appear here";
       break;
       default:
         throw "Unexpected type found in tokens";
@@ -117,13 +112,19 @@ const logPreprocessorProcessParse = (tokens) => {
 const logPreprocessorTokenize = (logs) => {
   // tokenize
   const regexp = /<<<<< (SECTION):([\w\-\s]+)<<<<</;
+  const beginningSectionDefaultDetails = "Logs begin";
 
   // The regex above will split the logs into three separate token types
   // 1. standard blocks of text
   // 2. markers for section starts containing "SECTION" only
   // 3. section header details (the second capture in the regex above)
   let tokens = logs.split(regexp);
-  // process
+
+  // if the first element is an empty string, we can discard it because
+  // it's an artifact of the split
+  if(tokens.length > 0 && tokens[0].length == 0) {
+    tokens.shift();
+  }
 
   let tokenizedLogs = [];
   let sectionIsOpen = false;
@@ -131,23 +132,23 @@ const logPreprocessorTokenize = (logs) => {
     if(tokens[i] == "SECTION") {
       let sectionDetails = tokens[i + 1]; //we're guaranteed to have this given the match criteria
 
-      if(sectionIsOpen) { //we need to add a closing div for the last section
-        tokenizedLogs.push({type:"sectionCloser"});
-      }
       // let sectionOpening = `<div class="logsection"><div class="logsection-details>${sectionDetails}</div><pre>`;
-      tokenizedLogs.push({type:"sectionOpener", key:i, details:sectionDetails})
+      tokenizedLogs.push({type:"section-opener", key:i, details:sectionDetails})
       // tokenizedLogs.push(sectionCloser + sectionOpening);
       sectionIsOpen = true;
 
       //we also need to _skip_ the next token, since we've already used it, and continue on
       i++; continue;
     }
-    tokenizedLogs.push({type:"logText", text: tokens[i].trim(), key: i});
+    tokenizedLogs.push({type:"log-text", text: tokens[i].trim(), key: i});
   }
 
-  // We need to close up any outstanding sections
-  if(sectionIsOpen) {
-    tokenizedLogs.push({type:"sectionCloser"});
+
+  // if the first element is an empty log-text, we add a section to it
+  // this will ensure that everything is eventually wrapped in a section
+  // even if there's one massive log
+  if(tokenizedLogs.length > 0 && tokenizedLogs[0].type == "log-text") {
+    tokenizedLogs.unshift({type:"section-opener", key:-1, details: beginningSectionDefaultDetails})
   }
 
   return tokenizedLogs;
