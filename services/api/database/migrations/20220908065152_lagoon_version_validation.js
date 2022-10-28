ce980dc45013ffcce593cf42d528af71cc80d32a
Chris <chris_2190@hotmail.com>
2022-10-28 16:18:37 +1100

    Cleaning inconsistencies and adding Indexs

--- services/api/database/migrations/20220908065152_lagoon_version_validation.js
+++ services/api/database/migrations/20220908065152_lagoon_version_validation.js
@@ -3,7 +3,7 @@
  * @returns { Promise<void> }
  */
  exports.up = async function(knex) {
-    lagoonVersion = await knex.schema.hasColumn('openshift', 'build_image'); // Add Lagoon Version table
+    lagoonVersion = await knex.schema.hasColumn('openshift', 'build_image');
     if (!lagoonVersion) {
         throw new Error("Exiting - build older than 2.10, please update to version 2.10")
     }
