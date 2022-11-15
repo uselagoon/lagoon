7200d8ca81e2063ed7c811e9c1111abff91bd7c3
Toby Bellwood <toby.bellwood@amazee.io>
2022-11-16 08:20:32 +1100

    tabs v spaces

--- services/keycloak/javascript/mappers/groups-and-roles.js
+++ services/keycloak/javascript/mappers/groups-and-roles.js
@@ -9,29 +9,29 @@ var projectGroupProjectIds = new HashMap();
 var forEach = Array.prototype.forEach;
 // add all groups the user is part of
 forEach.call(user.getGroups().toArray(), function(group) {
-	// remove the group role suffixes
-	// lets check if the group has a parent if this is a child
-	var groupName = group.getName().replace(/-(owner|maintainer|developer|reporter|guest)$/,"");
-	if(group.getFirstAttribute("type") == "role-subgroup") {
-		var parent = group.getParent();
-		var projectIds = parent.getFirstAttribute("lagoon-projects");
-		if(parent.getFirstAttribute("type") == "project-default-group") {
-			if(projectIds !== null) {
-				forEach.call(projectIds.split(","), function(g) {
-					projectGroupProjectIds.put(g, groupName)
-				});
-				return;
-			}
-		} else {
-			if(projectIds !== null) {
-				forEach.call(projectIds.split(","), function(g) {
-					groupProjectIds.put(g, groupName)
-				});
-				return;
-			}
-		}
-	}
-	return;
+  // remove the group role suffixes
+  // lets check if the group has a parent if this is a child
+  var groupName = group.getName().replace(/-(owner|maintainer|developer|reporter|guest)$/,"");
+  if(group.getFirstAttribute("type") == "role-subgroup") {
+    var parent = group.getParent();
+    var projectIds = parent.getFirstAttribute("lagoon-projects");
+    if(parent.getFirstAttribute("type") == "project-default-group") {
+      if(projectIds !== null) {
+        forEach.call(projectIds.split(","), function(g) {
+          projectGroupProjectIds.put(g, groupName)
+        });
+        return;
+      }
+    } else {
+      if(projectIds !== null) {
+        forEach.call(projectIds.split(","), function(g) {
+          groupProjectIds.put(g, groupName)
+        });
+        return;
+      }
+    }
+  }
+  return;
 });
 
 // add all the unique project ids roles the user has, that aren't in an already existing group
@@ -44,7 +44,7 @@ for each (var e in uniqueGroups) groupsAndRoles.add(e);
 
 // add all roles the user is part of
 forEach.call(user.getRoleMappings().toArray(), function(role) {
-	var roleName = role.getName();
-	groupsAndRoles.add(roleName);
+  var roleName = role.getName();
+  groupsAndRoles.add(roleName);
 });
 exports = groupsAndRoles;
