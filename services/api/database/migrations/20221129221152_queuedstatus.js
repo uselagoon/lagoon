e973ad376e41681a7e64781e4bb0ce1e84b4bc4d
shreddedbacon <b@benjackson.email>
2022-11-30 11:00:17 +1100

    chore: fix migrations

--- services/api/database/migrations/20221129221152_queuedstatus.js
+++ services/api/database/migrations/20221129221152_queuedstatus.js
@@ -3,21 +3,43 @@
  * @returns { Promise<void> }
  */
  exports.up = async function(knex) {
-    return knex.schema
-    .alterTable('deployment', function (table) {
-        table.enu('status', ['new', 'pending', 'running', 'cancelled', 'error', 'failed', 'complete', 'queued']).notNullable();
-    })
-    .alterTable('task', function (table) {
-        table.enu('status', ['new', 'pending', 'running', 'cancelled', 'error', 'failed', 'complete', 'queued']).notNullable();
-    })
+    // cant alter enums in place, so drop the column first :D
+    buildStep = await knex.schema.hasColumn('deployment', 'build_step');
+    if (!buildStep) {
+        return knex.schema
+        .alterTable('deployment', (table) => {
+            table.dropColumn('status');
+            table.string('build_step');
+        })
+        .alterTable('task', (table) => {
+            table.dropColumn('status');
+        })
+        .alterTable('deployment', function (table) {
+            table.enu('status', ['new', 'pending', 'running', 'cancelled', 'error', 'failed', 'complete', 'queued']).notNullable();
+        })
+        .alterTable('task', function (table) {
+            table.enu('status', ['new', 'pending', 'running', 'cancelled', 'error', 'failed', 'complete', 'queued']).notNullable();
+        })
+    }
+    else {
+        return knex.schema
+    }
 };
 
 /**
 * @param { import("knex").Knex } knex
 * @returns { Promise<void> }
 */
-exports.down = function(knex) {
+exports.down = async function(knex) {
+    // cant alter enums in place, so drop the column first :D
     return knex.schema
+    .alterTable('deployment', (table) => {
+        table.dropColumn('status');
+        table.dropColumn('build_step');
+    })
+    .alterTable('task', (table) => {
+        table.dropColumn('status');
+    })
     .alterTable('deployment', function (table) {
         table.enu('status', ['new', 'pending', 'running', 'cancelled', 'error', 'failed', 'complete']).notNullable();
     })
