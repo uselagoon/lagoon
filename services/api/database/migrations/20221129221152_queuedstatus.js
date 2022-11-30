5d6144c24877de9b5613b9a24b1ddb2c3c492e91
shreddedbacon <b@benjackson.email>
2022-11-30 18:52:22 +1100

    chore: remove unneeded field check in migration

--- services/api/database/migrations/20221129221152_queuedstatus.js
+++ services/api/database/migrations/20221129221152_queuedstatus.js
@@ -3,21 +3,14 @@
  * @returns { Promise<void> }
  */
  exports.up = async function(knex) {
-    // cant alter enums in place, so drop the column first :D
-    buildStep = await knex.schema.hasColumn('deployment', 'build_step');
-    if (!buildStep) {
-        return knex.schema
-        .alterTable('deployment', (table) => {
-            table.string('build_step');
-        })
-        .raw(`ALTER TABLE task
-        MODIFY status ENUM("new", "pending", "running", "cancelled", "error", "failed", "complete", "active", "succeeded", "queued") NOT NULL;`)
-        .raw(`ALTER TABLE deployment
-        MODIFY status ENUM("new", "pending", "running", "cancelled", "error", "failed", "complete", "queued") NOT NULL;`)
-    }
-    else {
-        return knex.schema
-    }
+    return knex.schema
+    .alterTable('deployment', (table) => {
+        table.string('build_step');
+    })
+    .raw(`ALTER TABLE task
+    MODIFY status ENUM("new", "pending", "running", "cancelled", "error", "failed", "complete", "active", "succeeded", "queued") NOT NULL;`)
+    .raw(`ALTER TABLE deployment
+    MODIFY status ENUM("new", "pending", "running", "cancelled", "error", "failed", "complete", "queued") NOT NULL;`)
 };
 
 /**
