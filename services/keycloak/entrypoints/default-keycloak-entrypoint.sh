4bc3487fcd60c791af382e09b6e4517b3a6c5a54
shreddedbacon <b@benjackson.email>
2023-10-17 12:19:32 +1100

    chore: use frontendurl for admin page

--- services/keycloak/entrypoints/default-keycloak-entrypoint.sh
+++ services/keycloak/entrypoints/default-keycloak-entrypoint.sh
@@ -10,6 +10,10 @@ export KC_HOSTNAME_STRICT_HTTPS=${KC_HOSTNAME_STRICT_HTTPS:-false}
 export KC_HTTP_RELATIVE_PATH=${KC_HTTP_RELATIVE_PATH:-/auth}
 export KC_HOSTNAME_URL=${KEYCLOAK_FRONTEND_URL}
 
+# this may need to be changed to an optional override and only set for k3d testing so that it allows access to
+# the admin page in a k3d deployed environment locally
+export KC_HOSTNAME_ADMIN_URL=${KEYCLOAK_FRONTEND_URL}
+
 export KC_DB_POOL_MAX_SIZE=${KEYCLOAK_DS_MAX_POOL_SIZE:-20}
 export KC_DB_POOL_MIN_SIZE=${KEYCLOAK_DS_MIN_POOL_SIZE:-0}
 
