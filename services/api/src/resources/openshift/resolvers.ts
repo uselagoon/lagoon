2572b4af3e582bdb8cdf8b95f313192d92a32001
cgoodwin90 <chris_2190@hotmail.com>
2023-11-03 17:29:47 +1100

    Fixes one of the ts errors

--- services/api/src/resources/openshift/resolvers.ts
+++ services/api/src/resources/openshift/resolvers.ts
@@ -91,8 +91,7 @@ export const deleteOpenshift: ResolverFn = async (
   .join('openshift', 'project.openshift', '=', 'openshift.id')
   .where('openshift.name', input.name).count('project.id', {as: 'numactive'}).toString());
 
-  const numberActiveOs = R.path(['0', 'numactive'], res);
-  // @ts-ignore - TS2365: Operator '>' cannot be applied to types 'unknown' and 'number'.
+  const numberActiveOs: number = R.path(['0', 'numactive'], res);
   if(numberActiveOs > 0) {
     throw new Error(`Openshift "${input.name} still in use, can not delete`);
   }
