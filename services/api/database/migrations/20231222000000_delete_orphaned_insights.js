262fb54a8e866213d1ade2da83aa7ec5a555a8b9
Blaize M Kaye <blaize.kaye@gmail.com>
2023-12-22 12:55:07 +1300

    Adds problems to migration

--- services/api/database/migrations/20231222000000_delete_orphaned_insights.js
+++ services/api/database/migrations/20231222000000_delete_orphaned_insights.js
@@ -3,12 +3,20 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-    return knex.schema.raw(`
-    DELETE ef
-    FROM environment_fact ef
-    LEFT JOIN environment e ON ef.environment = e.id
-    WHERE e.id IS NULL OR e.deleted != '0000-00-00 00:00:00'
-  `);
+    return Promise.all([
+      knex.schema.raw(`
+       DELETE ef
+       FROM environment_fact ef
+       LEFT JOIN environment e ON ef.environment = e.id
+       WHERE e.id IS NULL OR e.deleted != '0000-00-00 00:00:00'
+      `),
+      knex.schema.raw(`
+       DELETE ep
+       FROM environment_problem ep
+       LEFT JOIN environment e ON ep.environment = e.id
+       WHERE e.id IS NULL OR e.deleted != '0000-00-00 00:00:00'
+      `),
+]);
   };
 
   /**
