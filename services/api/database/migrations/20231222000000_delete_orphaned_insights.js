915f3818290ca790fa541ffd4e6576ca0b227acc
Blaize M Kaye <blaize.kaye@gmail.com>
2024-01-08 08:09:24 +1300

    Adds insights cleanup for missing projects

--- services/api/database/migrations/20231222000000_delete_orphaned_insights.js
+++ services/api/database/migrations/20231222000000_delete_orphaned_insights.js
@@ -8,15 +8,15 @@ exports.up = function(knex) {
        DELETE ef
        FROM environment_fact ef
        LEFT JOIN environment e ON ef.environment = e.id
-       WHERE e.id IS NULL OR e.deleted != '0000-00-00 00:00:00'
+       WHERE e.id IS NULL OR e.deleted != '0000-00-00 00:00:00' OR e.project not in (select id from project)
       `),
       knex.schema.raw(`
        DELETE ep
        FROM environment_problem ep
        LEFT JOIN environment e ON ep.environment = e.id
-       WHERE e.id IS NULL OR e.deleted != '0000-00-00 00:00:00'
+       WHERE e.id IS NULL OR e.deleted != '0000-00-00 00:00:00' OR e.project not in (select id from project)
       `),
-]);
+    ]);
   };
 
   /**
