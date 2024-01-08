57d0fa9cc9829d43c89b4fc70b8867261072e43c
Blaize M Kaye <blaize.kaye@gmail.com>
2024-01-09 08:43:39 +1300

    Adds migration to delete orphaned environments

---
+++ services/api/database/migrations/20240108000000_fix_env_orphans.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    return knex('environment')
+    .update('deleted', knex.raw('NOW()'))
+    .whereNotIn('project', function() {
+      this.select('id').from('project');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    return knex.schema
+};
