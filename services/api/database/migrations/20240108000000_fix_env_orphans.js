89a61e51c963817b3020079be1f3a5ca8cc06954
Blaize M Kaye <blaize.kaye@gmail.com>
2024-01-09 08:54:32 +1300

    changes logic of update for orphaned envs

--- services/api/database/migrations/20240108000000_fix_env_orphans.js
+++ services/api/database/migrations/20240108000000_fix_env_orphans.js
@@ -4,10 +4,9 @@
  */
 exports.up = async function(knex) {
     return knex('environment')
-    .update('deleted', knex.raw('NOW()'))
-    .whereNotIn('project', function() {
-      this.select('id').from('project');
-    });
+    .leftJoin('project', 'environment.project', 'project.id')
+    .whereNull('project.id')
+    .update('deleted', knex.raw('NOW()'));
 };
 
 /**
