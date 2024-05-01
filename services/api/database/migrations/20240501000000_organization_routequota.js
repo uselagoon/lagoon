b450ffaffb07aa546da27618aa7f88473a56aa86
shreddedbacon <b@benjackson.email>
2024-05-01 11:43:09 +1000

    refactor: change default route quota to unlimited for organizations

---
+++ services/api/database/migrations/20240501000000_organization_routequota.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+  .raw(`ALTER TABLE organization ALTER quota_route SET DEFAULT -1;`)
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+  .raw(`ALTER TABLE organization ALTER quota_route SET DEFAULT 5;`)
+};
