99e55dc49ddcd1084dd70d6f9bccefb5cf014bd8
Toby Bellwood <toby.bellwood@amazee.io>
2024-05-30 13:28:07 +1000

    chore: update feature flag enabler to be startup safe

--- services/broker/enable-feature-flags.sh
+++ services/broker/enable-feature-flags.sh
@@ -3,7 +3,25 @@
 # This script needs to be run after a minor version update (or prior to one)
 # to ensure that any disabled feature_flags are correctly enabled.
 
-for feature in $(curl -s -u "${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}" "http://${SERVICE_NAME}:15672/api/feature-flags/" | gojq -r '.[] | select(.state=="disabled") | .name'); do
-  echo enabling ${feature}
-  curl -X PUT --header "Content-Type: application/json" -u "${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}" -d '{"name":"'${feature}'"}' http://${SERVICE_NAME}:15672/api/feature-flags/${feature}/enable
+function is_broker_running {
+  local http_code=$(curl -s -o /dev/null -w "%{http_code}" -u "${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}" "http://${SERVICE_NAME}:15672/api/health/checks/virtual-hosts/")
+  if [[ $http_code -eq 200 ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+echo check broker is running
+until is_broker_running; do
+  echo Broker not running, waiting 2 seconds
+  sleep 2
 done
+
+echo enabling any disabled feature flags
+for feature in $(curl -s -u "${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}" "http://${SERVICE_NAME}:15672/api/feature-flags/" | gojq -r '.[] | select(.state=="disabled") | .name'); do
+  echo " - enabling ${feature}"
+  curl -X PUT --header "Content-Type: application/json" -s -u "${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}" -d '{"name":"'${feature}'"}' http://${SERVICE_NAME}:15672/api/feature-flags/${feature}/enable
+done
+
+echo all feature flags enabled
