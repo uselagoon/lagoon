924f087a2957e182a0e963d82478b7ceb694c6b8
shreddedbacon <b@benjackson.email>
2024-07-08 10:54:08 +1000

    chore: support s3 file export in actions-handler

--- services/actions-handler/handler/action_retention.go
+++ services/actions-handler/handler/action_retention.go
@@ -1,16 +1,17 @@
 package handler
 
 import (
-	"context"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	mq "github.com/cheshir/go-mq/v2"
+	"github.com/uselagoon/machinery/api/schema"
 	"github.com/uselagoon/machinery/utils/namespace"
 )
 
@@ -26,7 +27,14 @@ type S3RetentionCleanUp struct {
 	RemoteID      string `json:"remoteId"`
 }
 
-func (m *Messenger) handleRetention(ctx context.Context, messageQueue *mq.MessageQueue, action *Action, messageID string) error {
+type S3SaveHistory struct {
+	Environment       schema.Environment       `json:"environment"`
+	Project           schema.Project           `json:"project"`
+	TaskHistory       []map[string]interface{} `json:"taskHistory"`
+	DeploymentHistory []map[string]interface{} `json:"deploymentHistory"`
+}
+
+func (m *Messenger) handleRetention(action *Action, messageID string) error {
 	prefix := fmt.Sprintf("(messageid:%s) %s: ", messageID, action.EventType)
 	data, _ := json.Marshal(action.Data)
 	retention := S3RetentionCleanUp{}
@@ -55,7 +63,7 @@ func (m *Messenger) handleRetention(ctx context.Context, messageQueue *mq.Messag
 			retention,
 		)
 		if err != nil {
-			log.Println(fmt.Sprintf("%sError: %v", prefix, err))
+			log.Printf("%sError: %v", prefix, err)
 			return err
 		}
 		// handle cleaning up task logs
@@ -65,7 +73,7 @@ func (m *Messenger) handleRetention(ctx context.Context, messageQueue *mq.Messag
 			retention,
 		)
 		if err != nil {
-			log.Println(fmt.Sprintf("%sError: %v", prefix, err))
+			log.Printf("%sError: %v", prefix, err)
 			return err
 		}
 	case "buildCleanup":
@@ -81,7 +89,32 @@ func (m *Messenger) handleRetention(ctx context.Context, messageQueue *mq.Messag
 			retention,
 		)
 		if err != nil {
-			log.Println(fmt.Sprintf("%sError: %v", prefix, err))
+			log.Printf("%sError: %v", prefix, err)
+			return err
+		}
+	}
+	return nil
+}
+
+// handleSaveHistory will save the deployment and task history into a bucket for long term storage, this can be used later to retrieve stats history for deleted environments in the future
+func (m *Messenger) handleSaveHistory(action *Action, messageID string) error {
+	prefix := fmt.Sprintf("(messageid:%s) %s: ", messageID, action.EventType)
+	data, _ := json.Marshal(action.Data)
+	retention := S3SaveHistory{}
+	json.Unmarshal(data, &retention)
+	switch action.EventType {
+	case "saveHistory":
+		if m.EnableDebug {
+			log.Printf("%sSaving history for project %s-%d, environment %s-%d", prefix, retention.Project.Name, retention.Project.ID, retention.Environment.Name, retention.Environment.ID)
+		}
+		// save the file into a history directory in the lagoon files bucket
+		filePath := fmt.Sprintf("history/%s-%d/%s-%d/history-%d.json",
+			retention.Project.Name, retention.Project.ID,
+			retention.Environment.Name, retention.Environment.ID,
+			time.Now().Unix(),
+		)
+		err := m.uploadFileS3(prefix, filePath, "application/json", data)
+		if err != nil {
 			return err
 		}
 	}
@@ -90,8 +123,7 @@ func (m *Messenger) handleRetention(ctx context.Context, messageQueue *mq.Messag
 
 // deleteFileS3
 func (m *Messenger) deleteFileS3(prefix, fileName string, retention S3RetentionCleanUp) error {
-	var forcePath bool
-	forcePath = true
+	forcePath := true
 	session, err := session.NewSession(&aws.Config{
 		Region:           aws.String(m.S3Configuration.S3FilesRegion),
 		Endpoint:         aws.String(m.S3Configuration.S3FilesOrigin),
@@ -111,15 +143,14 @@ func (m *Messenger) deleteFileS3(prefix, fileName string, retention S3RetentionC
 		return err
 	}
 	if m.EnableDebug {
-		log.Println(fmt.Sprintf("%sDeleted file %s for environment: %v, id: %v", prefix, fileName, retention.EnvironmentName, retention.EnvironmentID))
+		log.Printf("%sDeleted file %s for environment: %v, id: %v", prefix, fileName, retention.EnvironmentName, retention.EnvironmentID)
 	}
 	return nil
 }
 
-// deleteDirFileS3
+// deleteDirFileS3 deletes files from a directory in s3
 func (m *Messenger) deleteFileInDirS3(prefix, fileName string, retention S3RetentionCleanUp) error {
-	var forcePath bool
-	forcePath = true
+	forcePath := true
 	session, err := session.NewSession(&aws.Config{
 		Region:           aws.String(m.S3Configuration.S3FilesRegion),
 		Endpoint:         aws.String(m.S3Configuration.S3FilesOrigin),
@@ -141,17 +172,49 @@ func (m *Messenger) deleteFileInDirS3(prefix, fileName string, retention S3Reten
 				Key:    c.Key,
 			})
 			if err != nil {
-				log.Println(fmt.Sprintf("%sError deleting file %s for environment: %v, id: %v: %v", prefix, *c.Key, retention.EnvironmentName, retention.EnvironmentID, err))
+				log.Printf("%sError deleting file %s for environment: %v, id: %v: %v", prefix, *c.Key, retention.EnvironmentName, retention.EnvironmentID, err)
 				continue // try other files
 			}
 			if m.EnableDebug {
-				log.Println(fmt.Sprintf("%sDeleted file %s for environment: %v, id: %v", prefix, *c.Key, retention.EnvironmentName, retention.EnvironmentID))
+				log.Printf("%sDeleted file %s for environment: %v, id: %v", prefix, *c.Key, retention.EnvironmentName, retention.EnvironmentID)
 			}
 		}
 		return *page.IsTruncated
 	})
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+// uploadFileS3 saves a file into s3
+func (m *Messenger) uploadFileS3(prefix, fileName, contentType string, message []byte) error {
+	forcePath := true
+	session, err := session.NewSession(&aws.Config{
+		Region:           aws.String(m.S3Configuration.S3FilesRegion),
+		Endpoint:         aws.String(m.S3Configuration.S3FilesOrigin),
+		Credentials:      credentials.NewStaticCredentials(m.S3Configuration.S3FilesAccessKeyID, m.S3Configuration.S3FilesSecretAccessKey, ""),
+		S3ForcePathStyle: &forcePath,
+	})
+	if err != nil {
+		return err
+	}
+
+	object := s3.PutObjectInput{
+		Bucket:      aws.String(m.S3Configuration.S3FilesBucket),
+		Key:         aws.String(fileName),
+		Body:        bytes.NewReader(message),
+		ContentType: aws.String(contentType),
+	}
+	if !m.S3Configuration.S3IsGCS {
+		object.ACL = aws.String("private")
+	}
+	_, err = s3.New(session).PutObject(&object)
+	if err != nil {
+		return err
+	}
+	if m.EnableDebug {
+		log.Printf("%sUploaded file %s", prefix, fileName)
 	}
 	return nil
 }
