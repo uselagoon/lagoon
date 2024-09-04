9d5f177aa2a6bdff29fda254fc45c1d2bb670b39
Brandon Williams <brandon.williams@amazee.io>
2024-09-04 07:13:28 -0500

    fix: Missed changes from keycloak 21 upgrade

--- services/keycloak/startup-scripts/10-newrelic.sh
+++ services/keycloak/startup-scripts/10-newrelic.sh
@@ -4,7 +4,7 @@ if [ -v NEW_RELIC_LICENSE_KEY ]
 then
     echo "Enabling newrelic monitor"
 
-    cat << 'EOF' >> /opt/jboss/keycloak/bin/standalone.conf
+    cat << 'EOF' >> /opt/keycloak/bin/standalone.conf
 
 JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/newrelic/newrelic.jar"
 EOF
