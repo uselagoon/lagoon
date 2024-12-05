a9b2f869ca63d3cf92e871f87fdaa776e0ac4895
Toby Bellwood <toby.bellwood@amazee.io>
2024-12-05 16:45:29 +1100

    chore: ensure migration down matches up

--- services/api/database/migrations/20241231000100_mysql8_compatibility.js
+++ services/api/database/migrations/20241231000100_mysql8_compatibility.js
@@ -49,25 +49,29 @@ exports.down = async function(knex) {
     return knex.schema
     .alterTable('advanced_task_definition', function (table) {
         table.specificType('description', 'text').defaultTo('').notNullable().alter();
-        table.string('group_name', 300).alter();
+        table.string('group_name', 2000).alter();
         table.specificType('command', 'text').defaultTo('').alter();
+    })
+    .alterTable('environment', function (table) {
+        table.string('deploy_base_ref', 100).alter();
+        table.string('deploy_head_ref', 100).alter();
     })
     .alterTable('environment_fact', function (table) {
         table.specificType('description', 'text').defaultTo('').alter();
         table.specificType('category', 'text').defaultTo('').alter();
     })
     .alterTable('environment_problem', function (table) {
-        table.string('identifier', 100).notNullable().alter();
-        table.string('lagoon_service', 100).defaultTo('').alter();
+        table.string('identifier', 300).notNullable().alter();
+        table.string('lagoon_service', 300).defaultTo('').alter();
         table.specificType('description', 'text').defaultTo('').alter();
-        table.string('version', 100).defaultTo('').alter();
+        table.string('version', 300).defaultTo('').alter();
     })
     .alterTable('project', function (table) {
         table.json('metadata').defaultTo('{}').alter();
     })
     .alterTable('s3_file', function (table) {
         table.datetime('created').notNullable().defaultTo(knex.fn.now()).alter();
-        table.datetime('deleted').notNullable().alter();
+        table.datetime('deleted').notNullable().defaultTo('0000-00-00 00:00:00').alter();
     })
     .alterTable('organization', function (table) {
         table.specificType('description', 'text').notNullable().defaultTo('').alter();
