791ce000f97af9e319093d2d9b207d814786ed4e
Blaize Kaye <blaize.kaye@amazee.io>
2025-06-19 16:53:30 +1200

    more types

--- services/api/database/migrations/20250619040511_add_file_to_auditlog_type.js
+++ services/api/database/migrations/20250619040511_add_file_to_auditlog_type.js
@@ -2,94 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-  return knex.schema.alterTable('audit_log', function (table) {
-    table
-      .enu('resource_type', [
-        'backup',
-        'bulkdeployment',
-        'deployment',
-        'deploytarget',
-        'deploytargetconfig',
-        'environment',
-        'group',
-        'notification',
-        'organization',
-        'project',
-        'sshkey',
-        'task',
-        'user',
-        'variable',
-        'workflow',
-        'file',
-      ])
-      .alter();
-    table
-      .enu('linked_resource_type', [
-        'backup',
-        'bulkdeployment',
-        'deployment',
-        'deploytarget',
-        'deploytargetconfig',
-        'environment',
-        'group',
-        'notification',
-        'organization',
-        'project',
-        'sshkey',
-        'task',
-        'user',
-        'variable',
-        'workflow',
-        'file',
-      ])
-      .alter();
-  });
+exports.up = function(knex) {
+    return knex.schema.
+        raw(`ALTER TABLE audit_log MODIFY resource_type ENUM('backup', 'bulkdeployment', 'deployment', 'deploytarget', 'deploytargetconfig', 'environment', 'group', 'notification', 'organization', 'project', 'sshkey', 'task', 'user', 'variable', 'workflow', 'file') NOT NULL;`).
+        raw(`ALTER TABLE audit_log MODIFY linked_resource_type ENUM('backup', 'bulkdeployment', 'deployment', 'deploytarget', 'deploytargetconfig', 'environment', 'group', 'notification', 'organization', 'project', 'sshkey', 'task', 'user', 'variable', 'workflow', 'file') NOT NULL;`);
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {
-  return knex.schema.alterTable('audit_log', function (table) {
-    table
-      .enu('resource_type', [
-        'backup',
-        'bulkdeployment',
-        'deployment',
-        'deploytarget',
-        'deploytargetconfig',
-        'environment',
-        'group',
-        'notification',
-        'organization',
-        'project',
-        'sshkey',
-        'task',
-        'user',
-        'variable',
-        'workflow',
-      ])
-      .alter();
-    table
-      .enu('linked_resource_type', [
-        'backup',
-        'bulkdeployment',
-        'deployment',
-        'deploytarget',
-        'deploytargetconfig',
-        'environment',
-        'group',
-        'notification',
-        'organization',
-        'project',
-        'sshkey',
-        'task',
-        'user',
-        'variable',
-        'workflow',
-      ])
-      .alter();
-  });
+exports.down = function(knex) {
+    return knex.schema.
+        raw(`ALTER TABLE audit_log MODIFY resource_type ENUM('backup', 'bulkdeployment', 'deployment', 'deploytarget', 'deploytargetconfig', 'environment', 'group', 'notification', 'organization', 'project', 'sshkey', 'task', 'user', 'variable', 'workflow') NOT NULL;`).
+        raw(`ALTER TABLE audit_log MODIFY linked_resource_type ENUM('backup', 'bulkdeployment', 'deployment', 'deploytarget', 'deploytargetconfig', 'environment', 'group', 'notification', 'organization', 'project', 'sshkey', 'task', 'user', 'variable', 'workflow') NOT NULL;`);
+
 };
