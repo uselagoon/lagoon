f23bd2074da96e63060d400f22d713d2ffb1e9a5
Blaize Kaye <blaize.kaye@amazee.io>
2025-06-23 16:00:28 +1200

    Removes extraneous comments of possible configs

--- services/api/src/util/config.ts
+++ services/api/src/util/config.ts
@@ -1,11 +1,6 @@
 export * from '@lagoon/commons/dist/util/config';
 
 const s3Config = {
-//   accessKeyId: process.env.S3_FILES_ACCESS_KEY_ID || 'minio',
-//   secretAccessKey: process.env.S3_FILES_SECRET_ACCESS_KEY || 'minio123',
-//   bucket: process.env.S3_FILES_BUCKET || 'lagoon-files',
-//   region: process.env.S3_FILES_REGION,
-//   origin: process.env.S3_FILES_HOST || 'http://docker.for.mac.localhost:9000',
   signedLinkExpiration: process.env.S3_SIGNED_LINK_EXPIRATION
     ? parseInt(process.env.S3_SIGNED_LINK_EXPIRATION, 10)
     : 300, // Default to 5 minutes
