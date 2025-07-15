c0885d3f66d0dba44ad322dbd5b7256b5480aada
Blaize Kaye <blaize.kaye@amazee.io>
2025-07-16 10:56:07 +1200

    Removed old email template

--- services/logs2notifications/internal/handler/email_events.go
+++ services/logs2notifications/internal/handler/email_events.go
@@ -9,38 +9,6 @@ import (
 	"text/template"
 )
 
-var htmlTemplate = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-  <title>{{.Title}}</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0 " />
-  <style>
-	@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
-	body {
-	  font-family: 'Roboto', sans-serif;
-	}
-	.main{
-	  border-left: 10px solid {{.Color}};
-	  padding: 10px;
-	}
-	ul {
-	  margin: 2px;
-	  list-style-type:none;
-	}
-  </style>
-</head>
-<body>
-  <div class="main">
-	<h2><strong>{{.Emoji}} [{{.ProjectName}}]</strong></h2>
-	<p>
-	    {{.MainHTML}}
-	</p>
-  </div>
-</body>
-</html>`
-
 var htmlContentTemplate = `  <div style="border-left: 10px solid {{.Color}};padding: 10px;">
 	<h2><strong>{{.Emoji}} [{{.ProjectName}}]</strong></h2>
 	<p>
