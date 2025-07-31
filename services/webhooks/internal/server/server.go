5bd829b57172716d1b22fb3085d13ae2963d693e
shreddedbacon <b@benjackson.email>
2025-07-31 10:40:42 +1000

    refactor: rewrite webhook-handler and webhooks2task

--- services/webhooks/internal/server/server.go
+++ services/webhooks/internal/server/server.go
@@ -3,8 +3,10 @@ package server
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"slices"
 
 	"github.com/drone/go-scm/scm"
@@ -13,11 +15,17 @@ import (
 	"github.com/drone/go-scm/scm/driver/gitea"
 	"github.com/drone/go-scm/scm/driver/github"
 	"github.com/drone/go-scm/scm/driver/gitlab"
+	"github.com/drone/go-scm/scm/driver/gogs"
+	"github.com/drone/go-scm/scm/driver/stash"
 	"github.com/gorilla/mux"
+	"github.com/uselagoon/lagoon-sneak/services/webhooks/internal/events"
+	syshook "github.com/uselagoon/lagoon-sneak/services/webhooks/internal/gitlab"
+	"github.com/uselagoon/lagoon-sneak/services/webhooks/internal/messaging"
 )
 
 type Server struct {
-	Router *mux.Router
+	Router    *mux.Router
+	Messaging *messaging.Messenger
 }
 
 func (s *Server) Initialize() {
@@ -48,24 +56,57 @@ func (s *Server) handleWebhookPost(w http.ResponseWriter, r *http.Request) {
 		client, _ = gitea.New("")
 		isGit = true
 	}
+	if r.Header.Get("X-Gogs-Event") != "" {
+		log.Println("GOGS")
+		// pass an empty url we only need the webhook parse capability
+		client, _ = gogs.New("")
+		isGit = true
+	}
+	// azure might not be usable https://github.com/uselagoon/lagoon/issues/3470#issuecomment-1607066097
 	if r.Header.Get("X-Lagoon-Azure-Devops") != "" {
 		log.Println("AZURE")
 		// pass an empty url we only need the webhook parse capability
 		client, _ = azure.New("", "none", "")
 		isGit = true
 	}
+	// check if stash or bitbucket
 	if r.Header.Get("X-Event-Key") != "" {
-		log.Println("BITBUCKET")
-		// bitbucket
-		client = bitbucket.NewDefault()
-		isGit = true
+		if slices.Contains(
+			// stash event types
+			[]string{"repo:refs_changed", "pr:opened", "pr:from_ref_updated", "pr:modified", "pr:declined", "pr:deleted", "pr:merged"},
+			r.Header.Get("X-Event-Key"),
+		) {
+			log.Println("STASH")
+			client = stash.NewDefault()
+			isGit = true
+		} else {
+			log.Println("BITBUCKET")
+			client = bitbucket.NewDefault()
+			isGit = true
+		}
 	}
 	if r.Header.Get("X-Gitlab-Event") != "" {
+		if r.Header.Get("X-Gitlab-Event") == "System Hook" {
+			// drone scm doesn't process gitlab system hooks that we need
+			// so handle them differently
+			log.Println("GITLAB SECURE")
+			// handle gitlab system hooks here
+			defer r.Body.Close()
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Invalid Body", http.StatusBadRequest)
+			}
+			token := os.Getenv("GITLAB_SYSTEM_HOOK_TOKEN")
+			if r.Header.Get("X-Gitlab-Token") != "" && r.Header.Get("X-Gitlab-Token") != token {
+				http.Error(w, "Gitlab system hook secret verification failed", http.StatusUnauthorized)
+			}
+			if err = syshook.HandleSystemHook("api", token, body); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+			return
+		}
 		log.Println("GITLAB")
 		client = gitlab.NewDefault()
-		if slices.Contains(secureGitlabSystemHooks, r.Header.Get("X-Gitlab-Event")) {
-			log.Println("GITLAB SECURE")
-		}
 		isGit = true
 	}
 	if r.Header.Get("X-GitHub-Event") != "" {
@@ -76,51 +117,19 @@ func (s *Server) handleWebhookPost(w http.ResponseWriter, r *http.Request) {
 	if isGit {
 		webhook, err = client.Webhooks.Parse(r, secret)
 		if err != nil {
-			// log.Println("ERR:", err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		if webhook != nil {
-			// b, _ := json.Marshal(webhook)
-			// fmt.Println(string(b))
 			switch event := webhook.(type) {
 			case *scm.PushHook:
-				log.Println(
-					"A2",
-					event.Repo.Clone,
-					event.Repo.CloneSSH,
-					event.Ref,
-					event.Commit.Sha,
-					event.Commit.Message,
-					event.Repo.Namespace,
-					event.Repo.Name,
-					event.Sender.Login,
-				)
+				events.HandlePush(event)
 			case *scm.BranchHook:
-				log.Println(
-					"B2",
-					event.Repo.Clone,
-					event.Repo.CloneSSH,
-					event.Ref,
-					event.Repo.Namespace,
-					event.Repo.Name,
-					event.Sender.Login,
-				)
+				events.HandleBranch(event)
 			case *scm.TagHook:
-			case *scm.IssueHook:
-			case *scm.IssueCommentHook:
+				// future?
 			case *scm.PullRequestHook:
-				log.Println(
-					"C2",
-					event.Repo.Clone,
-					event.Repo.CloneSSH,
-					event.PullRequest.Number,
-					event.Repo.Namespace,
-					event.Repo.Name,
-					event.Sender.Login,
-				)
-			case *scm.PullRequestCommentHook:
-			case *scm.ReviewCommentHook:
+				events.HandlePull(event)
 			}
 		}
 	}
@@ -145,24 +154,4 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
-}
-
-var secureGitlabSystemHooks = []string{
-	"group_create",
-	"group_rename",
-	"group_destroy",
-	"project_create",
-	"project_transfer",
-	"project_rename",
-	"project_update",
-	"project_destroy",
-	"user_create",
-	"user_rename",
-	"user_destroy",
-	"user_add_to_group",
-	"user_remove_from_group",
-	"user_add_to_team",
-	"user_remove_from_team",
-	"key_create",
-	"key_destroy",
 }
