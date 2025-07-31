844234490eaa448c5d8ac067199046392bd1dfe8
shreddedbacon <b@benjackson.email>
2025-07-31 10:40:42 +1000

    refactor: rewrite webhook-handler and webhooks2task

--- services/webhooks/cmd/main.go
+++ services/webhooks/cmd/main.go
@@ -1,11 +1,135 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"time"
+
+	mq "github.com/cheshir/go-mq/v2"
+	"github.com/uselagoon/lagoon-sneak/services/webhooks/internal/messaging"
 	"github.com/uselagoon/lagoon-sneak/services/webhooks/internal/server"
+	"github.com/uselagoon/machinery/utils/variables"
+)
+
+var (
+	mqUser              string
+	mqPass              string
+	mqHost              string
+	mqTLS               bool
+	mqVerify            bool
+	mqCACert            string
+	mqClientCert        string
+	mqClientKey         string
+	mqWorkers           int
+	rabbitRetryInterval int
 )
 
 func main() {
+	flag.StringVar(&mqUser, "rabbitmq-username", "guest",
+		"The username of the rabbitmq user.")
+	flag.StringVar(&mqPass, "rabbitmq-password", "guest",
+		"The password for the rabbitmq user.")
+	flag.StringVar(&mqHost, "rabbitmq-hostname", "localhost:5672",
+		"The hostname:port for the rabbitmq host.")
+	flag.BoolVar(&mqTLS, "rabbitmq-tls", false,
+		"To use amqps instead of amqp.")
+	flag.BoolVar(&mqVerify, "rabbitmq-verify", false,
+		"To verify rabbitmq peer connection.")
+	flag.StringVar(&mqCACert, "rabbitmq-cacert", "",
+		"The path to the ca certificate")
+	flag.StringVar(&mqClientCert, "rabbitmq-clientcert", "",
+		"The path to the client certificate")
+	flag.StringVar(&mqClientKey, "rabbitmq-clientkey", "",
+		"The path to the client key")
+	flag.IntVar(&mqWorkers, "rabbitmq-queue-workers", 1,
+		"The number of workers to start with.")
+	flag.IntVar(&rabbitRetryInterval, "rabbitmq-retry-interval", 30,
+		"The retry interval for rabbitmq.")
+
+	flag.Parse()
+
+	// get overrides from environment variables
+	mqUser = variables.GetEnv("RABBITMQ_USERNAME", mqUser)
+	mqPass = variables.GetEnv("RABBITMQ_PASSWORD", mqPass)
+	mqHost = variables.GetEnv("RABBITMQ_HOSTNAME", mqHost)
+	mqTLS = variables.GetEnvBool("RABBITMQ_TLS", mqTLS)
+	mqCACert = variables.GetEnv("RABBITMQ_CACERT", mqCACert)
+	mqClientCert = variables.GetEnv("RABBITMQ_CLIENTCERT", mqClientCert)
+	mqClientKey = variables.GetEnv("RABBITMQ_CLIENTKEY", mqClientKey)
+	mqVerify = variables.GetEnvBool("RABBITMQ_VERIFY", mqVerify)
+
+	brokerDSN := fmt.Sprintf("amqp://%s:%s@%s", mqUser, mqPass, mqHost)
+	if mqTLS {
+		verify := "verify_none"
+		if mqVerify {
+			verify = "verify_peer"
+		}
+		brokerDSN = fmt.Sprintf("amqps://%s:%s@%s?verify=%s", mqUser, mqPass, mqHost, verify)
+		if mqCACert != "" {
+			brokerDSN = fmt.Sprintf("%s&cacertfile=%s", brokerDSN, mqCACert)
+		}
+		if mqClientCert != "" {
+			brokerDSN = fmt.Sprintf("%s&certfile=%s", brokerDSN, mqClientCert)
+		}
+		if mqClientKey != "" {
+			brokerDSN = fmt.Sprintf("%s&keyfile=%s", brokerDSN, mqClientKey)
+		}
+	}
+	config := mq.Config{
+		ReconnectDelay: time.Duration(rabbitRetryInterval) * time.Second,
+		Exchanges: mq.Exchanges{
+			{
+				Name: "lagoon-tasks",
+				Type: "direct",
+				Options: mq.Options{
+					"durable":       true,
+					"delivery_mode": "2",
+					"headers":       "",
+					"content_type":  "",
+				},
+			},
+		},
+		Consumers: mq.Consumers{},
+		Queues: mq.Queues{
+			{
+				Name:     "lagoon-tasks:controller",
+				Exchange: "lagoon-tasks",
+				Options: mq.Options{
+					"durable":       true,
+					"delivery_mode": "2",
+					"headers":       "",
+					"content_type":  "",
+				},
+			},
+		},
+		Producers: mq.Producers{
+			{
+				Name:     "lagoon-logs",
+				Exchange: "lagoon-logs",
+				Options: mq.Options{
+					"delivery_mode": "2",
+					"headers":       "",
+					"content_type":  "",
+				},
+			},
+			{
+				Name:       "lagoon-tasks:controller",
+				Exchange:   "lagoon-tasks",
+				RoutingKey: "controller",
+				Options: mq.Options{
+					"delivery_mode": "2",
+					"headers":       "",
+					"content_type":  "",
+				},
+			},
+		},
+		DSN: brokerDSN,
+	}
+
+	messaging := messaging.New(config, true)
+
 	srv := server.Server{}
+	srv.Messaging = messaging
 	srv.Initialize()
 	srv.Run(":8010")
 }
--- services/webhooks/go.mod
+++ services/webhooks/go.mod
@@ -1,8 +1,25 @@
 module github.com/uselagoon/lagoon-sneak/services/webhooks
 
-go 1.23
+go 1.23.0
+
+toolchain go1.24.1
 
 require (
+	github.com/cheshir/go-mq/v2 v2.0.1
 	github.com/drone/go-scm v1.40.1
 	github.com/gorilla/mux v1.8.1
+	github.com/uselagoon/machinery v0.0.34
+	gitlab.com/gitlab-org/api/client-go v0.137.0
+)
+
+require (
+	github.com/NeowayLabs/wabbit v0.0.0-20210927194032-73ad61d1620e // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.8 // indirect
+	github.com/pborman/uuid v1.2.1 // indirect
+	github.com/rabbitmq/amqp091-go v1.7.0 // indirect
+	golang.org/x/oauth2 v0.30.0 // indirect
+	golang.org/x/time v0.12.0 // indirect
 )
