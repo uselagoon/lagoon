8865455f94edf020cd11671cf344e7d849883c85
shreddedbacon <b@benjackson.email>
2025-08-21 12:23:29 +1000

    chore: rename old and new webhook-handler

---
+++ services/webhook-handler/internal/events/deploy.go
@@ -0,0 +1,74 @@
+package events
+
+import (
+	"fmt"
+
+	"github.com/uselagoon/machinery/api/schema"
+	"github.com/uselagoon/machinery/utils/namespace"
+)
+
+func (e *Events) createDeployTask(project schema.Project, deployType, unSafeEnvironmentName, buildName, sourceUser, bulkID, bulkName string) ([]byte, error) {
+	environmentName := namespace.ShortenEnvironment(project.Name, namespace.MakeSafe(unSafeEnvironmentName))
+	if project.OrganizationDetails != nil {
+		for _, env := range project.Environments {
+			if env.Name != unSafeEnvironmentName {
+				if len(project.OrganizationDetails.Environments) >= project.OrganizationDetails.QuotaEnvironment && project.OrganizationDetails.QuotaEnvironment != -1 {
+					e.Messaging.Publish("lagoon-logs", []byte("exceed environment quota"))
+					return nil, fmt.Errorf("exceed environment quota")
+				}
+			}
+		}
+	}
+	prodEnvLimit := 2
+	// @TODO: handle `/` and `-` branch vs lagoon "made safe" names
+	if project.ProductionEnvironment == unSafeEnvironmentName ||
+		project.StandbyProductionEnvironment == unSafeEnvironmentName ||
+		project.ProductionEnvironment == environmentName ||
+		project.StandbyProductionEnvironment == environmentName {
+		prodEnvs := []schema.EnvironmentConfig{}
+		for _, env := range project.Environments {
+			if env.EnvironmentType == schema.ProductionEnv {
+				prodEnvs = append(prodEnvs, env)
+			}
+		}
+		if len(prodEnvs) >= prodEnvLimit {
+			exists := false
+			for _, env := range prodEnvs {
+				if env.Name == unSafeEnvironmentName || env.Name == environmentName {
+					exists = true
+				}
+			}
+			if !exists {
+				e.Messaging.Publish("lagoon-logs", []byte("exceed production limit"))
+				return nil, fmt.Errorf("exceed production limit")
+			}
+		}
+	} else {
+		devEnvs := []schema.EnvironmentConfig{}
+		for _, env := range project.Environments {
+			if env.EnvironmentType == schema.DevelopmentEnv {
+				devEnvs = append(devEnvs, env)
+			}
+		}
+		if project.DevelopmentEnvironmentsLimit != nil && len(devEnvs) >= int(*project.DevelopmentEnvironmentsLimit) {
+			exists := false
+			for _, env := range devEnvs {
+				if env.Name == unSafeEnvironmentName {
+					exists = true
+				}
+			}
+			if !exists {
+				e.Messaging.Publish("lagoon-logs", []byte("exceed development limit"))
+				return nil, fmt.Errorf("exceed development limit")
+			}
+		}
+	}
+
+	switch deployType {
+	case "push":
+		return e.deployPush(project, unSafeEnvironmentName, buildName, sourceUser, bulkID, bulkName)
+	case "pull":
+		return e.deployPull(project, unSafeEnvironmentName, buildName, sourceUser, bulkID, bulkName)
+	}
+	return nil, fmt.Errorf("nothing to do")
+}
---
+++ services/webhook-handler/internal/events/deploy_push.go
@@ -0,0 +1,126 @@
+package events
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+
+	"github.com/uselagoon/lagoon/services/webhook-handler/internal/lagoon"
+	"github.com/uselagoon/machinery/api/schema"
+)
+
+func (e *Events) deployPush(project schema.Project, unSafeEnvironmentName, buildName, sourceUser, bulkID, bulkName string) ([]byte, error) {
+	// get deploytargets for environment if the environment already exists
+	var deployTarget *schema.DeployTarget
+	var activeStanby *schema.DeployTarget
+	for _, env := range project.Environments {
+		if env.Name == unSafeEnvironmentName {
+			deployTarget = &env.DeployTarget
+		}
+		if project.StandbyProductionEnvironment == unSafeEnvironmentName || project.ProductionEnvironment == unSafeEnvironmentName {
+			activeStanby = &env.DeployTarget
+		}
+	}
+
+	if deployTarget != nil && activeStanby != nil {
+		if deployTarget.ID != activeStanby.ID {
+			e.Messaging.Publish("lagoon-logs", []byte("environments must be on same deploytarget"))
+			return nil, fmt.Errorf("environments must be on same deploytarget")
+		}
+	}
+
+	deployData := lagoon.DeployData{
+		BuildName:             buildName,
+		UnSafeEnvironmentName: unSafeEnvironmentName,
+		SourceUser:            sourceUser,
+		Project:               project,
+		SourceType:            lagoon.SourceWebhook,
+		DeployType:            schema.Branch,
+		BulkType:              lagoon.BulkDeploy,
+	}
+	if bulkID != "" {
+		deployData.BulkID = bulkID
+		deployData.BulkName = bulkName
+	}
+
+	if deployTarget != nil {
+		deployData.DeployTarget = *deployTarget
+		buildData, err := e.LagoonAPI.GetControllerBuildData(deployData)
+		if err != nil {
+			e.Messaging.Publish("lagoon-logs", []byte(err.Error()))
+			return nil, err
+		}
+		log.Println("deploy existing environment to specified target", deployTarget.Name, buildData.Spec.Project.Environment, buildData.Name)
+		e.Messaging.SendToLagoonTasks(fmt.Sprintf("%s:builddeploy", deployData.DeployTarget.Name), lagoon.BuildToBytes(buildData))
+		return []byte(buildData.Name), nil
+	}
+
+	if len(project.DeployTargetConfigs) > 0 {
+		for _, dtc := range project.DeployTargetConfigs {
+			switch dtc.Branches {
+			case "true":
+				deployData.DeployTarget = dtc.DeployTarget
+				buildData, err := e.LagoonAPI.GetControllerBuildData(deployData)
+				if err != nil {
+					e.Messaging.Publish("lagoon-logs", []byte(err.Error()))
+					return nil, err
+				}
+				log.Println("deploy environment to project target all branches allowed", dtc.DeployTarget.Name, buildData.Spec.Project.Environment, buildData.Name)
+				e.Messaging.SendToLagoonTasks(fmt.Sprintf("%s:builddeploy", deployData.DeployTarget.Name), lagoon.BuildToBytes(buildData))
+				return []byte(buildData.Name), nil
+			case "false":
+				e.Messaging.Publish("lagoon-logs", []byte("deployments disabled for target branches"))
+				return nil, fmt.Errorf("deployments disabled for project branches")
+			default:
+				re := regexp.MustCompile(dtc.Branches)
+				if re.MatchString(unSafeEnvironmentName) {
+					deployData.DeployTarget = dtc.DeployTarget
+					buildData, err := e.LagoonAPI.GetControllerBuildData(deployData)
+					if err != nil {
+						e.Messaging.Publish("lagoon-logs", []byte(err.Error()))
+						return nil, err
+					}
+					log.Println("deploy environment to project target matching regex pattern", dtc.DeployTarget.Name, buildData.Spec.Project.Environment, buildData.Name)
+					e.Messaging.SendToLagoonTasks(fmt.Sprintf("%s:builddeploy", deployData.DeployTarget.Name), lagoon.BuildToBytes(buildData))
+					return []byte(buildData.Name), nil
+				} else {
+					e.Messaging.Publish("lagoon-logs", []byte("didn't match regex pattern for target"))
+					return nil, fmt.Errorf("didn't match regex pattern for project")
+				}
+			}
+		}
+	} else {
+		switch project.Branches {
+		case "true":
+			deployData.DeployTarget = *project.DeployTarget
+			buildData, err := e.LagoonAPI.GetControllerBuildData(deployData)
+			if err != nil {
+				e.Messaging.Publish("lagoon-logs", []byte(err.Error()))
+				return nil, err
+			}
+			log.Println("deploy environment to deploytarget config defined target all branches allowed", project.DeployTarget.Name, buildData.Spec.Project.Environment, buildData.Name)
+			e.Messaging.SendToLagoonTasks(fmt.Sprintf("%s:builddeploy", deployData.DeployTarget.Name), lagoon.BuildToBytes(buildData))
+			return []byte(buildData.Name), nil
+		case "false":
+			e.Messaging.Publish("lagoon-logs", []byte("deployments disabled for project branches"))
+			return nil, fmt.Errorf("deployments disabled for project branches")
+		default:
+			re := regexp.MustCompile(project.Branches)
+			if re.MatchString(unSafeEnvironmentName) {
+				deployData.DeployTarget = *project.DeployTarget
+				buildData, err := e.LagoonAPI.GetControllerBuildData(deployData)
+				if err != nil {
+					e.Messaging.Publish("lagoon-logs", []byte(err.Error()))
+					return nil, err
+				}
+				log.Println("deploy environment to deploytarget config defined target matching regex pattern", project.DeployTarget.Name, buildData.Spec.Project.Environment, buildData.Name)
+				e.Messaging.SendToLagoonTasks(fmt.Sprintf("%s:builddeploy", deployData.DeployTarget.Name), lagoon.BuildToBytes(buildData))
+				return []byte(buildData.Name), nil
+			} else {
+				e.Messaging.Publish("lagoon-logs", []byte("didn't match regex pattern for project"))
+				return nil, fmt.Errorf("didn't match regex pattern for project")
+			}
+		}
+	}
+	return nil, fmt.Errorf("nothing to do")
+}
