07abc6200a853573930d5a67a0bbcb7b9e08565f
shreddedbacon <shreddedbacon@users.noreply.github.com>
2025-09-17 09:04:52 +1000

    fix: add index to organizationid in audit table

--- services/api/database/migrations/20250826000000_org_audit_log.js
+++ services/api/database/migrations/20250826000000_org_audit_log.js
@@ -6,6 +6,7 @@ exports.up = async function(knex) {
   return knex.schema
   .alterTable('audit_log', (table) => {
     table.integer('organization_id');
+    table.index('organization_id', 'organization_index');
   })
 };
 
