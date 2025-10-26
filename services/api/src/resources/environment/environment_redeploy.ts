7a28ddd06bbe07eb5f7a2de56770ca4920d8a1d5
Blaize Kaye <blaize.kaye@amazee.io>
2025-10-27 09:51:09 +1300

    Fixes env var updated query

--- services/api/src/resources/environment/environment_redeploy.ts
+++ services/api/src/resources/environment/environment_redeploy.ts
@@ -35,38 +35,29 @@ _,
 
 const getPendingEnvVarChanges = async(sqlClientPool, envId) => {
       const sql = `
-    SELECT DISTINCT
-      ev.name,
-      ev.updated,
-      e.id,
-      e.name as env_name,
-	    COALESCE(
-	      IF(ev.environment IS NULL,NULL,'Environment'),
-	      IF(ev.project IS NULL,NULL,'Project'),
-	      IF(ev.organization IS NULL,NULL,'Organization')
-      ) as varsource,
-      COALESCE(MAX(d.created) OVER (PARTITION BY e.id), '1970-01-01') as last_deployment
-    FROM environment as e
-    LEFT JOIN deployment as d ON e.id = d.environment
-    INNER JOIN project as p ON p.id = e.project
-    LEFT JOIN organization as o ON o.id = p.organization
-    LEFT JOIN env_vars as ev ON (
-      ev.environment = e.id OR
-      ev.project = p.id OR
-      ev.organization = o.id
-    )
-    WHERE ev.name IS NOT NULL AND e.id = ?
-  `;
+select e.id as env_id, e.name as env_name, ev.name as envvar_name, ev.updated as envvar_updated,
+  COALESCE(
+    IF(ev.environment IS NULL,NULL,'Environment'),
+      IF(ev.project IS NULL,NULL,'Project'),
+      IF(ev.organization IS NULL,NULL,'Organization')
+      ) as envvar_source
+FROM environment as e
+INNER JOIN project as p ON p.id = e.project
+LEFT JOIN organization as o ON o.id = p.organization
+LEFT JOIN env_vars as ev ON (
+   ev.environment = e.id OR
+   ev.project = p.id OR
+   ev.organization = o.id
+)
+WHERE ev.name IS NOT NULL AND e.id = ?
+AND ev.updated > (select coalesce(max(completed), '0000-00-00 00:00:00') from deployment where environment = ? and status = ?)
+ORDER BY ev.updated asc
+`;
 
-  const results = await query(sqlClientPool, sql, [envId]);
+  const results = await query(sqlClientPool, sql, [envId, envId, 'complete']);
 
-  // Filter in memory for env vars updated after last deployment
-  const pendingChanges = results.filter(row => {
-    const updated = new Date(row.updated);
-    const lastDeployment = new Date(row.lastDeployment);
-    return updated > lastDeployment;
-  }).map(row => {
-    return {type:`Environment Variable - ${row.varsource} level`, details: row.name};
+  const pendingChanges = results.map(row => {
+    return {type:`Environment Variable - ${row.envvarSource} level`, details: row.envvarName, date: row.envvarUpdated};
   });
 
   return pendingChanges;
