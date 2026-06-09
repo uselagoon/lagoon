7e67ae78c67999483bf84ea53cefdf07d1980ebe
Brandon Williams <brandon.williams@amazee.io>
2026-06-09 11:27:17 -0500

    Fix groupByNameAndOrganization resolver errors when group not found

--- services/api/src/resources/organization/resolvers.ts
+++ services/api/src/resources/organization/resolvers.ts
@@ -2,10 +2,11 @@ import * as R from 'ramda';
 import { ResolverFn } from '../';
 import { logger } from '../../loggers/logger';
 import { query, isPatchEmpty, knex } from '../../util/db';
+import { GroupNotFoundError } from '../../models/group';
 import { Helpers as projectHelpers } from '../project/helpers';
 import { Helpers} from './helpers';
 import { Sql } from './sql';
-import { arrayDiff, toNumber, requestsExtraFields } from '../../util/func';
+import { arrayDiff, requestsExtraFields } from '../../util/func';
 import { Helpers as openshiftHelpers } from '../openshift/helpers';
 import { Helpers as notificationHelpers } from '../notification/helpers';
 import { Helpers as groupHelpers } from '../group/helpers';
@@ -573,25 +574,21 @@ export const getGroupRolesByUserIdAndOrganization: ResolverFn =async (
 }
 
 // list all groups by organization id
-export const getGroupsByNameAndOrganizationId: ResolverFn = async (
-  root,
+export const getGroupByNameAndOrganizationId: ResolverFn = async (
+  _,
   { name, organization },
-  { hasPermission, models, sqlClientPool }
+  { hasPermission, models }
 ) => {
-  try {
-    await hasPermission('organization', 'viewGroup', {
-      organization: organization,
-    });
+  await hasPermission('organization', 'viewGroup', {
+    organization: organization,
+  });
 
-    const group = await models.GroupModel.loadGroupByName(name);
-    const org = await groupHelpers(sqlClientPool).selectOrganizationIdByGroupId(group.id)
-    if (org == organization) {
-      return group
-    }
-  } catch (err) {
-      return [];
+  const group = await models.GroupModel.loadSparseGroupByName(name);
+  if (!group.organization || group.organization != organization) {
+    throw new GroupNotFoundError(`Group not found: ${name}`);
   }
-  return [];
+
+  return group;
 };
 
 export const getGroupCountByOrganizationProject: ResolverFn = async (
