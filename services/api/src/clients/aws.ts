d19cdf577c7e305f711dd3ce5fb5396150886843
tomaioo <hongsaygio.com@gmail.com>
2026-07-31 23:46:50 -0700

    fix(security): 2 improvements across 2 files
    - Security: Hardcoded Default Credentials for S3-Compatible Storage
    - Security: Hardcoded Default Elasticsearch Admin Password
    
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- services/api/src/clients/aws.ts
+++ services/api/src/clients/aws.ts
@@ -3,8 +3,8 @@ import { getConfigFromEnv } from '../util/config';
 
 export const config = {
   origin: getConfigFromEnv('S3_FILES_HOST', 'http://docker.for.mac.localhost:9000'),
-  accessKeyId: getConfigFromEnv('S3_FILES_ACCESS_KEY_ID', 'minio'),
-  secretAccessKey: getConfigFromEnv('S3_FILES_SECRET_ACCESS_KEY', 'minio123'),
+  accessKeyId: getConfigFromEnv('S3_FILES_ACCESS_KEY_ID'),
+  secretAccessKey: getConfigFromEnv('S3_FILES_SECRET_ACCESS_KEY'),
   region: getConfigFromEnv('S3_FILES_REGION', 'us-east-1'),
   bucket: getConfigFromEnv('S3_FILES_BUCKET', 'lagoon-files')
 };
